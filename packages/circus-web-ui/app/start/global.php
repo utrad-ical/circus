debb6f9d04af559366b136086c6dea85f7546238
Soichiro Miki <smiki-tky@umin.ac.jp>
2015-01-16 00:29:20 +0900

    Modify class autoloader to use "util" directory

--- packages/circus-web-ui/app/start/global.php
+++ packages/circus-web-ui/app/start/global.php
@@ -17,7 +17,8 @@ ClassLoader::addDirectories(array(
 	app_path().'/controllers',
 	app_path().'/models',
 	app_path().'/database/seeds',
-	app_path().'/lib'
+	app_path().'/lib',
+	app_path().'/util'
 
 ));
 

b96efdc1b5ad4c11b73e78ec7357fb4a5765ae6a
Yukihiro Nomura <nomuray-tky@umin.ac.jp>
2015-01-26 10:21:24 +0900

    Modify static_path_resolver.js
    Corresponding to directory structure for DICOM storage

--- packages/circus-rs/static_path_resolver.js
+++ packages/circus-rs/static_path_resolver.js
@@ -1,10 +1,15 @@
 var path = require('path');
 var fs = require('fs');
 var config = require('./static_path_resolver_config');
+var crypto = require('crypto');
 
 function resolvPath(series, callback)
 {
-  var dcmdir = path.join(config.datadir, series);
+  var hash = crypto.createHash('sha256');
+  hash.update(series);
+  var hashStr = hash.digest('hex');
+
+  var dcmdir = path.join(config.datadir, hashStr.substring(0,2), hashStr.substring(2,4), series);
   if (!fs.existsSync(dcmdir)) {
     console.log('not exists:' + dcmdir);
     dcmdir = null;
@@ -16,4 +21,3 @@ function resolvPath(series, callback)
 module.exports = {
 	resolvPath: resolvPath
 }
-

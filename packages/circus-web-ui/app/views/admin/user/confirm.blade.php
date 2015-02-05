92a3f10b2e0024b89491aeeff54604c08e7a71c7
Sayuri Tani <stani@spiritek.co.jp>
2015-02-05 11:37:31 +0900

    Modification associated with the column type change

--- packages/circus-web-ui/app/views/admin/user/confirm.blade.php
+++ packages/circus-web-ui/app/views/admin/user/confirm.blade.php
@@ -1,6 +1,54 @@
 @extends('common.layout')
 @section('content')
-<script type="text/javascript" src="{{asset('/js/ajax/user.js')}}"></script>
+<script type="text/javascript">
+$(function() {
+	$('.link_user_input').click(function(){
+		//Get the form ID to be sent
+		var post_data = '{"btnBack":"btnBack"}';
+		post_data = JSON.parse(post_data);
+
+		var target_elm = $('.frm_user_input');
+
+		$.ajax({
+			url: "./input",
+			type: 'POST',
+			data: post_data,
+			dataType: 'json',
+			error: function(){
+				alert('I failed to communicate.');
+			},
+			success: function(res){
+				target_elm.empty();
+				target_elm.append(res.response);
+				target_elm.attr('style', 'display:inline;');
+			}
+		});
+		return false;
+	});
+
+	//Processing at the time of registration button is pressed
+	$('.user_complete').click(function(){
+		var post_data = $('.frm_user_complete').serializeArray();
+		var target_elm = $('.frm_user_input');
+
+		$.ajax({
+			url: "./complete",
+			type: 'POST',
+			data: post_data,
+			dataType: 'json',
+			error: function(){
+				alert('I failed to communicate.');
+			},
+			success: function(res){
+				target_elm.empty();
+				target_elm.append(res.response);
+				target_elm.attr('style', 'display:inline;');
+			}
+		});
+		return false;
+	});
+});
+</script>
 <div class="page_unique">
 	<h1 class="page_ttl">{{$title}}</h1>
 	{{Form::open(['url' => asset('admin/user/complete'), 'method' => 'post'])}}

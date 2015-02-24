53f8b24dcfbc5d23ac7e0a45b8e1d07a667b8120
Soichiro Miki <smiki-tky@umin.ac.jp>
2015-02-24 19:14:47 +0900

    Update Project model and implement better validation rules

--- packages/circus-web-ui/app/models/Project.php
+++ packages/circus-web-ui/app/models/Project.php
@@ -1,11 +1,22 @@
 <?php
 
-use Jenssegers\Mongodb\Model as Eloquent;
-
 /**
  * Model class for projects.
+ * @property number projectID Project ID.
+ * @property string projectName Project name.
+ * @property array createGroups List of groups which can create new case for this project.
+ * @property array viewGroups List of groups which can view cases belonging to this project.
+ * @property array updateGroups List of groups which can modify cases belonging to this project.
+ * @property array reviewGroups List of groups which can set 'reviewed' status for cases belonging to this project.
+ * @property array deleteGroups List of groups which can delete cases from this project.
+ * @property array personalInfoViewGroups List of groups which can view personal information.
+ * @property string windowPriority Order of which window data takes precedence.
+ * @property array windowPresets Array of window presets.
+ * @property array caseAttributesSchema List of attributes for cases.
+ * @property array labelAttributesSchema List of attributes for labels.
  */
-class Project extends Eloquent {
+class Project extends BaseModel
+{
 	protected $connection = 'mongodb';
 
 	const COLLECTION = 'Projects';
@@ -41,6 +52,11 @@ class Project extends Eloquent {
 	const AUTH_TYPE_DELETE = 'deleteGroups';
 
 	/**
+	 * Case deleting role
+	 */
+	const AUTH_TYPE_PERSONAL_INFO_VIEW = 'personalInfoViewGroups';
+
+	/**
 	 * List all projects where the current user has access with the given authority type.
 	 * @return Project login user operable List
 	 * @param $auth_gp string Authority type
@@ -72,13 +88,19 @@ class Project extends Eloquent {
 		return $project ? $project[0]->projectName : '';
 	}
 
-	/**
-	 * Validation rules
-	 * @var rules Validate rules array
-	 */
-	private $rules = array(
-		'projectID'		=>	'required|integer',
-		'projectName'	=>	'required'
+	protected $rules = array(
+		'projectID' => 'required|strict_integer',
+		'projectName' => 'required|strict_string',
+		self::AUTH_TYPE_CREATE => 'required|array_of_group_ids',
+		self::AUTH_TYPE_VIEW => 'required|array_of_group_ids',
+		self::AUTH_TYPE_UPDATE => 'required|array_of_group_ids',
+		self::AUTH_TYPE_REVIEW => 'required|array_of_group_ids',
+		self::AUTH_TYPE_DELETE => 'required|array_of_group_ids',
+		self::AUTH_TYPE_PERSONAL_INFO_VIEW => 'required|array_of_group_ids',
+		'windowPriority' => 'required|strict_string',
+ 		'windowPresets' => 'array',
+		'caseAttributesSchema' => 'array',
+		'labelAttributesSchema' => 'array'
 	);
 
 	/**

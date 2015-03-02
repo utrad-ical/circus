22ccc6b804d69fce054d53ae7d6cb6e4273ec342
Sayuri Tani <stani@spiritek.co.jp>
2015-03-02 18:38:24 +0900

    Server configuration external file

--- packages/circus-web-ui/app/config/config.php
+++ packages/circus-web-ui/app/config/config.php
@@ -2,7 +2,5 @@
 //ユーザが設定可能なConfig
 return array(
 	'page_display'			=>	50,	//デフォルトの検索結果1ページ表示件数
-	'upload_path'			=>	dirname(__FILE__).'/../uploads/',
-	'domain'				=>	'http://160.16.56.191/',		//ドメイン
-	'dicom_img_base_url'	=>	'http://160.16.56.191:3000/'	//画像格納ディレクトリ
+	'upload_path'			=>	dirname(__FILE__).'/../uploads/'
 );

9d25e8b5e70cdb70658d6d44146c9dcdc42e2d4c
Soichiro Miki <smiki-tky@umin.ac.jp>
2015-03-24 19:53:15 +0900

    Do some refactor on common.js
    Reduce the number of multibyte comments.

--- packages/circus-web-ui/public/js/common.js
+++ packages/circus-web-ui/public/js/common.js
@@ -1,113 +1,101 @@
-$(function(){
+$(function () {
 
-	//背景色切替
-	$('#btn_area').find('.color_select').change(function(){
-		var mode_flg	=	$('#btn_area').find('.color_select:checked').val();
-		if(mode_flg==0){
-			//白背景モード
-			$('body').removeClass('mode_black')
-				.addClass('mode_white');
-		}else{
-			//黒背景モード
-			$('body').removeClass('mode_white')
-				.addClass('mode_black');
-		}
-	});
+  // Temporarily changes the color theme
+  var color_select = $('#btn_area .color_select');
+  color_select.change(function () {
+    var mode = color_select.filter(':checked').val();
+    $('body').removeClass('mode_white mode_black')
+      .addClass(mode == 1 ? 'mode_black' : 'mode_white');
+  });
 
+  /* Run jQuery UI widgets */
 
+  // datepicker
+  if ($('.datepicker').length > 0) {
+    $('.datepicker').datepicker({
+      dateFormat: "yy/mm/dd"
+    });
+  }
 
-	/*jQuery UI widget Run*/
+  // droppable area
+  if ($('.droppable_area').length > 0) {
 
-	//datepicker
-	if($('.datepicker').length>0){
-		$('.datepicker').datepicker({
-			dateFormat:"yy/mm/dd"
-		});
-	}
+    jQuery.event.props.push('dataTransfer');
+    $('.droppable_area').each(function () {
 
-	//droppable area
-	if($('.droppable_area').length>0){
+      var droppable_area = $(this);
+      var the_form = $(this).closest('form');
+      var input_elm = $(this).find('.upload_file_input_elm');
 
-		jQuery.event.props.push('dataTransfer');
-		$('.droppable_area').each(function(){
+      // ファイル選択フォームからの入力
+      input_elm.bind("change", function () {
+        // 選択されたファイル情報を取得
+        var files = this.files;
 
-			var droppable_area	=	$(this);
-			var the_form	=	$(this).closest('form');
-			var input_elm	=	$(this)	.find('.upload_file_input_elm');
+        // アップロード処理
+        uploadAjax(files);
+      });
 
-			 // ファイル選択フォームからの入力
-			 input_elm.bind("change", function(){
-				  // 選択されたファイル情報を取得
-				  var files = this.files;
+      // ドラッグドロップからの入力
+      droppable_area.bind("drop", function (event) {
+        event.stopPropagation();
+        event.preventDefault();
 
-				  // アップロード処理
-				  uploadAjax(files);
-			 });
+        var dt = event.dataTransfer;
+        var ajax_files = dt.files[0];
+        var the_url = the_form.attr('action');
+        uploadAjax(the_url, ajax_files);
 
-			 // ドラッグドロップからの入力
-			droppable_area.bind("drop", function(event){
-				event.stopPropagation();
-				event.preventDefault();
+      }).bind("dragenter dragover", false);
 
-				var dt = event.dataTransfer;
-				var ajax_files = dt.files[0];
-				var the_url	=	the_form.attr('action');
-				uploadAjax(the_url,ajax_files);
+    });
 
-			 }).bind("dragenter dragover", false);
+  }
 
-		});//eachここまで
+  // UI multiple select
+  if ($('.multi_select').length > 0) {
+    $('.multi_select').not('.active').multiselect({
+      header: false,
+      noneSelectedText: '(all)',
+      selectedList: 10
+    }).addClass('active');
+  }
 
-	}//droppable_areaへのイベント付与のためのif文ここまで
-
-	//Ui multiple select
-	if($('.multi_select').length>0){
-		multiSelectActivate();
-	}
-
-	//UI soirtable
-	if($('.ui-sortable').length>0){
-		$('.ui-sortable').sortable();
-	}
+  // UI sortable
+  if ($('.ui-sortable').length > 0) {
+    $('.ui-sortable').sortable();
+  }
 
 });
 
-
-
-var multiSelectActivate	=	function(){
-
-	$('.multi_select').not('.active').multiselect({
-		header: false,
-		noneSelectedText: '(all)',
-		selectedList: 10
-	})
-	.addClass('active');
-
-}
-
-
-
 //ファイルアップロード時ajax
-var uploadAjax = function (ajax_url,files) {
+var uploadAjax = function (ajax_url, files) {
 
   var fd = new FormData();// FormData オブジェクトを用意
 
   // ファイル情報を追加する
   for (var i = 0; i < files.length; i++) {
-		fd.append("files", files[i]);
+    fd.append("files", files[i]);
   }
 
   alert('本来はここでAjax');	//開発用,本来はこのアラートはない
 
   // XHR で送信
   $.ajax({
-	url: ajax_url,
-	type: "POST",
-	data: fd,
-	processData: false,
-	contentType: false
+    url: ajax_url,
+    type: "POST",
+    data: fd,
+    processData: false,
+    contentType: false
   });
 };
 
-//桁数整形
-var zeroFormat=function(e,t){var n=String(e).length;if(t>n){return(new Array(t-n+1)).join(0)+e}else{return e}}
+// Pads '0' at the left of the given string
+var zeroFormat = function (input, width) {
+  var n = String(input).length;
+  if (width > n) {
+    return (new Array(width - n + 1)).join(0) + input;
+  } else {
+    return input;
+  }
+};

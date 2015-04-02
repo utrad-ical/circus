75b65890aaee7159daab4c041219e0f868c4037c
Yukihiro Nomura <nomuray-tky@umin.ac.jp>
2015-04-02 20:52:35 +0900

    Refine comment

--- packages/circus-web-ui/app/lib/VolumeExporter/Exporter.php
+++ packages/circus-web-ui/app/lib/VolumeExporter/Exporter.php
@@ -30,7 +30,7 @@ class Exporter
 		}
 		exec($this->dumperPath() . ' ' . implode(' ', $args), $output);
 
-		// Check exported file
+		// Checking whether 'dicom_voxel_cump' command successfully executed or not
 		if (count($output) > 0 && strncmp(end($output), "Succeeded", 9) !== 0) {
 			throw new Exception("Failed to create original volume.");
 		}

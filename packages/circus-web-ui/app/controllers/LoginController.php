b279d4180a5b74eb53dae6566f8aab3ee683d847
Soichiro Miki <smiki-tky@umin.ac.jp>
2015-04-09 22:36:34 +0900

    Code cleanup for login display
    Added 'autofocus' to the userID field.

--- packages/circus-web-ui/app/controllers/LoginController.php
+++ packages/circus-web-ui/app/controllers/LoginController.php
@@ -2,66 +2,49 @@
 /**
  * Class to perform the operation of the login screen
  */
-class LoginController extends BaseController {
+class LoginController extends BaseController
+{
 	/**
 	 * Login screen display
 	 */
-	public function getIndex() {
-		$result = array();
-		self::setCommonSettings($result);
-		return View::make('login', $result);
+	public function getIndex()
+	{
+		return View::make('login');
 	}
 
 	/**
 	 * Login process
 	 */
-	public function login() {
-		$result = array();
-		self::setCommonSettings($result);
-
-		//Input value acquisition
+	public function login()
+	{
 		$inputs = Input::only(array('loginID', 'password'));
-		$result['loginID'] = $inputs['loginID'];
+		$param = array('loginID' => $inputs['loginID']);
 
-		//Some ID / PW both input
-		if ($inputs['loginID'] && $inputs['password']) {
-			//Check whether there is user information
-			$secret_key = Config::get('const.hash_key');
-			if (!Auth::attempt($inputs)){
-				$result['error_msg'] = 'ID or password is incorrect.';
-				return View::make('login', $result);
-			} else {
-				//Data acquisition
-				$user = Auth::user();
-				$user->lastLoginTime = new MongoDate(strtotime(date('Y-m-d h:i:s')));
-				$user->lastLoginIP =  $_SERVER['REMOTE_ADDR'];
-				$user->save();
-				return Redirect::to('home');
-			}
-		} else {
-			$result['error_msg'] = 'Please enter the ID and password.';
-			return View::make('login', $result);
+		if (!strlen($inputs['loginID']) || !strlen($inputs['password'])) {
+			$param['error_msg'] = 'Please enter your ID and password.';
+			return View::make('login', $param);
 		}
+		if (!Auth::attempt($inputs)) {
+			$param['error_msg'] = 'Invalid ID or password.';
+			return View::make('login', $param);
+		}
+
+		// login succeeded
+		$user = Auth::user();
+		$user->lastLoginTime = new MongoDate();
+		$user->lastLoginIP = $_SERVER['REMOTE_ADDR'];
+		$user->save();
+		return Redirect::to('home');
 	}
 
 	/**
-	 * Log-out processing
+	 * Logout process
 	 */
-	public function logout() {
-		//Log-out processing
+	public function logout()
+	{
 		Session::flush();
 		Auth::logout();
-
-		//And redirected to the login screen
 		return Redirect::to('login');
 	}
 
-	/**
-	 * Common setting
-	 * @param $ary Result set array
-	 */
-	public function setCommonSettings(&$ary) {
-		$ary['title'] = 'Sign in';
-		$ary['url'] = 'login';
-	}
 }

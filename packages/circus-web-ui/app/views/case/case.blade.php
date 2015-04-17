c80cfbf3a321afa70434486fef71ed7202d6ffde
Sayuri Tani <stani@spiritek.co.jp>
2015-04-17 18:13:00 +0900

    検索結果リスト下部のソート機能およびページング削除 Sort Orderまたはdisplay_numが選択されている場合は再検索を行わないように修正

--- packages/circus-web-ui/app/views/case/case.blade.php
+++ packages/circus-web-ui/app/views/case/case.blade.php
@@ -3,10 +3,10 @@
 		<ul class="common_pager clearfix">
 			{{$list_pager->links()}}
 			<li class="pager_sort_order">
-				{{Form::select('sort', Config::get('const.search_case_sort'), isset($inputs['sort']) ? $inputs['sort'] : '', array('class' => 'w_max change_select', 'data-target-dom' => 'sort_order_down', 'id' => 'sort_order_up'))}}
+				{{Form::select('sort', Config::get('const.search_case_sort'), isset($inputs['sort']) ? $inputs['sort'] : '', array('class' => 'w_max change_select'))}}
 			</li>
 			<li class="pager_disp_num">
-				{{Form::select('disp', Config::get('const.search_display'), isset($inputs['disp']) ? $inputs['disp'] : '', array('class' => 'w_max change_select', 'data-target-dom' => 'display_num_down', 'id' => 'display_num_up'))}}
+				{{Form::select('disp', Config::get('const.search_display'), isset($inputs['disp']) ? $inputs['disp'] : '', array('class' => 'w_max change_select'))}}
 			</li>
 		</ul>
 	@endif
@@ -60,17 +60,6 @@
 			</tr>
 		@endif
 	</table>
-	@if (count($list) > 0)
-		<ul class="common_pager clearfix">
-			{{$list_pager->links()}}
-			<li class="pager_sort_order">
-				{{Form::select('sort', Config::get('const.search_case_sort'), isset($inputs['sort']) ? $inputs['sort'] : '', array('class' => 'w_max change_select', 'data-target-dom' => 'sort_order_up', 'id' => 'sort_order_down'))}}
-			</li>
-			<li class="pager_disp_num">
-				{{Form::select('disp', Config::get('const.search_display'), isset($inputs['disp']) ? $inputs['disp'] : '', array('class' => 'w_max change_select', 'data-target-dom' => 'display_num_up', 'id' => 'display_num_down'))}}
-			</li>
-		</ul>
-	@endif
 	<script>
 		$(function() {
 			$('.link_detail').click(function(){
@@ -80,27 +69,19 @@
 			});
 
 			$('.change_select').change(function(){
-				// Get the combo ID you want to change
-				var change_select = $(this).attr('data-target-dom');
-				// Get the value you want to selected
-				var select_value = $("select[name='"+$(this).attr('name')+"']").val();
-				// Change selected in the combo
-				$('#'+change_select).find('option').each(function(){
-					var this_num = $(this).val();
-					if(this_num == select_value){
-						$(this).attr('selected','selected');
-					}
-				});
-
 				//Add a hidden element so do a search
 				var sort = $("select[name='sort']").val();
 				var disp = $("select[name='disp']").val();
-				var sort_elm = $("<input>", {type:"hidden", name:"sort", value:sort});
-				$('#form_case_search').append(sort_elm);
-				var disp_elm = $("<input>", {type:"hidden", name:"disp", value:disp});
-				$('#form_case_search').append(disp_elm);
-				//Event firing
-				$('#btn_submit').trigger('click');
+
+				//display_num または Sort Order指定時は検索が行わない
+				if (sort.length && disp.length) {
+					var sort_elm = $("<input>", {type:"hidden", name:"sort", value:sort});
+					$('#form_case_search').append(sort_elm);
+					var disp_elm = $("<input>", {type:"hidden", name:"disp", value:disp});
+					$('#form_case_search').append(disp_elm);
+					//Event firing
+					$('#btn_submit').trigger('click');
+				}
 				return false;
 			});
 

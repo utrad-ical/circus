dcf29fda0f07950436f170f8a1ffe29316e0cb85
spkyokoyama <yokoyama@cube.in.spiritek.co.jp>
2015-04-28 14:52:48 +0900

    #602 ウィンドウプリセット対応 series詳細画面にて対応。　※case detail画面は未完

--- packages/circus-web-ui/app/views/series/detail.blade.php
+++ packages/circus-web-ui/app/views/series/detail.blade.php
@@ -16,176 +16,224 @@
 
 @if (!isset($error_msg))
 <script>
-var series_slider_max = 0;
-//Project-specific feature set to control the controller viewer widget
-$(function(){
-	//Data group to pass when you ignite the controller immediately after page load
-	//The controller 1 per group to be linked to multiple viewers
-	var	voxel_container	=	new voxelContainer();	//Label information storage object (three sides shared)
-	voxel_container.name	=	'my_voxel';
+    var series_slider_max = 0;
+    //Project-specific feature set to control the controller viewer widget
+    $(function(){
+        //Data group to pass when you ignite the controller immediately after page load
+        //The controller 1 per group to be linked to multiple viewers
+        var	voxel_container	= new voxelContainer();	//Label information storage object (three sides shared)
+        voxel_container.name = 'my_voxel';
 
-	var	initInfo	=	[
-		{
-			baseUrl : "{{{Helper\ConfigHelper::getServerConfig('dicom_img_base_url')}}}",
-			series : {{$series_list}},
-			control : {
-				window : {
-					panel : false
-				}
-			},
-			elements : {
-				parent : 'page_series_detail',
-				panel : 'the_panel_inner',
-				label : 'the_panel_inner'
-			},
-			control: {
-			      boldness: {
-			        active: false,
-			        value: 1
-			      }, //太さ変更
-			      bucket: {
-			        active: false,
-			        value: 1
-			      }, //太さ変更
-			      measure: {
-			        active: true, //定規機能の有効・無効
-			        panel: true //定規表示パネルの有無
-			      },
-			      color: {
-			        control: false //カラーピッカーの有無
-			      }, 
-			      pan: true, //手のひらツール
-			      window: {
-                    active : true,
-			        panel: false
-			      },
-			      pen: {
-			        active: false, //描画機能の有効・無効
-			        panel: false, //ラベル情報表示パネルの有無
-			      },
-			      show: true, //そもそもコントロールパネルを置くかどうか
-			      undo: false //戻す・やり直す一括
-			},
-			viewer : [
-				{//First sheet
-					elementId : 'img_area_axial',
-					orientation : 'axial',
-					container : voxel_container,
-					number:{
-						maximum : 260, //What sheets cross section is stored
-						current : 0	//Initial display number
-					},
-					window: {
-						level: {current : 1000, maximum : 50000, minimum : -5000},
-						width: {current : 6000, maximum : 9000, 	minimum : 1},
-						preset : [
-							 {label: 'Apply from the source only to axial'	, level: 0000	, width : 2000},
-						]
-					}
-				}
-			]
-		}
-	];
+        var	initInfo	=	[
+            {
+                baseUrl : "{{{Helper\ConfigHelper::getServerConfig('dicom_img_base_url')}}}",
+                series : {{$series_list}},
+                control : {
+                    window : {
+                        panel : false
+                    }
+                },
+                elements : {
+                    parent : 'page_series_detail',
+                    panel : 'the_panel_inner',
+                    label : 'the_panel_inner'
+                },
+                control: {
+                      boldness: {
+                        active: false,
+                        value: 1
+                      }, //太さ変更
+                      bucket: {
+                        active: false,
+                        value: 1
+                      }, //太さ変更
+                      measure: {
+                        active: true,
+                        panel: true
+                      },
+                      color: {
+                        control: false
+                      },
+                      pan: true,
+                      window: {
+                        active : true,
+                        panel: false
+                      },
+                      pen: {
+                        active: false, //描画機能の有効・無効
+                        panel: false, //ラベル情報表示パネルの有無
+                      },
+                      show: true, //そもそもコントロールパネルを置くかどうか
+                      undo: false //戻す・やり直す一括
+                },
+                viewer : [
+                    {//First sheet
+                        elementId : 'img_area_axial',
+                        orientation : 'axial',
+                        container : voxel_container,
+                        number:{
+                            maximum : 260, //What sheets cross section is stored
+                            current : 0	//Initial display number
+                        },
+                        window: {
+                            level: {current : 1000, maximum : 50000, minimum : -5000},
+                            width: {current : 6000, maximum : 9000, minimum : 1},
+                            preset : []
+                        }
+                    }
+                ]
+            }
+        ];
 
-	//accept a series information from node.js by performing a number worth of ajax of series
-	var ajax_cnt = 0;
-	var initAjax= function(){
-		var tmp_series = initInfo[0].series[ajax_cnt];
-		$.ajax({
-			url: '{{{Helper\ConfigHelper::getServerConfig("series_path")}}}',
-			type: 'GET',
-			data: {
-				mode : 'metadata',
-				series : tmp_series.id
-			},//Transmitted data
-			dataType: 'json',
-			error: function(){
-				alert('I failed to communicate');
-			},
-			success: function(response){
+        //accept a series information from node.js by performing a number worth of ajax of series
+        var initAjax= function(){
+            var tmp_series = initInfo[0].series[0];
+            $.ajax({
+                url: '{{{Helper\ConfigHelper::getServerConfig("series_path")}}}',
+                type: 'GET',
+                data: {
+                    mode : 'metadata',
+                    series : tmp_series.id
+                },//Transmitted data
+                dataType: 'json',
+                error: function(){
+                    alert('I failed to communicate');
+                },
+                success: function(response){
 
-				if(typeof response.allow_mode != 'undefined'){
-					tmp_series.allow_mode = 	$.extend(true,tmp_series.allow_mode ,response.allow_mode);
-				}
+                    if(typeof response.allow_mode != 'undefined'){
+                            tmp_series.allow_mode = $.extend(true,tmp_series.allow_mode ,response.allow_mode);
+                        }
 
-				if(typeof tmp_series.voxel != 'object'){
-					tmp_series.voxel =  new Object();
-				}
+                        //set 3D length settings
 
-				if(typeof response.voxel_x == 'number'){
-					tmp_series.voxel.voxel_x = response.voxel_x;
-				};
+                        if(typeof tmp_series.voxel != 'object'){
+                            tmp_series.voxel =  new Object();
+                        }
 
-				if(typeof response.voxel_y == 'number'){
-					tmp_series.voxel.voxel_y = response.voxel_y;
-				};
+                        if(typeof response.voxel_x == 'number'){
+                            tmp_series.voxel.voxel_x = response.voxel_x;
+                        };
 
-				if(typeof response.voxel_z == 'number'){
-					tmp_series.voxel.voxel_z = response.voxel_z;
-				};
+                        if(typeof response.voxel_y == 'number'){
+                            tmp_series.voxel.voxel_y = response.voxel_y;
+                        };
 
-				if(typeof response.x == 'number'){
-					tmp_series.voxel.x = response.x;
-				};
+                        if(typeof response.voxel_z == 'number'){
+                            tmp_series.voxel.voxel_z = response.voxel_z;
+                        };
 
-				if(typeof response.y == 'number'){
-					tmp_series.voxel.y = response.y;
-				};
+                        if(typeof response.x == 'number'){
+                            tmp_series.voxel.x = response.x;
+                        };
 
-				if(typeof response.z == 'number'){
-					tmp_series.voxel.z = response.z;
-				};
+                        if(typeof response.y == 'number'){
+                            tmp_series.voxel.y = response.y;
+                        };
 
-				if(typeof response.window_level == 'number'){
-					tmp_series.window.level.current = response.y;
-				};
+                        if(typeof response.z == 'number'){
+                            tmp_series.voxel.z = response.z;
+                        };
 
-				if(typeof response.window_width == 'number'){
-					tmp_series.window.width.current = response.window_width;
-				};
 
-				if(ajax_cnt==initInfo[0].series.length-1){
-					controllerRun();
-				}else{
-					ajax_cnt++;
-					initAjax();
-				}
-			}
-		});
-	};
-	initAjax();//ajax firing
+                        //set window settings
+                        if(typeof tmp_series.window != 'object'){
+                            tmp_series.window = new Object();
+                        }
 
-	var	controllerRun	=	function(){
-		//Controller issue interlocking series one per one
-		for(var j=0;	j<initInfo.length;	j++){
-			$('#'+initInfo[j].wrapElementId).imageViewerController('init',initInfo[j]);
-		}
-	};
+                        if(typeof tmp_series.window.level != 'object'){
+                            tmp_series.window.level = new Object();
+                        }
 
-	//Button is pressed when you return to the series Search
-	$('#btnBack').click(function() {
-		$('body').append('<form action="./search" method="POST" class="hidden" id="frm_back"></form>');
-		$('#frm_back').append('<input type="hidden" name="btnBack" value="">');
-		$('#frm_back').submit();
-		return false;
-	});
+                        if(typeof tmp_series.window.width != 'object'){
+                            tmp_series.window.width = new Object();
+                        }
 
-	//Case registration button is pressed during
-	$('.link_new_case').click(function() {
-		var COOKIE_NAME = "seriesCookie";
-		if(typeof $.cookie(COOKIE_NAME) === "undefined"){
-			var first_array = [];
-			$.cookie(COOKIE_NAME , first_array , { expires: 1, path:'/' });
-		}
-		var series_num_array = [];
-		var target_number = "{{$series_detail['seriesUID']}}";
-		series_num_array.push(target_number);
-		$.cookie(COOKIE_NAME , series_num_array.join('_') , { expires: 1, path:'/'  });
+                        console.log(tmp_series);
 
-		$('#form_edit_new_case').submit();
-		return false;
-	});
-});
+                        if(typeof response.window_level == 'number'){
+                            tmp_series.window.level.current = response.window_level;
+                        };
+
+                        if(typeof response.window_width == 'number'){
+                            tmp_series.window.width.current = response.window_width;
+                        };
+
+                        tmp_series.window.preset = new Array(0);
+
+                        //set dicom-written-info into the last of Preset array.
+                        if(typeof response.window_level_dicom == 'number' && typeof response.window_width_dicom == 'number'){
+                            var tmp_preset_dicom = {
+                                label: 'DICOM' ,
+                                level: response.window_level_dicom ,
+                                width: response.window_width_dicom
+                            }
+                            tmp_series.window.preset.push(tmp_preset_dicom);
+                        };
+
+                        //set auto-info into the last of Preset array.
+                        if(typeof response.window_level == 'number' && typeof response.window_width == 'number'){
+                            var tmp_preset_auto = {
+                                label: 'AUTO' ,
+                                level: response.window_level ,
+                                width: response.window_width
+                            }
+                            tmp_series.window.preset.push(tmp_preset_auto);
+                        };
+
+                        if(typeof response.window_level_max == 'number'){
+                            tmp_series.window.level.maximum = response.window_level_max;
+                        };
+
+                        if(typeof response.window_level_min == 'number'){
+                            tmp_series.window.level.minimum = response.window_level_min;
+                        };
+
+                        if(typeof response.window_width_max == 'number'){
+                            tmp_series.window.width.maximum = response.window_width_max;
+                        };
+
+                        if(typeof response.window_width_min == 'number'){
+                            tmp_series.window.width.minimum = response.window_width_min;
+                        };
+
+                        controllerRun();
+                }
+            });
+        };
+        initAjax();//ajax firing
+
+        var	controllerRun	=	function(){
+            //Controller issue interlocking series one per one
+            for(var j=0;	j<initInfo.length;	j++){
+                $('#'+initInfo[j].wrapElementId).imageViewerController('init',initInfo[j]);
+            }
+        };
+
+        //Button is pressed when you return to the series Search
+        $('#btnBack').click(function() {
+            $('body').append('<form action="./search" method="POST" class="hidden" id="frm_back"></form>');
+            $('#frm_back').append('<input type="hidden" name="btnBack" value="">');
+            $('#frm_back').submit();
+            return false;
+        });
+
+        //Case registration button is pressed during
+        $('.link_new_case').click(function() {
+            var COOKIE_NAME = "seriesCookie";
+            if(typeof $.cookie(COOKIE_NAME) === "undefined"){
+                var first_array = [];
+                $.cookie(COOKIE_NAME , first_array , { expires: 1, path:'/' });
+            }
+            var series_num_array = [];
+            var target_number = "{{$series_detail['seriesUID']}}";
+            series_num_array.push(target_number);
+            $.cookie(COOKIE_NAME , series_num_array.join('_') , { expires: 1, path:'/'  });
+
+            $('#form_edit_new_case').submit();
+            return false;
+        });
+    });
 
 </script>
 @endif
@@ -219,7 +267,6 @@ id="page_series_detail"
 @if (isset($error_msg))
 	<p class="font_red">{{$error_msg}}</p>
 @else
-
 	<div class="export_area" style="display:none;">
 		<div class="pad_20">
 			@include('series.export')

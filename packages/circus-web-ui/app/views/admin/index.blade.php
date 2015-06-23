b518010a80b927719826f79dd15ac3a48a784631
Soichiro Miki <smiki-tky@umin.ac.jp>
2015-06-23 12:10:54 +0900

    Admin page design enhancement

--- packages/circus-web-ui/app/views/admin/index.blade.php
+++ packages/circus-web-ui/app/views/admin/index.blade.php
@@ -1,22 +1,30 @@
 @extends('common.layout')
 
 @section('page_css')
-{{HTML::style('css/page.css')}}
-{{HTML::style('css/color.css')}}
+<style>
+#admin_menu li {
+	margin-bottom: 8px;
+}
+#admin_menu .common_btn {
+	width: 20em;
+}
+</style>
 @stop
 
 @section('title')
-Administrator
+Administration
 @stop
 
 @section('page_title')
-Administrator
+Administration
 @stop
 
 @section('content')
-{{HTML::link(asset('administration/group'), 'Group List', array('class' => 'common_btn'))}}
-{{HTML::link(asset('administration/user'), 'User List', array('class' => 'common_btn'))}}
-{{HTML::link(asset('administration/storage'), 'Storage List', array('class' => 'common_btn'))}}
-{{HTML::link(asset('administration/project'), 'Project List', array('class' => 'common_btn'))}}
-{{HTML::link(asset('administration/server_param'), 'Setting Server Params', array('class' => 'common_btn'))}}
+<ul id="admin_menu">
+	<li>{{HTML::link(asset('administration/server_param'), 'Server Configuration', array('class' => 'common_btn'))}}</li>
+	<li>{{HTML::link(asset('administration/group'), 'Groups', array('class' => 'common_btn'))}}</li>
+	<li>{{HTML::link(asset('administration/user'), 'Users', array('class' => 'common_btn'))}}</li>
+	<li>{{HTML::link(asset('administration/storage'), 'Storage', array('class' => 'common_btn'))}}</li>
+	<li>{{HTML::link(asset('administration/project'), 'Projects', array('class' => 'common_btn'))}}</li>
+</ul>
 @stop

369acd83b55a9304cc4c606734f28609e7bc2df3
Sayuri Tani <stani@spiritek.co.jp>
2015-08-21 17:34:23 +0900

    fixed bug

--- packages/circus-web-ui/app/views/case/tag.blade.php
+++ packages/circus-web-ui/app/views/case/tag.blade.php
@@ -1,6 +1,6 @@
-@if ($prj_tags && count($case_tags) > 0)
+@if ($prj_tags)
 	@foreach ($prj_tags as $idx => $prj_tag)
-		{{Form::checkbox('tags', $idx, (array_search($idx, $case_tags) !== false), array('id' => $tag_caseID.'_'.$idx, 'class' => 'select_tags'))}}
+		{{Form::checkbox('tags', $idx, ($case_tags && array_search($idx, $case_tags) !== false), array('id' => $tag_caseID.'_'.$idx, 'class' => 'select_tags'))}}
 		<script>renderTag("{{$prj_tag['name']}}", "{{$prj_tag['color']}}", "{{{$tag_caseID.'_'.$idx}}}");</script>
 	@endforeach
 @endif

c653eb165fa77393087484024c268606dfa4638f
Soichiro Miki <smiki-tky@umin.ac.jp>
2015-08-27 01:07:58 +0900

    Rewrite server params page.

--- packages/circus-web-ui/app/models/ServerParam.php
+++ packages/circus-web-ui/app/models/ServerParam.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Model class for ServerParams.
+ * Model class for general server configurations stored in the database.
  * @property string key
  * @property mixed value
  */
@@ -13,45 +13,45 @@ class ServerParam extends BaseModel
 	protected $collection = self::COLLECTION;
 	protected $primaryKey = 'key';
 
-
-	public static function getVal($key){
+	public static function getVal($key)
+	{
 		$obj = self::find($key);
 		if ($obj)
 			return $obj->value;
 		return null;
 	}
 
-	public static function setVal($key, $value) {
-		if ($value === null) $value = '';
-		$obj = new ServerParam();
-		$obj->key = $key;
-		$obj->value = $value;
-		$obj->save();
+	public static function setVal($key, $value)
+	{
+		DB::collection(self::COLLECTION)
+			->where('key', $key)
+			->update(
+				['$set' => ['value' => $value]],
+				array('upsert' => true)
+			);
 	}
 
 	protected $rules = array(
-		'key'           => 'required',
-		'value'         => 'required',
-		'createTime'    => 'mongodate',
-		'updateTime'    => 'mongodate'
+		'key' => 'required',
+		'value' => 'required',
+		'createTime' => 'mongodate',
+		'updateTime' => 'mongodate'
 	);
 
-	public static function getDomainList() {
+	public static function getDomainList()
+	{
 		$domain_list = array();
 
-		//get default domain
+		// get default domain
 		$default_domain = self::getVal('defaultDomain');
 		if ($default_domain)
 			$domain_list[$default_domain] = $default_domain;
 
-		//selectable domain
+		// selectable domain
 		$domains = self::getVal('domains');
-
-		if ($domains) {
-			$domains = array_values(json_decode($domains, true));
-			foreach($domains as $domain) {
-				$domain_list[$domain] = $domain;
-			}
+		if (!is_array($domains)) $domains = [];
+		foreach ($domains as $domain) {
+			$domain_list[$domain] = $domain;
 		}
 
 		return $domain_list;
--- packages/circus-web-ui/app/views/admin/server_param.blade.php
+++ packages/circus-web-ui/app/views/admin/server_param.blade.php
@@ -1,28 +1,83 @@
-@extends('admin.base')
-@section('admin-content')
-    <script>
-        $(function () {
-            adminEditor.run({
-                resource: 'serverParam',
-                primaryKey: 'key',
-                captionKey: 'key',
-                listColumns: [
-                    {key: 'key', label: 'key'},
-                    {key: 'value', label: 'value'},
-                ],
-                form: [
-                    {
-                        caption: 'Key',
-                        key: 'key',
-                        type: 'text'
-                    },
-                    {
-                    	caption: 'Value',
-                        key: 'value',
-                        type: 'text'
-                    }
-                ]
-            });
-        });
-    </script>
+@extends('common.layout')
+
+@section('title')
+	General Server Configuration
+@stop
+
+@section('head')
+	{{HTML::style('css/jquery-ui.css')}}
+	{{HTML::style('css/jquery.flexforms.css')}}
+	<style>
+		.ui-propertyeditor-row > td {
+			padding-bottom: 1em;
+			width: 500px;
+		}
+
+		.ui-propertyeditor-heading > th {
+			padding-top: 1em;
+			border-bottom: 1px solid silver;
+		}
+	</style>
+
+	{{HTML::script('js/jquery-ui.min.js')}}
+	{{HTML::script('js/jquery.flexforms.js')}}
+
+	<script>
+		$(function () {
+			var editor = $('#editor').propertyeditor({
+				properties: [
+					{
+						key: 'domains',
+						caption: 'Domains',
+						type: 'list',
+						spec: {
+							elementType: 'text',
+							elementSpec: {
+								regex: /^[_a-zA-Z][_a-zA-Z0-9\-]*$/
+							}
+						}
+					}
+				]
+			});
+
+			$('#cancel').on('click', refresh);
+			refresh();
+
+			function refresh() {
+				$.ajax({
+					url: '/api/server_param',
+					success: function (data) {
+						editor.propertyeditor('option', 'value', data);
+					},
+					dataType: 'json',
+					cached: false
+				});
+			}
+
+			$('#save').on('click', function() {
+				$.ajax({
+					url: '/api/server_param',
+					method: 'POST',
+					data: editor.propertyeditor('option', 'value'),
+					dataType: 'json',
+					success: function (data) {
+						alert('Saved.');
+						refresh();
+					},
+					error: function (err) { alert(err); },
+					cached: false
+				});
+			});
+
+		});
+	</script>
+
+@stop
+
+@section('content')
+	<div id="editor"></div>
+	<div class="al_c">
+		<button id="cancel" class="common_btn">Cancel</button>
+		<button id="save" class="common_btn">Save</button>
+	</div>
 @stop

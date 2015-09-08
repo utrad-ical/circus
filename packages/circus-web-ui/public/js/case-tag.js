2cc91578a141af9be4d1c4998ae05ada3e47f93e
Soichiro Miki <smiki-tky@umin.ac.jp>
2015-09-08 09:44:58 +0900

    Partial implementation of tag editor/selector

--- packages/circus-web-ui/public/js/case-tag.js
+++ packages/circus-web-ui/public/js/case-tag.js
@@ -1,8 +1,10 @@
 /* Include this JS to use the tag related features. */
 /* Requires common.js color picker. */
 
-var tag;
+var tag = {};
 (function () {
+	var projectTagList = {};
+
 	// http://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
 	function lumiComp(hex) {
 		var intensity = parseInt(hex, 16) / 255;
@@ -31,7 +33,82 @@ var tag;
 		});
 	}
 
+	/**
+	 * Registers the project tag list.
+	 * @param list { projectID[string]: TagInfo[] }
+	 */
+	function registerProjectTags(list) {
+		if (typeof list !== 'object') return;
+		projectTagList = $.extend(projectTagList, list);
+	}
+	tag.registerProjectTags = registerProjectTags;
+
+	/**
+	 * When this module doesn't know the tag list,
+	 * ask it to the server.
+	 * It is recommended to register such list beforehands
+	 * to reduce the number of connections.
+	 * @param projectID
+	 * @param callback
+	 */
+	function fetchProjectTags(projectID, callback) {
+		api('project/' + projectID, {
+			success: function(data) {
+				var tmp = {};
+				tmp[projectID] = data.tags || [];
+				registerProjectTags(tmp);
+				callback();
+			}
+		});
+	}
+
+	/**
+	 * Opens tag selection dialog.
+	 * @param projectID Target project ID.
+	 * @param current string[] Currently selected tags.
+	 */
+	function tagSelector(projectID, current, callback) {
+		if (!(projectID in projectTagList)) {
+			fetchProjectTags(projectID, function() {
+				tagSelector(projectID, current, callback);
+			})
+			return;
+		}
+
+		var projectTags = projectTagList[projectID];
+		if (!$.isArray(current)) current = [];
+
+		var dialog = $('<div>')
+			.attr('title', 'Select Tags')
+			.addClass('balloon');
+		var list = $('<ul>').appendTo(dialog);
+		projectTags.forEach(function(tag) {
+			var li = $('<li>')
+				.addClass('selectable')
+				.appendTo(list);
+			$('<span>')
+				.text(tag.name)
+				.tag(tag.color)
+				.appendTo(li);
+		});
+		dialog.dialog({
+			dialogClass: 'tag-selector',
+			buttons: {
+				Cancel: function() {
+					dialog.dialog('close');
+				},
+				OK: function() {
+					dialog.dialog('close');
+					var selected = [];
+					$('input:checked', list).each(function() {
+						selected.push($(this).data('tag'));
+					})
+					callback(selected);
+				}
+			}
+		});
+	}
+	tag.tagSelector = tagSelector;
+
 	$.fn.extend({tag: renderTag});
 })(tag);
-
-

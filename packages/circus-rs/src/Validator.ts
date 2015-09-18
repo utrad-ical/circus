12dc60993338dad8b3e7da4f7634af05e635dade
Nobuyuki Fujita <nfujita@spiritek.co.jp>
2015-09-18 18:12:56 +0900

    fixed: mismatch argument when calling 'function' type validator .

--- packages/circus-rs/src/Validator.ts
+++ packages/circus-rs/src/Validator.ts
@@ -58,7 +58,7 @@ export class Validator {
 				});
 			} else if (typeof rule === 'function') {
 				// The rule is checked by the given function
-				ok = (<Function><any>rule)(this, input[key]);
+				ok = (<Function><any>rule)(input[key]);
 			} else if (rule instanceof RegExp) {
 				// The rule is checked with the given regexp
 				ok = rule.test(input[key]);

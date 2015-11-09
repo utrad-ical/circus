4baa011d156450c2e2b71fc4e1e97579faabcf55
Soichiro Miki <smiki-tky@umin.ac.jp>
2015-11-10 00:41:55 +0900

    Fix MockDicomDumper

--- packages/circus-rs/src/dicom-dumpers/MockDicomDumper.ts
+++ packages/circus-rs/src/dicom-dumpers/MockDicomDumper.ts
@@ -11,7 +11,7 @@ import * as Promise from 'bluebird';
 export default class MockDicomDumper extends DicomDumper {
 
 	public readDicom(dcmdir: string): Promise<DicomVolume> {
-		var vol = this.makeMockVol();
+		let vol = this.makeMockVol();
 		return Promise.resolve(vol);
 	}
 
@@ -19,23 +19,23 @@ export default class MockDicomDumper extends DicomDumper {
 	 * Buffer data: block data in dcm_voxel_dump combined format
 	 */
 	public makeMockVol(): DicomVolume {
-		var raw = new DicomVolume();
+		let raw = new DicomVolume();
 		raw.appendHeader({
 			Dummy: 'Header'
 		});
-		var d = ('d' in this.config) ? this.config.d : 128;
+		let d = ('d' in this.config) ? this.config.d : 128;
 		raw.setDimension(512, 512, d, PixelFormat.Int16);
 		for (var z = 0; z < d; z++) {
-			var sliceData = new Uint8Array(512 * 512 * 2);
-			for (var x = 0; x < 512; x++) {
-				for (var y = 0; y < 512; y++) {
-					var val = ( Math.floor(x * 0.02)
+			let sliceData = new Int16Array(512 * 512);
+			for (let x = 0; x < 512; x++) {
+				for (let y = 0; y < 512; y++) {
+					let val = ( Math.floor(x * 0.02)
 						+ Math.floor(y * 0.02)
 						+ Math.floor(z * 0.02) ) % 3 * 30;
-					sliceData[(x + y * 512) * 2] = val;
+					sliceData[x + y * 512] = val;
 				}
 			}
-			raw.insertSingleImage(z, sliceData);
+			raw.insertSingleImage(z, sliceData.buffer);
 		}
 		raw.setVoxelDimension(0.5, 0.5, 0.5);
 		raw.setEstimatedWindow(10, 100);

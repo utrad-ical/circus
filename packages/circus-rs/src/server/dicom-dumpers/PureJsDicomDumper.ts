2696867730708d808b93421261ccd9ddc56422c6
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-02-20 23:21:24 +0900

    Remove clumsy usage of Promise

--- packages/circus-rs/src/server/dicom-dumpers/PureJsDicomDumper.ts
+++ packages/circus-rs/src/server/dicom-dumpers/PureJsDicomDumper.ts
@@ -48,71 +48,67 @@ export default class PureJsDicomDumper extends DicomDumper {
 	}
 
 	public readDicom(dcmdir: string): Promise<DicomVolume> {
-		return new Promise<DicomVolume>((resolve: (RawData) => void, reject) => {
-			let raw = new DicomVolume();
-			let lastSliceLocation: number;
-			let pitch: number = undefined;
-			let seriesMinValue: number = null;
-			let seriesMaxValue: number = null;
-			raw.setEstimatedWindow(50, 75);
+		let raw = new DicomVolume();
+		let lastSliceLocation: number;
+		let pitch: number = undefined;
+		let seriesMinValue: number = null;
+		let seriesMaxValue: number = null;
+		raw.setEstimatedWindow(50, 75);
 
-			this.scanDicomCount(dcmdir).then(count => {
-				let loader: Promise<any> = Promise.resolve(null);
-				for (let i = 1; i <= count; i++) {
-					// TODO: Remove this closure after TS1.8 release
-					// https://github.com/Microsoft/TypeScript/issues/3915
-					(i => {
-						loader = loader.then(() => {
-							// logger.debug(`reading ${i}`);
-							return this.readSingleDicomImageFile(`${dcmdir}/${this.pad8(i)}.dcm`);
-						}).then(result => {
-							// logger.debug(`inserting ${i} with ${result.pixelData.byteLength} bytes of data`);
-							if (i === 1) {
-								if ('x00180088' in result.dataset.elements) {
-									// [0018, 0088] Spacing between slices
-									pitch = result.dataset.floatString('x00180088');
-									raw.setVoxelDimension(result.pixelSpacing[0], result.pixelSpacing[1], pitch);
-								}
-								raw.appendHeader({
-									modality: result.modality,
-									rescaleSlope: result.rescale.slope,
-									rescaleIntercept: result.rescale.intercept
-								});
-								raw.dcm_wl = result.window.level;
-								raw.dcm_ww = result.window.width;
-								raw.setDimension(result.columns, result.rows, count, result.pixelFormat);
-							} else if (i > 1 && pitch === undefined) {
-								pitch = Math.abs(lastSliceLocation - result.sliceLocation);
+		return this.scanDicomCount(dcmdir).then(count => {
+			let loader: Promise<any> = Promise.resolve(null);
+			for (let i = 1; i <= count; i++) {
+				// TODO: Remove this closure after TS1.8 release
+				// https://github.com/Microsoft/TypeScript/issues/3915
+				(i => {
+					loader = loader.then(() => {
+						// logger.debug(`reading ${i}`);
+						return this.readSingleDicomImageFile(`${dcmdir}/${this.pad8(i)}.dcm`);
+					}).then(result => {
+						// logger.debug(`inserting ${i} with ${result.pixelData.byteLength} bytes of data`);
+						if (i === 1) {
+							if ('x00180088' in result.dataset.elements) {
+								// [0018, 0088] Spacing between slices
+								pitch = result.dataset.floatString('x00180088');
 								raw.setVoxelDimension(result.pixelSpacing[0], result.pixelSpacing[1], pitch);
 							}
-							seriesMinValue = Math.min(seriesMinValue, result.minValue);
-							seriesMaxValue = Math.max(seriesMinValue, result.maxValue);
-							lastSliceLocation = result.sliceLocation;
-							raw.insertSingleImage(i - 1, result.pixelData);
-							return true;
-						});
-					})(i);
-				}
-				return loader;
-			}).then(() => {
-				if (raw.getHeader('modality') === 'CT') {
-					let slope = raw.getHeader('rescaleSlope');
-					let intercept = raw.getHeader('rescaleIntercept');
-					// logger.debug(`Apply rescale: slope=${slope}, intercept=${intercept}`);
-					raw.convert(PixelFormat.Int16, originalValue => {
-						return originalValue * slope + intercept;
+							raw.appendHeader({
+								modality: result.modality,
+								rescaleSlope: result.rescale.slope,
+								rescaleIntercept: result.rescale.intercept
+							});
+							raw.dcm_wl = result.window.level;
+							raw.dcm_ww = result.window.width;
+							raw.setDimension(result.columns, result.rows, count, result.pixelFormat);
+						} else if (i > 1 && pitch === undefined) {
+							pitch = Math.abs(lastSliceLocation - result.sliceLocation);
+							raw.setVoxelDimension(result.pixelSpacing[0], result.pixelSpacing[1], pitch);
+						}
+						seriesMinValue = Math.min(seriesMinValue, result.minValue);
+						seriesMaxValue = Math.max(seriesMinValue, result.maxValue);
+						lastSliceLocation = result.sliceLocation;
+						raw.insertSingleImage(i - 1, result.pixelData);
+						return true;
 					});
-					seriesMinValue = seriesMinValue * slope + intercept;
-					seriesMaxValue = seriesMaxValue * slope + intercept;
-				}
-				// Specific to CIRCUS RS: Apply rescale only when the modality is CT
-				let estimatedWidth = Math.floor(seriesMaxValue - seriesMinValue + 1);
-				let estimatedLevel = Math.floor(seriesMinValue + estimatedWidth / 2);
-				raw.setEstimatedWindow(estimatedLevel, estimatedWidth);
-				resolve(raw);
-			}).catch(err => {
-				reject(err);
-			});
+				})(i);
+			}
+			return loader;
+		}).then(() => {
+			if (raw.getHeader('modality') === 'CT') {
+				let slope = raw.getHeader('rescaleSlope');
+				let intercept = raw.getHeader('rescaleIntercept');
+				// logger.debug(`Apply rescale: slope=${slope}, intercept=${intercept}`);
+				raw.convert(PixelFormat.Int16, originalValue => {
+					return originalValue * slope + intercept;
+				});
+				seriesMinValue = seriesMinValue * slope + intercept;
+				seriesMaxValue = seriesMaxValue * slope + intercept;
+			}
+			// Specific to CIRCUS RS: Apply rescale only when the modality is CT
+			let estimatedWidth = Math.floor(seriesMaxValue - seriesMinValue + 1);
+			let estimatedLevel = Math.floor(seriesMinValue + estimatedWidth / 2);
+			raw.setEstimatedWindow(estimatedLevel, estimatedWidth);
+			return raw;
 		});
 	}
 }

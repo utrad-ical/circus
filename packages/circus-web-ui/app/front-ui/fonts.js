f258e700c778af99cdb0823e91c3f388ffa44718
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-05-17 14:47:24 +0900

    Remove gulp and introduce Webpack.
    Currently, it only compiles Web fonts and LESS files.
    
    The CSS rules emitted by fontgen-loader is currently discarded.

---
+++ packages/circus-web-ui/app/front-ui/fonts.js
@@ -0,0 +1,8 @@
+module.exports = {
+	files: [ '../assets/icons/*.svg' ],
+	fontName: 'css/fonts/circus-db-font',
+	classPrefix: 'cicrus-icon-',
+	baseClass: 'circus-icon',
+	fixedWidth: true,
+	types: ['woff']
+};

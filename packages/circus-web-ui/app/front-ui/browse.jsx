99fc68d50b4bcbee3d19c69a25bc0ced34180849
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-05-25 22:08:48 +0900

    Add server control page

--- packages/circus-web-ui/app/front-ui/browse.jsx
+++ packages/circus-web-ui/app/front-ui/browse.jsx
@@ -7,6 +7,7 @@ import { App } from './components/app.jsx';
 import { SeriesSearch } from './components/search-series.jsx';
 import { CaseSearch } from './components/search-case.jsx';
 import { ImportSeries } from './components/import-series.jsx';
+import { DicomImageServerAdmin } from './components/admin/dicom-image-server-admin.jsx';
 import * as modal from './components/modal.jsx';
 
 require('style!css!./bs-style/bootstrap.less');
@@ -16,9 +17,12 @@ function render() {
 	ReactDOM.render(
 		<Router history={browserHistory}>
 			<Route path='/' component={App}>
-				<Route path='/browse/series' component={SeriesSearch} />
-				<Route path='/browse/case' component={CaseSearch} />
-				<Route path='/import-series' component={ImportSeries} />
+				<Route path='browse/series' component={SeriesSearch} />
+				<Route path='browse/case' component={CaseSearch} />
+				<Route path='import-series' component={ImportSeries} />
+				<Route path='admin'>
+					<Route path='server' component={DicomImageServerAdmin} />
+				</Route>
 			</Route>
 		</Router>,
 		document.getElementById('app')

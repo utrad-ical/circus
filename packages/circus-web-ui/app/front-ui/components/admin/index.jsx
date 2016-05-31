4b493f2c625f5a89b4ca1685604d567cdb79449f
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-05-31 11:51:58 +0900

    Create admin index page

---
+++ packages/circus-web-ui/app/front-ui/components/admin/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { browserHistory } from 'react-router';
+import { ButtonGroup, Button, Glyphicon } from '../react-bootstrap';
+
+export const AdminIndex = props => {
+	const btn = (to, link, glyph) => (
+		<Button bsStyle="primary"
+			onClick={() => browserHistory.push('admin/' + to)}
+		>
+			{glyph ?
+				<span><Glyphicon glyph={glyph} />&ensp;</span>
+			: null}
+			{link}
+		</Button>
+	);
+
+	return <div>
+		<h1>Administration</h1>
+		<ButtonGroup vertical>
+			{btn('global', 'Server Configuration', 'tasks')}
+			{btn('server', 'DICOM Image Server', 'hdd')}
+			{btn('storage', 'Storage', 'save-file')}
+			{btn('group', 'Groups', 'record')}
+			{btn('user', 'Users', 'user')}
+			{btn('project', 'Projects', 'education')}
+		</ButtonGroup>
+	</div>;
+}

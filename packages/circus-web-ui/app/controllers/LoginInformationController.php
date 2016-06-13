7f4b9c609170f44066fa8bb4e275d3fcbd84a6df
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-06-14 01:43:07 +0900

    Show more info on profile screen

--- packages/circus-web-ui/app/controllers/LoginInformationController.php
+++ packages/circus-web-ui/app/controllers/LoginInformationController.php
@@ -19,6 +19,7 @@ class LoginInformationController extends ApiBaseController {
 				'lastLoginIP']
 		);
 
+		// Build project information
 		$projects = $user->listAccessibleProjectsWithRoles();
 		foreach ($projects as &$entry) {
 			$entry['project'] = array_only(
@@ -30,9 +31,16 @@ class LoginInformationController extends ApiBaseController {
 			);
 		}
 
+		// Build group information
+		$groups = [];
+		foreach ($user->groups as $group_id) {
+			$groups[] = Group::findOrFail($group_id)->groupName;
+		}
+
 		$user_data = array_merge($user_data, [
 			'lastLoginTime' => date('c', $user->lastLoginTime->sec),
 			'privileges' => $user->listPrivileges(),
+			'groups' => $groups,
 			'uploadFileSizeMax' => ini_get('upload_max_filesize'),
 			'uploadFileMax' => intval(ini_get('max_file_uploads')),
 			'dicomImageServer' => 'http://' . $_SERVER['SERVER_NAME'] . ':3000/',

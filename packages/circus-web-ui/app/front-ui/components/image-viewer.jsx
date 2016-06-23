c4d09bdafcbf2ac1b69f92a21bd3096874588a41
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-06-24 00:33:22 +0900

    Update CIRCUS RS and confirm it can be webpack-ed with the main app

--- packages/circus-web-ui/app/front-ui/components/image-viewer.jsx
+++ packages/circus-web-ui/app/front-ui/components/image-viewer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import * as rs from 'circus-rs';
 
 export class ImageViewer extends React.Component {
 	render() {
+		console.log(rs);
 		return <div style={{backgroundColor: 'black', width: 512, height: 512}}>
 		</div>;
 	}

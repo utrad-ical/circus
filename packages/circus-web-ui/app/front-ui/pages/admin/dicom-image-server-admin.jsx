0c15e39b008f9847ac574f862303a36e25a1db65
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-07-14 15:43:24 +0900

    Add hidden configuration to start RS server without authorization

--- packages/circus-web-ui/app/front-ui/pages/admin/dicom-image-server-admin.jsx
+++ packages/circus-web-ui/app/front-ui/pages/admin/dicom-image-server-admin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Panel, Button, ButtonToolbar, Glyphicon } from 'components/react-bootstrap';
+import { Alert, Panel, Button, ButtonToolbar, Glyphicon } from 'components/react-bootstrap';
 import { Loading } from 'components/loading';
 import { api } from 'utils/api';
 import axios from 'axios';
@@ -18,12 +18,15 @@ export class DicomImageServerAdmin extends React.Component {
 	async sendControl(command) {
 		if (this.state.isFetching) return;
 		const dicomImageServer = store.getState().loginUser.data.dicomImageServer;
-		this.setState({ isFetching: true });
+		this.setState({ isFetching: true, noAuthorizationWarning: false });
 		const data = await api('server/' + command, { method: 'post' });
 		this.setState({ processStatus: data[0] });
 		try {
-			const status = await axios.get(dicomImageServer + '/status');
-			this.setState({ rsStatus: JSON.stringify(status.data, null, '    ') });
+			const status = (await axios.get(dicomImageServer + '/status')).data;
+			this.setState({
+				rsStatus: JSON.stringify(status, null, '    '),
+				noAuthorizationWarning: status.authorization && status.authorization.enabled === false
+			});
 		} catch(err) {
 			let message = 'The server did not respond. It may not be running.';
 			if (typeof err.state === 'number') {
@@ -73,6 +76,12 @@ export class DicomImageServerAdmin extends React.Component {
 				<pre>{this.state.processStatus}</pre>
 				<p>CIRCUS RS Status:</p>
 				<pre>{this.state.rsStatus}</pre>
+				{ this.state.noAuthorizationWarning ?
+					<Alert bsStyle="danger">
+						<strong>RS Server running without authorization enabled!</strong>&ensp;
+						This can be a security risk. Use this only for debugging purposes.
+					</Alert>
+				: null }
 			</div>
 		</div>;
 	}

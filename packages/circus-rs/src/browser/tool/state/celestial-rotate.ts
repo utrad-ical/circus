f2b07917207c6b3668344a3880ae73c1fdb61fe6
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-07-30 17:22:31 +0900

    Fix runtime error in CelestialRotateTool

--- packages/circus-rs/src/browser/tool/state/celestial-rotate.ts
+++ packages/circus-rs/src/browser/tool/state/celestial-rotate.ts
@@ -15,9 +15,8 @@ export class CelestialRotateTool extends DraggableTool implements ViewerEventTar
 	}
 
 	public dragMoveHandler(ev: ViewerEvent, dragInfo): void {
-
 		if (Math.abs(dragInfo.dx) && Math.abs(dragInfo.dx) >= Math.abs(dragInfo.dy)) {
-			let hdeg = ( dragInfo.dx < 0 ? -1 : 1  ) * ( ev.original.ctrlKey ? 2 : 1 );
+			const hdeg = ( dragInfo.dx < 0 ? -1 : 1  ) * ( ev.original.ctrlKey ? 2 : 1 );
 			this.horizontalRotate(ev.viewer, hdeg);
 			ev.viewer.render();
 			ev.stopPropagation();
@@ -30,26 +29,23 @@ export class CelestialRotateTool extends DraggableTool implements ViewerEventTar
 	}
 
 	public mouseWheelHandler(ev: ViewerEvent): void {
-		let vdeg = ( ev.original.deltaY < 0 ? -1 : 1  ) * ( ev.original.ctrlKey ? 2 : 1 );
+		const vdeg = ( ev.original.deltaY < 0 ? -1 : 1  ) * ( ev.original.ctrlKey ? 2 : 1 );
 		this.verticalRotate(ev.viewer, vdeg);
 		ev.viewer.render();
 		ev.stopPropagation();
 	}
 
 	public horizontalRotate(viewer: Viewer, deg: number) {
-
-		let state = viewer.getState();
-		let [ vw, vh ] = state.resolution;
-
-		let radian = Math.PI / 180.0 * deg;
-
-		let end0 = state.section.xAxis.concat();
+		const state = viewer.getState();
+		const [vw, vh] = viewer.getResolution();
+		const radian = Math.PI / 180.0 * deg;
+		const end0 = state.section.xAxis.concat();
 
 		// rotate
-		let transform = mat4.rotate(mat4.create(), mat4.create(), radian, state.section.yAxis);
+		const transform = mat4.rotate(mat4.create(), mat4.create(), radian, state.section.yAxis);
 		vec3.transformMat4(state.section.xAxis, state.section.xAxis, transform);
 
-		let end1 = state.section.xAxis.concat();
+		const end1 = state.section.xAxis.concat();
 
 		state.section.origin[0] -= ( end1[0] - end0[0] ) / 2;
 		state.section.origin[1] -= ( end1[1] - end0[1] ) / 2;
@@ -59,19 +55,16 @@ export class CelestialRotateTool extends DraggableTool implements ViewerEventTar
 	}
 
 	public verticalRotate(viewer: Viewer, deg: number) {
-
-		let state = viewer.getState();
-		let [ vw, vh ] = state.resolution;
-
-		let radian = Math.PI / 180.0 * deg;
-
-		let end0 = state.section.yAxis.concat();
+		const state = viewer.getState();
+		const [vw, vh] = viewer.getResolution();
+		const radian = Math.PI / 180.0 * deg;
+		const end0 = state.section.yAxis.concat();
 
 		// rotate
-		let transform = mat4.rotate(mat4.create(), mat4.create(), radian, state.section.xAxis);
+		const transform = mat4.rotate(mat4.create(), mat4.create(), radian, state.section.xAxis);
 		vec3.transformMat4(state.section.yAxis, state.section.yAxis, transform);
 
-		let end1 = state.section.yAxis.concat();
+		const end1 = state.section.yAxis.concat();
 
 		state.section.origin[0] -= ( end1[0] - end0[0] ) / 2;
 		state.section.origin[1] -= ( end1[1] - end0[1] ) / 2;

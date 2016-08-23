2b9b0b1828cd114b76d3f8f6b3951e4bf87c3a08
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-08-23 18:26:34 +0900

    Clean up WindowTool

--- packages/circus-rs/src/browser/tool/state/window.ts
+++ packages/circus-rs/src/browser/tool/state/window.ts
@@ -1,29 +1,20 @@
 import { DraggableTool } from '../../../browser/tool/draggable';
-import { Viewer } from '../../../browser/viewer/viewer';
-import { ViewState } from '../../view-state';
 import { ViewerEvent } from '../../../browser/viewer/viewer-event';
-import { ViewerEventTarget } from '../../../browser/interface/viewer-event-target';
 
-export class WindowTool extends DraggableTool implements ViewerEventTarget {
+/**
+ * WindowTool handles mouse drag and modifies the window level/width accordingly.
+ */
+export class WindowTool extends DraggableTool {
 
 	public dragHandler(ev: ViewerEvent): void {
 		super.dragHandler(ev);
 		const dragInfo = this.dragInfo;
 		const viewState = ev.viewer.getState();
-		this.windowWidthBy(viewState, dragInfo.dx * ( ev.original.ctrlKey ? 20 : 5 ));
-		this.windowLevelBy(viewState, dragInfo.dy * ( ev.original.ctrlKey ? 2 : 1 ));
+		const speed = ev.original.ctrlKey ? 5 : 1;
+		viewState.window.level += dragInfo.dy * speed;
+		viewState.window.width += dragInfo.dx * speed;
+		if (viewState.window.width < 1) viewState.window.width = 1;
 		ev.viewer.setState(viewState);
 	}
 
-	/**
-	 * window level / width
-	 */
-	public windowLevelBy(state: ViewState, dl: number) {
-		state.window.level += dl;
-	}
-
-	public windowWidthBy(state: ViewState, dw: number) {
-		state.window.width += dw;
-		if (state.window.width < 1) state.window.width = 1;
-	}
 }

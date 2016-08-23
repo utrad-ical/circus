425264143b83867ca57c88df9e26a0fc144b225e
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-08-23 20:16:39 +0900

    Comment

--- packages/circus-rs/src/browser/tool/draggable.ts
+++ packages/circus-rs/src/browser/tool/draggable.ts
@@ -5,19 +5,37 @@ import { ViewerEvent } from '../../browser/viewer/viewer-event';
  * DragInfo holds convenient data while dragging.
  */
 export interface DragInfo {
+	/**
+	 * The mouse travel distance since the previous drag event.
+	 */
 	dx: number;
+
+	/**
+	 * The mouse travel distance since the previous drag event.
+	 */
 	dy: number;
+
+	/**
+	 * The total mouse travel distance since the mouse was pressed.
+	 */
 	totalDx: number;
+
+	/**
+	 * The total mouse travel distance since the mouse was pressed.
+	 */
 	totalDy: number;
 }
 
 /**
- * DraggableTool is a base class for tools that handles drag and drop.
+ * DraggableTool is a base class for tools that handles mouse dragging over the Viewer.
  * This class manages a protected member "dragInfo", which holds handy values
  * such as the total mouse move distance from the drag start point.
  */
 export abstract class DraggableTool extends Tool {
 
+	/**
+	 * Holds some useful drag-related variables.
+	 */
 	protected dragInfo: DragInfo;
 
 	private prevX: number;
@@ -43,7 +61,7 @@ export abstract class DraggableTool extends Tool {
 
 	private updateInfo(ev: ViewerEvent): void {
 		// Hack: For some reason, drag events can be called with zero screenX/Y values
-		// when the mouse is released. We ignore such events altogether.
+		// when the mouse is released. We ignore such events by setting dx and dy to zero.
 		if (ev.original.screenX === 0 && ev.original.screenY === 0) {
 			this.dragInfo.dx = 0;
 			this.dragInfo.dy = 0;

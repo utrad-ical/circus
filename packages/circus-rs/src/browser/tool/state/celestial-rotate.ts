b5009c17faf099b3e3f917731cfe0203c75977fc
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-08-24 12:01:51 +0900

    Change the behavior of CelestialRotateTool.
    Closes #30

--- packages/circus-rs/src/browser/tool/state/celestial-rotate.ts
+++ packages/circus-rs/src/browser/tool/state/celestial-rotate.ts
@@ -1,7 +1,7 @@
 import { mat4, vec3 } from 'gl-matrix';
 import { DraggableTool } from '../draggable';
-import { Viewer } from '../../viewer/viewer';
 import { ViewerEvent } from '../../viewer/viewer-event';
+import { Section } from '../../section';
 
 /**
  * CelestialRotateTool handles mouse drags and wheel moves on the Viewer and
@@ -12,50 +12,53 @@ export class CelestialRotateTool extends DraggableTool {
 	public dragHandler(ev: ViewerEvent): void {
 		super.dragHandler(ev);
 		const dragInfo = this.dragInfo;
-		if (Math.abs(dragInfo.dx) && Math.abs(dragInfo.dx) >= Math.abs(dragInfo.dy)) {
-			const hdeg = ( dragInfo.dx < 0 ? -1 : 1  ) * ( ev.original.ctrlKey ? 2 : 1 );
-			this.horizontalRotate(ev.viewer, hdeg);
+		const speed = ev.original.ctrlKey ? 3 : 1;
+
+		if (dragInfo.dx === 0 && dragInfo.dy === 0) return;
+
+		const state = ev.viewer.getState();
+		if (Math.abs(dragInfo.dx)) {
+			const deg = this.sign(dragInfo.dx) * speed;
+			state.section = this.rotateAroundYAxis(state.section, deg);
 		}
+		if (Math.abs(dragInfo.dx)) {
+			const deg = this.sign(dragInfo.dy) * speed;
+			state.section = this.rotateAroundXAxis(state.section, deg);
+		}
+		ev.viewer.setState(state);
 	}
 
-	public wheelHandler(ev: ViewerEvent): void {
-		const vdeg = ( ev.original.deltaY < 0 ? -1 : 1  ) * ( ev.original.ctrlKey ? 2 : 1 );
-		this.verticalRotate(ev.viewer, vdeg);
-	}
-
-	public horizontalRotate(viewer: Viewer, deg: number) {
-		const state = viewer.getState();
+	private rotateAroundYAxis(section: Section, deg: number): Section {
 		const radian = Math.PI / 180.0 * deg;
-		const end0 = state.section.xAxis.concat();
+		const end0 = section.xAxis.concat();
 
 		// rotate
-		const transform = mat4.rotate(mat4.create(), mat4.create(), radian, state.section.yAxis);
-		vec3.transformMat4(state.section.xAxis, state.section.xAxis, transform);
+		const transform = mat4.rotate(mat4.create(), mat4.create(), radian, section.yAxis);
+		vec3.transformMat4(section.xAxis, section.xAxis, transform);
 
-		const end1 = state.section.xAxis.concat();
+		const end1 = section.xAxis.concat();
 
-		state.section.origin[0] -= ( end1[0] - end0[0] ) / 2;
-		state.section.origin[1] -= ( end1[1] - end0[1] ) / 2;
-		state.section.origin[2] -= ( end1[2] - end0[2] ) / 2;
+		section.origin[0] -= ( end1[0] - end0[0] ) / 2;
+		section.origin[1] -= ( end1[1] - end0[1] ) / 2;
+		section.origin[2] -= ( end1[2] - end0[2] ) / 2;
 
-		viewer.setState(state);
+		return section;
 	}
 
-	public verticalRotate(viewer: Viewer, deg: number) {
-		const state = viewer.getState();
+	private rotateAroundXAxis(section: Section, deg: number): Section {
 		const radian = Math.PI / 180.0 * deg;
-		const end0 = state.section.yAxis.concat();
+		const end0 = section.yAxis.concat();
 
 		// rotate
-		const transform = mat4.rotate(mat4.create(), mat4.create(), radian, state.section.xAxis);
-		vec3.transformMat4(state.section.yAxis, state.section.yAxis, transform);
+		const transform = mat4.rotate(mat4.create(), mat4.create(), radian, section.xAxis);
+		vec3.transformMat4(section.yAxis, section.yAxis, transform);
 
-		const end1 = state.section.yAxis.concat();
+		const end1 = section.yAxis.concat();
 
-		state.section.origin[0] -= ( end1[0] - end0[0] ) / 2;
-		state.section.origin[1] -= ( end1[1] - end0[1] ) / 2;
-		state.section.origin[2] -= ( end1[2] - end0[2] ) / 2;
+		section.origin[0] -= ( end1[0] - end0[0] ) / 2;
+		section.origin[1] -= ( end1[1] - end0[1] ) / 2;
+		section.origin[2] -= ( end1[2] - end0[2] ) / 2;
 
-		viewer.setState(state);
+		return section;
 	}
 }

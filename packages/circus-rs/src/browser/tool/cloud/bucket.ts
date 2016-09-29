1ecd55b8372dc04494d88039c1c106c0783fe2f6
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-09-29 04:02:16 +0000

    Remove obsolete cloud-editor

--- packages/circus-rs/src/browser/tool/cloud/bucket.ts
+++ packages/circus-rs/src/browser/tool/cloud/bucket.ts
@@ -1,24 +1,14 @@
 import { ViewerEvent } from '../../../browser/viewer/viewer-event';
-import { ViewerEventTarget } from '../../../browser/interface/viewer-event-target';
 import { Tool } from '../../../browser/tool/tool';
-import { CloudEditor } from '../../../browser/tool/cloud/cloud-editor';
 
-export class BucketTool extends Tool implements ViewerEventTarget {
-
-	private drawing: boolean = false;
-	public cloudEditor: CloudEditor;
-
-	constructor() {
-		super();
-	}
+/**
+ * Bucket tool performs the flood-fill operation along an orthogonal MPR plane.
+ */
+export class BucketTool extends Tool {
 
 	public mouseDownHandler(ev: ViewerEvent) {
-		if (this.cloudEditor) {
-			this.cloudEditor.prepare(ev.viewer.getState());
-			this.cloudEditor.fill(ev.viewerX, ev.viewerY);
-			this.emit('filled');
-			ev.stopPropagation();
-		}
+		super.mouseDownHandler(ev);
+		// TODO: Implement flood-fill
 	}
 
 }

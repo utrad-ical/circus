e53cfc77e660d4939f16ea87021c21dbbd1e30f0
Soichiro Miki <smiki-tky@umin.ac.jp>
2016-11-13 22:47:37 +0900

    Display DICOM image using CIRCUS RS component

--- packages/circus-web-ui/app/front-ui/components/image-viewer.jsx
+++ packages/circus-web-ui/app/front-ui/components/image-viewer.jsx
@@ -6,22 +6,47 @@ import * as rs from 'circus-rs';
  * Wraps CIRCUS RS Dicom Viewer.
  */
 export class ImageViewer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			client: null // RsHttpClient
+		}
+	}
+
+	componentWillReceiveProps(newProps) {
+		if (this.seriesUID !== newProps.seriesUID) {
+			this.updateComposition(newProps.seriesUID);
+		}
+	}
+
+	updateComposition(seriesUID) {
+		if (seriesUID) {
+			const src = new rs.DynamicImageSource({
+				client: this.state.client,
+				series: seriesUID
+			});
+			this.state.composition.setImageSource(src);
+			this.state.viewer.setComposition(this.state.composition);
+		} else {
+			// this.viewer.setComposition(null);
+		}
+	}
+
 	componentDidMount() {
-		const viewer = this.refs.viewer;
-		const comp = new rs.Composition();
+		const container = this.refs.container;
+		const viewer = new rs.Viewer(container);
+		const composition = new rs.Composition();
 		const server = store.getState().loginUser.data.dicomImageServer;
-		const src = new rs.DynamicImageSource({
-			server,
-			series: this.props.seriesUID
+		const client = new rs.RsHttpClient(server);
+		this.setState({
+			viewer,
+			client,
+			composition
 		});
-		console.log('SRC', src);
-		src.ready().then(() => comp.renderAll());
-		comp.setImageSource(src);
-		comp.createViewer(viewer, { stateName: 'axial' });
-		comp.setTool('Hand');
+		this.updateComposition(this.props.seriesUID);
 	}
 
 	render() {
-		return <div ref="viewer"></div>;
+		return <div ref="container"></div>;
 	}
 }

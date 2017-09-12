612a03914c30c7018852f89bea145ab289e8a2f4
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-09-12 15:39:41 +0900

    Implement Project settings fields except for attribute schemas

--- packages/circus-web-ui/app/front-ui/pages/admin/ProjectAdmin.js
+++ packages/circus-web-ui/app/front-ui/pages/admin/ProjectAdmin.js
@@ -1,10 +1,17 @@
 import EditorPage from './EditorPage';
 import React from 'react';
+import { api } from 'utils/api';
 import * as et from 'rb/editor-types';
+import ShrinkSelect from 'rb/ShrinkSelect';
+import MultiSelect from 'rb/MultiSelect';
+import WindowPresetEditor from './WindowPresetEditor';
+import LoadingIndicator from 'rb/LoadingIndicator';
 
-const editorProperties = [
-	{ key: 'projectName', caption: 'Project Name', editor: et.text() },
-	{ key: 'description', caption: 'Description', editor: et.text() }
+
+const windowPriorityOptions = [
+	'dicom,preset,auto', 'dicom,auto',
+	'preset,dicom,auto', 'preset,auto',
+	'auto'
 ];
 
 const listColumns = [
@@ -19,16 +26,62 @@ const makeEmptyItem = () => {
 	};
 };
 
-const ProjectAdmin = () => {
-	return <EditorPage
-		title='Projects'
-		icon='education'
-		endPoint='project'
-		primaryKey='projectID'
-		editorProperties={editorProperties}
-		listColumns={listColumns}
-		makeEmptyItem={makeEmptyItem}
-	/>;
-};
+export default class ProjectAdmin extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { ready: false };
 
-export default ProjectAdmin;
+		this.editorProperties = [
+			{ key: 'projectName', caption: 'Project Name', editor: et.text() },
+			{ key: 'description', caption: 'Description', editor: et.text() },
+			{
+				key: 'windowPresets',
+				caption: 'Window Presets',
+				editor: et.arrayOf(WindowPresetEditor, { label: '', level: 0, width: 0 })
+			},
+			{
+				key: 'windowPriority',
+				caption: 'Window Priority',
+				editor: props => <ShrinkSelect
+					options={windowPriorityOptions} {...props}
+				/>
+			},
+			{ key: 'addSeriesGroups', caption: 'Add Series Groups', editor: null },
+			{ key: 'moderateGroups', caption: 'Moderate Groups', editor: null },
+			{ key: 'viewPersonalInfoGroups', caption: 'View Personal Info Groups', editor: null },
+			{ key: 'readGroups', caption: 'Read Groups', editor: null },
+			{ key: 'writeGroups', caption: 'Write Groups', editor: null }
+		];
+
+	}
+
+	async componentDidMount() {
+		const groups = await api('group');
+		const groupIdMap = {};
+		groups.forEach(g => groupIdMap[g.groupID] = g.groupName);
+		const GroupsMultiSelect = props => <MultiSelect
+			options={groupIdMap} numericalValue {...props}
+		/>;
+		this.editorProperties.forEach(property => {
+			if (/Groups$/.test(property.key)) {
+				property.editor = GroupsMultiSelect;
+			}
+		});
+
+		// this.groupIdMap = groupIdMap;
+		this.setState({ ready: true });
+	}
+
+	render() {
+		if (!this.state.ready) return <LoadingIndicator />;
+		return <EditorPage
+			title='Projects'
+			icon='education'
+			endPoint='project'
+			primaryKey='projectID'
+			editorProperties={this.editorProperties}
+			listColumns={listColumns}
+			makeEmptyItem={makeEmptyItem}
+		/>;
+	}
+}

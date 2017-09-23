7d5d5d24d0d624275d97689da25b8c875d8a12b6
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-09-23 17:45:27 +0000

    Add schema for project

--- packages/circus-api/test/validator.js
+++ packages/circus-api/test/validator.js
@@ -8,7 +8,7 @@ describe('validator', function() {
 		validator = await createValidator();
 	});
 
-	it('should validate user data', async function main() {
+	it('should validate user data', function() {
 		const testData = {
 			userEmail: 'a@b.c',
 			loginId: 'bar',
@@ -16,6 +16,25 @@ describe('validator', function() {
 			groups: []
 		};
 		assert.isTrue(validator.validate('user', testData));
+	});
+	
+	it('should validate project data', function() {
+		const testData = {
+			projectId: 'aaaaaaaaaa',
+			projectName: 'MRA',
+			tags: [],
+			caseAttributesSchema: [
+				{ key: 'smoking', caption: 'Smoking Index', type: 'number' },
+				{ key: 'hypertension', caption: 'Hypertension', type: 'boolean' },
+			],
+			labelAttributesSchema: [],
+			windowPresets: [{ label: 'lung', width: 1000, level: -100 }],
+			windowPriority: 'auto',
+			groups: []
+		};
+		const result = validator.validate('project', testData);
+		// console.log(validator.errors);
+		assert.isTrue(result);
 	});
 });
 

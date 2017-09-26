8b6bf9ff6f0da54967dda1d912a3896eaac98a28
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-09-26 16:51:41 +0000

    Complete errorHandler with tests

--- packages/circus-api/src/middleware/errorHandler.js
+++ packages/circus-api/src/middleware/errorHandler.js
@@ -1,5 +1,10 @@
-// The JSON error handler middleware that always outputs some JSON string
-const errorHandler = () => {
+import Ajv from 'ajv';
+
+/**
+ * Creates an error handler middleware that always outputs JSON
+ * as a HTTP response regardless of the cause of the error.
+ */
+export default function errorHandler(debugMode) {
 	return async function errorHandler(ctx, next) {
 		try {
 			await next();
@@ -8,13 +13,46 @@ const errorHandler = () => {
 				ctx.status = 404; // Reassign is necessary
 			}
 		} catch (err) {
-			if (err instanceof TypeError) {
-				// validation error
+			if (err instanceof Ajv.ValidationError) {
+				// JSON validation error occurred.
+				if (err.phase == 'response') {
+					// Response validation error.
+					// This means there is a bug on the server-side,
+					// or the database is corrupted.
+					ctx.status = 500;
+					ctx.body = {
+						error: 'Response schema validation error detected.'
+					};
+				} else {
+					// Request validation error.
+					// This just means the user has sent wrong data that does
+					// not fulfill some JSON schema.
+					ctx.status = 400;
+					ctx.body = {
+						error: 'Request data is not correct.',
+						validationErrors: err.errors
+					};
+				}
+				return;
+			} else {
+				// Other exceptions thrown.
+				if (!err.status) {
+					// Exception without `status` means some unexpected
+					// run-time error happened outside of `ctx.throw()`.
+					ctx.status = 500;
+					if (debugMode) {
+						ctx.body = { error: err.message, stack: err.stack };
+					} else {
+						ctx.body = { error: 'Internal server error.' };
+					}
+				} else {
+					// Exception with `status` means `ctx.throw()` was
+					// manually called somewhere in our codebase.
+					// (We have successfully handled an exceptional event!)
+					ctx.status = err.status;
+					ctx.body = { error: err.message };
+				}
 			}
-			ctx.status = err.status || 500;
-			ctx.body = { error: err.message };
 		}
 	};
-};
-
-export default errorHandler;
+}
---
+++ packages/circus-api/test/errorHandler.test.js
@@ -0,0 +1,124 @@
+import createApp from '../src/createApp';
+import errorHandler from '../src/middleware/errorHandler';
+import axios from 'axios';
+import Koa from 'koa';
+import Router from 'koa-router';
+import Ajv from 'ajv';
+import { assert } from 'chai';
+
+const port = process.env.API_TEST_PORT || 8081;
+
+describe('errorHandler middleware', function() {
+	let serverUrl = `http://localhost:${port}/`;
+	let server;
+
+	before(function(done) {
+		const app = new Koa();
+		
+		app.use(errorHandler(true));
+		
+		const router = new Router();
+
+		router.get('/found', async ctx => {
+			ctx.body = 'Hello';
+		});
+
+		router.get('/invalid', async ctx => {
+			const ajv = new Ajv();
+			const schema = { $async: true, type: 'number' };
+			await ajv.validate(schema, 'hi'); // validation fails
+		});
+
+		router.get('/invalid-on-response', async ctx => {
+			const ajv = new Ajv();
+			const schema = { $async: true, type: 'number' };
+			try {
+				await ajv.validate(schema, 'hi'); // validation fails
+			} catch (err) {
+				err.phase = 'response';
+				throw err;
+			}
+		});
+
+		router.get('/server-side-error', async ctx => {
+			ctx.body = null.foo;
+		});
+
+		app.use(router.routes());
+		server = app.listen(
+			port,
+			'localhost',
+			err => { if (!err) done(); }
+		);
+	});
+	
+	after(function(done) {
+		if (server) {
+			server.close(err => { if (!err) done(); });
+		} else done();
+	});
+	
+	it('should return 200 for normal request', async function() {
+		const res = await axios.request({
+			url: serverUrl + 'found',
+			method: 'get'
+		});
+		assert.equal(res.status, 200);
+	});
+	
+	it('should return 404 for nonexistent path', async function() {
+		try {
+			await axios.request({
+				url: serverUrl + 'not-found',
+				method: 'get'
+			});
+		} catch (err) {
+			assert.equal(err.response.status, 404);
+			assert.match(err.response.data.error, /not found/i);
+			return;
+		}
+		throw new Error();
+	});
+	
+	it('should return 400 for request validation error', async function() {
+		try {
+			await axios.request({
+				url: serverUrl + 'invalid',
+				method: 'get'
+			});
+		} catch (err) {
+			assert.equal(err.response.status, 400);
+			assert.match(err.response.data.error, /request data is not correct/i);
+			return;
+		}
+		throw new Error();
+	});
+
+	it('should return 500 for server-side validation error', async function() {
+		try {
+			await axios.request({
+				url: serverUrl + 'invalid-on-response',
+				method: 'get'
+			});
+		} catch (err) {
+			assert.equal(err.response.status, 500);
+			assert.match(err.response.data.error, /response schema validation error/i);
+			return;
+		}
+		throw new Error();
+	});
+
+	it('should return 500 for server-side run-time error', async function() {
+		try {
+			await axios.request({
+				url: serverUrl + 'server-side-error',
+				method: 'get'
+			});
+		} catch (err) {
+			assert.equal(err.response.status, 500);
+			assert.exists(err.response.data.stack);
+			return;
+		}
+		throw new Error();
+	});
+});

86116b65c7c7a48c62ac80f4feb39197643a7453
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-09-26 14:43:27 +0000

    Add dummy-progress route for debugging

--- packages/circus-api/src/api/debug/index.js
+++ packages/circus-api/src/api/debug/index.js
@@ -1,3 +1,5 @@
+import { PassThrough } from 'stream';
+
 export async function handleGet(ctx, next) {
 	ctx.body = {
 		status: 'running'
@@ -6,4 +8,23 @@ export async function handleGet(ctx, next) {
 
 export async function handleEcho(ctx, next) {
 	ctx.body = ctx.request.body;
+}
+
+async function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function handleDummyProgress(ctx, next) {
+	const stream = new PassThrough();
+	ctx.body = stream;
+	ctx.type = 'text/event-stream';
+
+	for (let i = 0; i <= 10; i++) {
+		await delay(500);
+		stream.write(
+			'event: progress\n' +
+			'data: ' + JSON.stringify({ progress: i * 10 }) + '\n\n'
+		);
+	}
+	stream.end();
 }

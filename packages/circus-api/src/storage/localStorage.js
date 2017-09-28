6a7b681e7eda48963cdd97a268865b42603276de
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-09-28 09:45:13 +0000

    Refactor localStorage

---
+++ packages/circus-api/src/storage/localStorage.js
@@ -0,0 +1,36 @@
+import fs from 'fs-extra';
+import * as path from 'path';
+
+export default async function localStorage(params) {
+	const {
+		root,
+		nameToPath = (n => n)
+	} = params;
+
+	if (!root || !(await fs.pathExists(root))) {
+		throw new Error('Root directory does not exist');
+	}
+
+	function buildPath(key) {
+		return path.join(root, nameToPath(key));
+	}
+
+	async function read(key) {
+		return await fs.readFile(buildPath(key));
+	}
+
+	async function write(key, data) {
+		return await fs.outputFile(buildPath(key), data);
+	}
+
+	async function remove(key) {
+		return await fs.unlink(buildPath(key));
+	}
+
+	async function exists(key) {
+		// using fs-extra's extention method
+		return await fs.pathExists(buildPath(key));
+	}
+
+	return { read, write, remove, exists };
+}
--- packages/circus-api/test/storage.test.js
+++ packages/circus-api/test/storage.test.js
@@ -4,21 +4,33 @@ import * as path from 'path';
 import * as fs from 'fs-extra';
 
 describe('Storage', function() {
-	let store1;
-	let store2;
+	let store;
 
 	const tmpDir = path.join(__dirname, 'tmp-dir');
 
+	async function throws(func) {
+		try {
+			await func();
+		} catch (err) {
+			return;
+		}
+		throw new Error('did not throw');
+	}
+
 	before(async function() {
 		await fs.emptyDir(tmpDir);
-		store1 = await createStorage('local', { root: tmpDir, preHashed: true });
-		store2 = await createStorage('local', { root: tmpDir, preHashed: false });
+		store = await createStorage(
+			'local',
+			{
+				root: tmpDir,
+				nameToPath: p => path.join(p.substr(0, 2), p + '.txt')
+			}
+		);
 	});
 
 	beforeEach(async function() {
 		await fs.emptyDir(tmpDir);
-		await fs.outputFile(path.join(tmpDir, 'aa', 'bb', 'aabbcc.txt'), 'coconut');
-		await fs.outputFile(path.join(tmpDir, '91', 'e1', 'aabbcc.txt'), 'pudding');
+		await fs.outputFile(path.join(tmpDir, 'aa', 'aabbcc.txt'), 'coconut');
 	});
 
 	after(async function() {
@@ -27,53 +39,34 @@ describe('Storage', function() {
 		}
 	});
 
-	// sha1('aabbcc.txt') === '91e157b3...'
-	// sha1('xxyyzz,txt') === '176b3478...'
-
 	it('should throw error for nonexistent root', async function() {
-		try {
+		await throws(async function() {
 			await createStorage('local', 'somewhere/over/the/rainbow');
-		} catch (err) {
-			return;
-		}
-		throw new Error('Did not throw');
+		});
 	});
 
 	it('should execute write()', async function() {
-		await store1.write('xxyyzz.txt', 'biscuit');
-		await store2.write('xxyyzz.txt', 'chocolate');
-		const out1 = path.join(tmpDir, 'xx', 'yy', 'xxyyzz.txt');
-		assert.isTrue(await fs.pathExists(out1));
-		assert.equal(await fs.readFile(out1, 'utf8'), 'biscuit');
-		const out2 = path.join(tmpDir, '17', '6b', 'xxyyzz.txt');
-		assert.isTrue(await fs.pathExists(out2));
-		assert.equal(await fs.readFile(out2, 'utf8'), 'chocolate');
+		await store.write('xxyyzz', 'biscuit');
+		const out = path.join(tmpDir, 'xx', 'xxyyzz.txt');
+		assert.isTrue(await fs.pathExists(out));
+		assert.equal(await fs.readFile(out, 'utf8'), 'biscuit');
 	});
 
 	it('should execute read()', async function() {
-		assert.equal(await store1.read('aabbcc.txt'), 'coconut');
-		assert.equal(await store2.read('aabbcc.txt'), 'pudding');
-		try {
-			await store1.read('xxyyzz.txt');
-		} catch(err) {
-			return;
-		}
-		throw new Error('Did not throw');
+		assert.equal(await store.read('aabbcc'), 'coconut');
+		await throws(async function() {
+			await store.read('notafile');
+		});
 	});
 
 	it('should execute exists()', async function() {
-		assert.isTrue(await store1.exists('aabbcc.txt'));
-		assert.isTrue(await store2.exists('aabbcc.txt'));
-		assert.isFalse(await store1.exists('xxyyzz.txt'));
+		assert.isTrue(await store.exists('aabbcc'));
+		assert.isFalse(await store.exists('xxyyzz'));
 	});
 
 	it('should execute remove()', async function() {
-		assert.isTrue(await fs.pathExists(path.join(tmpDir, 'aa', 'bb', 'aabbcc.txt')));
-		await store1.remove('aabbcc.txt');
-		assert.isFalse(await fs.pathExists(path.join(tmpDir, 'aa', 'bb', 'aabbcc.txt')));
-
-		assert.isTrue(await fs.pathExists(path.join(tmpDir, '91', 'e1', 'aabbcc.txt')));
-		await store2.remove('aabbcc.txt');
-		assert.isFalse(await fs.pathExists(path.join(tmpDir, '91', 'e1', 'aabbcc.txt')));
+		assert.isTrue(await fs.pathExists(path.join(tmpDir, 'aa', 'aabbcc.txt')));
+		await store.remove('aabbcc');
+		assert.isFalse(await fs.pathExists(path.join(tmpDir, 'aa', 'aabbcc.txt')));
 	});
 });

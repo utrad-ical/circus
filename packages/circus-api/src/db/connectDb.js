5335a82a963e8d347b279be97a6e9534f10828ee
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-11-02 14:14:21 +0900

    Modfy connection URL env name

--- packages/circus-api/src/db/connectDb.js
+++ packages/circus-api/src/db/connectDb.js
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb';
 
 export default async function connectDb() {
-	const db = await MongoClient.connect(process.env.MONGO_URL);
+	const url = process.env.CIRCUS_MONGO_URL ||
+		process.env.MONGO_URL;
+	const db = await MongoClient.connect(url);
 	return db;
 }

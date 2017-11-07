4af41412e411954d2727284b2f0404892e0f9ad8
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-11-07 23:19:50 +0900

    Add ts-loader so the server can be executed without prior compilation

--- packages/circus-rs/server.js
+++ packages/circus-rs/server.js
@@ -2,23 +2,37 @@
  * CIRCUS RS
  */
 
-'use strict';
+// We register ts-node, which enables requiring *.ts
+// files directly from Node.js
+var tsOptions = require('./src/tsconfig').compilerOptions;
+require('ts-node').register({
+	compilerOptions: tsOptions,
+	disableWarnings: true
+});
 
 console.log('CIRCUS RS is starting up...');
 
 // Load configuration
-var config = require('./lib/server/Config');
+var config = require('./src/server/Config');
 
 // Execute server.
-var Server = require('./lib/server/Server').default;
+var Server = require('./src/server/Server').default;
 
-var moduleLoader = require('./lib/server/ModuleLoader');
+var moduleLoader = require('./src/server/ModuleLoader');
 
 var server = new Server(
-	moduleLoader.loadModule(moduleLoader.ModuleType.Logger, config.logger),
-	moduleLoader.loadModule(moduleLoader.ModuleType.ImageEncoder, config.imageEncoder),
-	moduleLoader.loadModule(moduleLoader.ModuleType.DicomFileRepository, config.dicomFileRepository),
-	moduleLoader.loadModule(moduleLoader.ModuleType.DicomDumper, config.dumper),
+	moduleLoader.loadModule(
+		moduleLoader.ModuleType.Logger, config.logger
+	),
+	moduleLoader.loadModule(
+		moduleLoader.ModuleType.ImageEncoder, config.imageEncoder
+	),
+	moduleLoader.loadModule(
+		moduleLoader.ModuleType.DicomFileRepository, config.dicomFileRepository
+	),
+	moduleLoader.loadModule(
+		moduleLoader.ModuleType.DicomDumper, config.dumper
+	),
 	config
 );
 

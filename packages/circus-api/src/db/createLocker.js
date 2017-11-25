fefc6a081f96927947645fcfbb110e56ee41ff05
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-11-25 17:47:04 +0900

    Implement dummy lock function

---
+++ packages/circus-api/src/db/createLocker.js
@@ -0,0 +1,56 @@
+import randomstring from 'randomstring';
+
+/**
+ * Provides dummy DB locking functionality.
+ * @example
+ * const locker = createLocker(db);
+ * let id;
+ * try {
+ *   id = await locker.lock('<my-resource-id>');
+ * } catch (err) {
+ *   console.log('This resource is busy, try later.');
+ * }
+ * try {
+ *   // do some work on my-resource
+ * } finally {
+ *   await locker.unlock(id);
+ * }
+ */
+export default function createLocker(db, collectionName = 'locks', timeout = 300000) {
+	async function lock(target) {
+		if (typeof target !== 'string' || !target.length) {
+			throw new TypeError('Lock target must be string');
+		}
+		const now = new Date();
+		const minTime = new Date(now.getTime() - timeout);
+
+		const myId = randomstring.generate({ length: 12, charset: 'hex' });
+		const res = await db.collection(collectionName).bulkWrite(
+			[
+				{ deleteMany: { filter: { target, createdAt: { $lt: minTime } } } },
+				{ insertOne: { document: { target, myId, createdAt: now } } },
+			]
+		);
+		// Check the raw bulkWrite result and see if there is any error
+		const errors = res.getRawResponse().writeErrors;
+		if (errors.length) {
+			if (errors[0].code === 11000) {
+				throw new Error('Resource busy.');
+			} else {
+				throw new Error('BulkWrite error');
+			}
+		}
+
+		return myId;
+	}
+
+	async function unlock(id) {
+		await db.collection(collectionName).deleteMany({ myId: id });
+	}
+
+	async function unlockAll(target) {
+		await db.collection(collectionName).deleteMany({ target });
+	}
+
+	return { lock, unlock, unlockAll };
+}
---
+++ packages/circus-api/test/createLocker.test.js
@@ -0,0 +1,45 @@
+import * as test from './test-utils';
+import createLocker from '../src/db/createLocker';
+
+async function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('createLocker', function() {
+	let db, locker;
+
+	before(async function() {
+		db = await test.connectMongo();
+		await db.collection('locks').deleteMany({});
+		locker = createLocker(db);
+	});
+
+	after(async function() {
+		if (db) await db.close();
+	});
+
+	it('should perform locking of one resource', async function() {
+		let id;
+		for (let i = 0; i <= 2; i++) {
+			try {
+				id = await locker.lock('orange');
+			} finally {
+				await locker.unlock(id);
+			}
+		}
+	});
+
+	it('should fail on the second lock trial', async function() {
+		await test.asyncThrows(async function() {
+			await locker.lock('sapphire');
+			await locker.lock('sapphire');
+		});
+	});
+
+	it('should not fail with a zombie lock', async function() {
+		const locker = createLocker(db, 'locker', 50);
+		await locker.lock('passion');
+		await delay(100);
+		await locker.lock('passion');
+	});
+});

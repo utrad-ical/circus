cedeb99ec3365e315abf3cd6e0712d88d3a98417
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-11-30 21:30:32 +0900

    Add determineUserAccessInfo

--- packages/circus-api/src/privilegeUtils.js
+++ packages/circus-api/src/privilegeUtils.js
@@ -15,6 +15,42 @@ export async function globalPrivilegesOfUser(models, user) {
 	return userPrivileges;
 }
 
+export async function determineUserAccessInfo(models, user) {
+	const globalPrivileges = {};
+	const domains = {};
+	const accessibleProjects = {};
+	for (const groupId of user.groups) {
+		const group = await models.group.findByIdOrFail(groupId);
+		for (const priv of group.privileges) {
+			globalPrivileges[priv] = true;
+		}
+		for (const domain of group.domains) {
+			domains[domain] = true;
+		}
+		const roleNames = ['read', 'write', 'viewPersonalInfo', 'moderate', 'addSeries'];
+		for (const role of roleNames) {
+			for (const pId of group[`${role}Projects`]) {
+				if (!(pId in accessibleProjects)) {
+					accessibleProjects[pId] = {
+						projectId: pId,
+						roles: {}
+					};
+				}
+				accessibleProjects[pId].roles[role] = true;
+			}
+		}
+	}
+	for (const p in accessibleProjects) {
+		const project = await models.project.findByIdOrFail(p);
+		accessibleProjects[p].project = project;
+		accessibleProjects[p].roles = Object.keys(accessibleProjects[p].roles);
+	}
+	return {
+		domains: Object.keys(domains),
+		globalPrivileges: Object.keys(globalPrivileges),
+		accessibleProjects: Object.values(accessibleProjects)
+	};
+}
 
 export async function accessibleProjectsForOperation(models, user, operation) {
 	const operations = [
---
+++ packages/circus-api/test/privilegeUtils.test.js
@@ -0,0 +1,42 @@
+import { assert } from 'chai';
+import * as test from './test-utils';
+import createValidator from '../src/createValidator';
+import createModels from '../src/db/createModels';
+import { determineUserAccessInfo } from '../src/privilegeUtils';
+
+describe('checkGlobalPrivileges middleware', function() {
+	let db, models;
+
+	before(async function() {
+		db = await test.connectMongo();
+		await test.setUpMongoFixture(db, ['groups', 'users']);
+		const validator = await createValidator();
+		models = createModels(db, validator);
+	});
+
+	after(async function() {
+		if (db) await db.close();
+	});
+
+	it('determineUserAccessInfo', async function() {
+		const alice = await models.user.findByIdOrFail('alice@example.com');
+		const privA = await determineUserAccessInfo(models, alice);
+		assert.sameMembers(privA.globalPrivileges, ['manageServer', 'personalInfoView']);
+		assert.sameMembers(privA.domains, ['sirius.org']);
+		assert.deepEqual(privA.accessibleProjects, []);
+
+		const bob = await models.user.findByIdOrFail('bob@example.com');
+		const privB = await determineUserAccessInfo(models, bob);
+		assert.sameMembers(privB.globalPrivileges, ['personalInfoView']);
+		assert.sameMembers(privB.domains, ['vega.org', 'altair.org']);
+		assert.equal(privB.accessibleProjects.length, 1);
+		assert.sameMembers(privB.accessibleProjects[0].roles, ['read', 'write']);
+
+		const guest = await models.user.findByIdOrFail('guest@example.com');
+		const privG = await determineUserAccessInfo(models, guest);
+		assert.sameMembers(privG.globalPrivileges, []);
+		assert.sameMembers(privG.domains, []);
+		assert.deepEqual(privG.accessibleProjects, []);
+	});
+
+});

85f5aa2e24c99997273ad9731253c48681e90b0f
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-07 16:49:32 +0900

    Fix CORS headers

--- packages/circus-api/src/middleware/cors.js
+++ packages/circus-api/src/middleware/cors.js
@@ -6,7 +6,8 @@ export default function cors(origin = '*') {
 		ctx.response.set('Access-Control-Allow-Origin', origin);
 		ctx.response.set('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS');
 		ctx.response.set('Access-Control-Allow-Credentials', 'true');
-		ctx.response.set('Access-Control-Allow-Headers', 'Authorization');
+		ctx.response.set('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+		ctx.response.set('Access-Control-Max-Age', '86400');
 		await next();
 	};
 }

ab891f7c3bd7118612f55d7808812cb0722165b9
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-15 01:11:35 +0900

    Implement tag select

---
+++ packages/circus-web-ui/app/front-ui/components/MultiTagSelect.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import MultiSelect from 'rb/MultiSelect';
+import { Tag } from './tag';
+
+const MultiTagSelect = props => {
+	const { tags } = props;
+
+	const options = {};
+	tags.forEach(tag => {
+		options[tag.name] = { caption: <Tag {...tag} /> };
+	});
+
+	return <MultiSelect
+		{...props}
+		options={options}
+	/>;
+};
+
+export default MultiTagSelect;

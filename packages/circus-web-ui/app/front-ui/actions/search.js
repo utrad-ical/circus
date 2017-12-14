b9692238efd5e8a2e56d86d3a3feb8f0d6aa366a
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-14 20:11:34 +0900

    Refactor search

--- packages/circus-web-ui/app/front-ui/actions/search.js
+++ packages/circus-web-ui/app/front-ui/actions/search.js
@@ -1,49 +1,48 @@
 import { api } from 'utils/api';
-import { store } from 'store';
 
-const dispatch = store.dispatch.bind(store);
-
-async function beginQuery(params) {
-	const state = store.getState();
-	const name = params.name;
-	const search = state.searches[name];
-	if (search && search.isFetching) {
-		throw new Error('Previous search not finished');
-	}
-
-	const resource = params.resource || search.resource;
-	const filter = params.filter || search.filter;
-	const page = params.page || search.page;
-	const sort = params.sort || search.sort;
-	const per = params.per || search.per;
-
-	dispatch({
-		type: 'START_SEARCH_QUERY',
-		name
-	});
-	const query = { filter, sort, page };
-	const result = await api(`${resource}`, { params: query });
-	dispatch({
-		type: 'LOAD_SEARCH_RESULTS',
-		name,
-		resource,
-		filter,
-		sort,
-		per,
-		page: result.page,
-		items: result.items,
-		totalItems: result.totalItems
-	});
+function beginQuery(params) {
+	return async (dispatch, getState) => {
+		const state = getState();
+		const name = params.name;
+		const search = state.searches[name];
+		if (search && search.isFetching) {
+			throw new Error('Previous search not finished');
+		}
+	
+		const resource = params.resource || search.resource;
+		const filter = params.filter || search.filter;
+		const page = params.page || search.page;
+		const sort = params.sort || search.sort;
+		const per = params.per || search.per;
+	
+		dispatch({
+			type: 'START_SEARCH_QUERY',
+			name
+		});
+		const query = { filter, sort, page };
+		const result = await api(resource, { params: query });
+		dispatch({
+			type: 'LOAD_SEARCH_RESULTS',
+			name,
+			resource,
+			filter,
+			sort,
+			per,
+			page: result.page,
+			items: result.items,
+			totalItems: result.totalItems
+		});
+	};
 }
 
 export function startNewSearch(name, resource, filter, sort) {
-	beginQuery({ name, resource, filter, page: 1, per: 20, sort });
+	return beginQuery({ name, resource, filter, page: 1, per: 20, sort });
 }
 
 export function changeSearchPage(name, page) {
-	beginQuery({ name, page });
+	return beginQuery({ name, page });
 }
 
 export function changeSearchSort(name, sort) {
-	beginQuery({ name, sort });
+	return beginQuery({ name, sort });
 }
--- packages/circus-web-ui/app/front-ui/pages/search/SeriesSearchResults.js
+++ packages/circus-web-ui/app/front-ui/pages/search/SeriesSearchResults.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchResultsView from './SearchResultsView';
+import SearchResultsView, { makeSortOptions } from './SearchResultsView';
 import { Button } from 'components/react-bootstrap';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
@@ -11,57 +11,67 @@ async function createCase(seriesUID) {
 }
 */
 
-class SeriesSearchResultsView extends SearchResultsView {
-	constructor(props) {
-		super(props);
-		this.makeSortOptions({
-			createTime: 'series import time',
-			seriesUID: 'series instance UID',
-			seriesDate: 'series date',
-			modality: 'modality'
-		});
-	}
-
-	renderItem(item) {
-		function anon(item) {
-			if (item) {
-				return item;
-			} else {
-				return <span className='anonymized'>(anonymized)</span>;
-			}
+const DataViewRow = props => {
+	function anon(item) {
+		if (item) {
+			return item;
+		} else {
+			return <span className='anonymized'>(anonymized)</span>;
 		}
-
-		return <div className='search-result series'>
-			<div className='modality'>{item.modality}</div>
-			<div className='series-date'>{item.seriesDate}</div>
-			<div className='create-time'>{item.createTime}</div>
-			<div className='series-description'>{item.seriesDescription}</div>
-			<div className='patient-id'>
-				{anon(item.patientInfo.patientID)}
-			</div>
-			<div className='patient-name'>
-				{anon(item.patientInfo.patientName)}
-			</div>
-			<div className='patient-age-sex'>
-				{anon(`${item.patientInfo.age} ${item.patientInfo.sex}`)}
-			</div>
-			<div className='register'>
-				<Link to={`/series/${item.seriesUID}`}>
-					<Button>
-						<span className='circus-icon circus-icon-series' />
-						View
-					</Button>
-				</Link>
-			</div>
-		</div>;
 	}
 
-}
+	const { item } = props;
+
+	return <div className='search-result series'>
+		<div className='modality'>{item.modality}</div>
+		<div className='series-date'>{item.seriesDate}</div>
+		<div className='create-time'>{item.createTime}</div>
+		<div className='series-description'>{item.seriesDescription}</div>
+		<div className='patient-id'>
+			{anon(item.patientInfo.patientID)}
+		</div>
+		<div className='patient-name'>
+			{anon(item.patientInfo.patientName)}
+		</div>
+		<div className='patient-age-sex'>
+			{anon(`${item.patientInfo.age} ${item.patientInfo.sex}`)}
+		</div>
+		<div className='register'>
+			<Link to={`/series/${item.seriesUID}`}>
+				<Button>
+					<span className='circus-icon circus-icon-series' />
+					View
+				</Button>
+			</Link>
+		</div>
+	</div>;
+};
+
+const DataView = props => {
+	const { items } = props;
+	return <div className='search-results'>{items.map(
+		item => <DataViewRow key={item.seriesUid} item={item} />
+	)}</div>;
+};
+
+const sortOptions = makeSortOptions({
+	createTime: 'series import time',
+	seriesUid: 'series instance UID',
+	seriesDate: 'series date',
+	modality: 'modality'
+});
+
+const SeriesSearchResultsView = props => {
+	return <SearchResultsView
+		sortOptions={sortOptions}
+		dataView={DataView}
+		{...props}
+	/>;
+};
 
 export default connect(
 	state => {
-		const name = 'series';
-		const search = state.searches[name] || {};
-		return { ...search, name };
+		const search = state.searches.series || {};
+		return { ...search, name: 'series' };
 	}
 )(SeriesSearchResultsView);

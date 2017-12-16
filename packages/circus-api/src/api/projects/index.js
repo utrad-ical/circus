91459fee31052a29c5805c2b45f138419c543b55
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-16 12:19:18 +0900

    Fix /api/projects/:projectId route

--- packages/circus-api/src/api/projects/index.js
+++ packages/circus-api/src/api/projects/index.js
@@ -1,16 +1,5 @@
-import { accessibleProjectsForOperation } from '../../privilegeUtils';
-import status from 'http-status';
-
-export const handleGet = ({ models }) => {
+export const handleGet = ({}) => {
 	return async (ctx, next) => {
-		const projectId = ctx.params.projectId;
-		const prjs = accessibleProjectsForOperation(ctx, ctx.user, 'read');
-		if (prjs[projectId] !== true) {
-			ctx.throw(
-				status.UNAUTHORIZED,
-				'You do not have read access to this project.'
-			);
-		}
-		ctx.body = await models.project.findByIdOrFail(projectId);
+		ctx.body = ctx.project;
 	};
 };
--- packages/circus-api/src/privilegeUtils.js
+++ packages/circus-api/src/privilegeUtils.js
@@ -38,24 +38,6 @@ export async function determineUserAccessInfo(models, user) {
 	};
 }
 
-export async function accessibleProjectsForOperation(models, user, operation) {
-	const operations = [
-		'read', 'write', 'addSeries', 'viewPersonalInfo', 'moderate'
-	];
-	if (typeof operation !== 'string' || operations.indexOf(operation) < 0) {
-		throw new TypeError('Unknown project operation type.');
-	}
-
-	const result = {};
-	for (const groupId of user.groups) {
-		const group = await models.group.findById(groupId);
-		for (const projId of group[operation + 'Projects']) {
-			result[projId] = true;
-		}
-	}
-	return result;
-}
-
 export function globalPrivileges() {
 	return [
 		{ privilege: 'createProject', caption: 'Create Project'},

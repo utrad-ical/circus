52154fee1d532439899990514850e0eab749d59e
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-16 17:25:52 +0900

    Better promsiify for server startup

--- packages/circus-rs/src/server/main.ts
+++ packages/circus-rs/src/server/main.ts
@@ -1,11 +1,18 @@
-console.log('CIRCUS RS is starting up...');
-
 import config from './Config';
 import Server from './Server';
-
 import loadModule, { ModuleType } from './ModuleLoader';
 
+function listen(app, ...args) {
+	return new Promise((resolve, reject) => {
+		const httpServer = app.listen.call(app, ...args, err => {
+			if (err) reject(err); else resolve(httpServer);
+		});
+	});
+}
+
 async function main() {
+	console.log('CIRCUS RS is starting up...');
+
 	const logger = loadModule(ModuleType.Logger, config.logger);
 
 	const server = new Server(
@@ -18,24 +25,18 @@ async function main() {
 	server.prepare();
 	const app = server.getApp();
 
-	const promise = new Promise<string>((resolve, reject) => {
-		try {
-			const port = config.port;
-			const httpServer = app.listen(port, '0.0.0.0', () => {
-				const message = `Server running on port ${port}`;
-				logger.info(message);
-				resolve(message);
-			});
-		} catch (e) {
-			console.error('Server failed to start');
-			console.error(e);
-			logger.error(e);
-			// This guarantees all the logs are flushed before actually exiting the program
-			logger.shutdown().then(() => process.exit(1));
-			reject(e);
-		}
-	});
-	return promise;
+	const port = config.port;
+	try {
+		const httpServer = await listen(app, port, '0.0.0.0');
+		const message = `Server running on port ${port}`;
+		logger.info(message);
+	} catch (e) {
+		console.error('Server failed to start');
+		console.error(e);
+		logger.error(e);
+		// This guarantees all the logs are flushed before actually exiting the program
+		logger.shutdown().then(() => process.exit(1));
+	}
 }
 
 main();

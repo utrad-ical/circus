a1484d5c1b61d3a38744ad89f66cb47d76ed3f1f
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-18 02:34:00 +0900

    Fix broken test caused by ts-register

--- packages/circus-api/server.js
+++ packages/circus-api/server.js
@@ -1,22 +1,4 @@
-// This imports babel-register, which makes all subsequent 'require's
-// parsed by babel using the settings specified by ".babelrc".
-require('babel-register');
-
-// Similarly, this enalbes direct import of TypeScript files.
-// We register ts-node, which enables requiring *.ts files directly from Node.js.
-// Consider removing this after the stable release of Babel 7.x,
-// which includes "typescript" preset.
-const compilerOptions = {
-	module: 'CommonJS',
-	target: 'es2017',
-	strictNullChecks: true,
-};
-
-require('ts-node').register({
-	compilerOptions,
-	disableWarnings: true,
-	ignore: false
-});
+require('./register');
 
 // Run the main script
 require('./src/main');

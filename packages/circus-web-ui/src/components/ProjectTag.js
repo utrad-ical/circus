343c5aa2679604203602f5ae29bf9d5772ab8d8f
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-18 23:14:28 +0900

    Implement ProjectTag

---
+++ packages/circus-web-ui/src/components/ProjectTag.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Tag } from './tag';
+
+const ProjectTagView = props => {
+	const { projectId, tag, user } = props;
+	const project = user.accessibleProjects.find(p => p.projectId === projectId);
+	const tagDef = project.project.tags.find(t => t.name === tag);
+	const color = tagDef ? tagDef.color : '#ffffff';
+	return <Tag color={color} name={tag} />;
+};
+
+const ProjectTag = connect(state => {
+	return { user: state.loginUser.data };
+})(ProjectTagView);
+
+export default ProjectTag;

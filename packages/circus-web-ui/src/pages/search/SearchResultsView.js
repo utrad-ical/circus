7a3cd49f903ce681ccbf64cf6184883cedbdb94c
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-18 20:18:46 +0900

    Refactor SearchResultView to SFC

--- packages/circus-web-ui/src/pages/search/SearchResultsView.js
+++ packages/circus-web-ui/src/pages/search/SearchResultsView.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ShrinkSelect from 'rb/ShrinkSelect';
 import { Pagination } from 'components/react-bootstrap';
 import { changeSearchPage, changeSearchSort } from 'actions';
-import { store } from 'store';
 
 export const makeSortOptions = sortKeys => {
 	const options = {};
@@ -13,43 +12,57 @@ export const makeSortOptions = sortKeys => {
 	return options;
 };
 
-export default class SearchResultsView extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handlePageClick = this.handlePageClick.bind(this);
-		this.handleSortChange = this.handleSortChange.bind(this);
+const SearchResultsView = props => {
+	const {
+		dispatch,
+		name,
+		totalItems,
+		per,
+		items,
+		page,
+		sort,
+		sortOptions,
+		dataView: DataView
+	} = props;
+
+	function handleSortChange(newSort) {
+		if (newSort === sort) return;
+		dispatch(changeSearchSort(name, newSort));
 	}
 
-	handleSortChange(sort) {
-		store.dispatch(changeSearchSort(this.props.name, sort));
+	function handlePageClick(newPage) {
+		if (newPage === page) return;
+		dispatch(changeSearchPage(name, newPage));
 	}
 
-	handlePageClick(page) {
-		if (page === this.props.page) return;
-		store.dispatch(changeSearchPage(this.props.name, page));
-	}
-
-	render() {
-		const { totalItems, per, items, page, sort, sortOptions, dataView: DataView } = this.props;
-		const pages = Math.ceil(totalItems / per);
-		return <div className='search-results-container'>
+	const pages = Math.ceil(totalItems / per);
+	return (
+		<div className='search-results-container'>
 			<div className='search-results-header'>
 				{totalItems + ' Result' + (totalItems === 1 ? '' : 's')}
-				&emsp;
-				Sort: <ShrinkSelect
+				&emsp; Sort:{' '}
+				<ShrinkSelect
 					options={sortOptions}
 					value={sort}
-					onChange={this.handleSortChange}
+					onChange={handleSortChange}
 				/>
 			</div>
-			{ items && items.length && <DataView items={items} /> }
+			{items && items.length && <DataView items={items} />}
 			<div className='search-results-pager'>
 				<Pagination
-					prev next first last ellipsis
-					items={pages} maxButtons={10} activePage={page}
-					onSelect={this.handlePageClick}
+					prev
+					next
+					first
+					last
+					ellipsis
+					items={pages}
+					maxButtons={10}
+					activePage={page}
+					onSelect={handlePageClick}
 				/>
 			</div>
-		</div>;
-	}
-}
+		</div>
+	);
+};
+
+export default SearchResultsView;

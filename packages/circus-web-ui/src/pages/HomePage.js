2cf4e2b68c5ab762706f121f8131a8121fbd81dd
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-19 14:05:46 +0900

    Improve home screen

--- packages/circus-web-ui/src/pages/HomePage.js
+++ packages/circus-web-ui/src/pages/HomePage.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import { Panel, Glyphicon, Row, Col, ListGroup, ListGroupItem }
-	from 'components/react-bootstrap';
+import {
+	Panel,
+	Glyphicon,
+	Row,
+	Col,
+	ListGroup,
+	ListGroupItem
+} from 'components/react-bootstrap';
 import moment from 'moment';
+import ProjectDisplay from 'components/ProjectDisplay';
+import Icon from 'components/Icon';
 
 const HomePage = props => (
 	<div>
 		<h1>Welcome to CIRCUS DB!</h1>
 		<ul className='home-menu'>
-			<Menu link='/browse/case' icon='case-search' title='Case Search'
+			<Menu
+				link='/browse/case'
+				icon='case-search'
+				title='Case Search'
 				description='Search and edit existing cases.'
 			/>
-			<Menu link='/browse/series' icon='series-search' title='Series Search'
+			<Menu
+				link='/browse/series'
+				icon='series-search'
+				title='Series Search'
 				description='Search and edit existing cases.'
 			/>
-			<Menu link='/import-series' icon='series-import' title='Series Import'
+			<Menu
+				link='/import-series'
+				icon='series-import'
+				title='Series Import'
 				description='Upload DICOM image files directly via the browser.'
 			/>
 		</ul>
@@ -25,61 +42,108 @@ const HomePage = props => (
 export default HomePage;
 
 function role2str(role) {
-	return role.replace(/Groups$/, '')
-		.replace(/([A-Z])/g, (m, s) => (' ' + s.toLowerCase()));
+	return role
+		.replace(/Groups$/, '')
+		.replace(/([A-Z])/g, (m, s) => ' ' + s.toLowerCase());
 }
 
-const ProfileView = ({ user }) => {
-	const lastLoginTime = moment(user.lastLoginTime).format('YYYY-MM-DD HH:mm');
-	return <Row>
-		<Col md={7}>
-			<Panel bsStyle='primary' header={<span><Glyphicon glyph='education'/> Your Projects</span>}>
-				<ListGroup fill>
-					{user.accessibleProjects.map(p => (
-						<ListGroupItem key={p.project.projectId}>
-							<strong>{p.project.projectName}</strong>&ensp;
-							<small>({p.roles.map(role2str).join(', ')})</small>
-						</ListGroupItem>
-					))}
-				</ListGroup>
-			</Panel>
-		</Col>
-		<Col md={5}>
-			<Panel bsStyle='primary' className='home-profile'
-				header={<span><Glyphicon glyph='user'/> Profile</span>}
-			>
-				<ListGroup fill>
-					<ListGroupItem>
-						You are logged in as: <b>{user.description}</b>
+const MyProjects = ({ user }) => {
+	return (
+		<Panel
+			bsStyle='primary'
+			header={
+				<span>
+					<Glyphicon glyph='education' /> My Projects
+				</span>
+			}
+		>
+			<ListGroup fill>
+				{user.accessibleProjects.map(p => (
+					<ListGroupItem key={p.project.projectId}>
+						<div>
+							<ProjectDisplay
+								projectId={p.projectId}
+								size='lg'
+								withName
+								withDescription
+							/>
+						</div>
+						<small>({p.roles.map(role2str).join(', ')})</small>
 					</ListGroupItem>
-					{ user.globalPrivileges.indexOf('manageServer') > -1 ?
-						<ListGroupItem bsStyle='danger'>
-							<strong>You have administrative privilege on CIRCUS DB! </strong>
-							Use this account only when you do administrative tasks.
-						</ListGroupItem>
-						: null }
-				</ListGroup>
-				<ul className='list-unstyled'>
-					<li><b>Login ID:</b> {user.loginId}</li>
-					<li><b>Email:</b> {user.userEmail}</li>
-					<li><b>Last login:</b> {lastLoginTime} (from {user.lastLoginIp})</li>
-					<li><b>Groups:</b> {user.groups.join(', ')}</li>
-					<li><b>Domains:</b> {user.domains.join(', ')}</li>
-				</ul>
-			</Panel>
-		</Col>
-	</Row>;
+				))}
+			</ListGroup>
+		</Panel>
+	);
 };
 
-const Profile = connect(
-	state => ({ user: state.loginUser.data })
-)(ProfileView);
+const MyProfile = ({ user }) => {
+	const lastLoginTime = moment(user.lastLoginTime).format('YYYY-MM-DD HH:mm');
+	return (
+		<Panel
+			bsStyle='primary'
+			className='home-profile'
+			header={
+				<span>
+					<Glyphicon glyph='user' /> Profile
+				</span>
+			}
+		>
+			<ListGroup fill>
+				<ListGroupItem>
+					You are logged in as: <b>{user.description}</b>
+				</ListGroupItem>
+				{user.globalPrivileges.indexOf('manageServer') > -1 ? (
+					<ListGroupItem bsStyle='danger'>
+						<Icon icon='warning-sign' />
+						<strong>
+							You have administrative privilege on CIRCUS DB!
+						</strong>
+						&ensp;
+						Use this account only when you do administrative tasks.
+					</ListGroupItem>
+				) : null}
+			</ListGroup>
+			<ul className='list-unstyled'>
+				<li>
+					<b>Login ID:</b> {user.loginId}
+				</li>
+				<li>
+					<b>Email:</b> {user.userEmail}
+				</li>
+				<li>
+					<b>Last login:</b> {lastLoginTime} (from {user.lastLoginIp})
+				</li>
+				<li>
+					<b>Groups:</b> {user.groups.join(', ')}
+				</li>
+				<li>
+					<b>Domains:</b> {user.domains.join(', ')}
+				</li>
+			</ul>
+		</Panel>
+	);
+};
+
+const ProfileView = ({ user }) => {
+	return (
+		<Row>
+			<Col md={7}>
+				<MyProjects user={user} />
+			</Col>
+			<Col md={5}>
+				<MyProfile user={user} />
+			</Col>
+		</Row>
+	);
+};
+
+const Profile = connect(state => ({ user: state.loginUser.data }))(ProfileView);
 
 const Menu = ({ link, icon, title, description }) => (
 	<li>
 		<Link to={link}>
 			<div className='img'>
-				<span className={'circus-icon circus-icon-' + icon}></span>
+				<span className={'circus-icon circus-icon-' + icon} />
 			</div>
 			<p>{title}</p>
 		</Link>

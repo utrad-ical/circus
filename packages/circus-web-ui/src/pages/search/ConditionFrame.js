9605aa210f3140da9e256f164d0909cb2dd5b75b
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-19 21:48:50 +0900

    Convert ConditionFrame to SFC

--- packages/circus-web-ui/src/pages/search/ConditionFrame.js
+++ packages/circus-web-ui/src/pages/search/ConditionFrame.js
@@ -4,92 +4,88 @@ import IconButton from 'rb/IconButton';
 import ConditionEditor, { conditionToMongoQuery } from 'rb/ConditionEditor';
 import { Tag } from 'components/tag';
 
-export default class ConditionFrame extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleChangeType = this.handleChangeType.bind(this);
-		this.handleBasicFilterChange = this.handleBasicFilterChange.bind(this);
-		this.handleAdvancedFilterChange = this.handleAdvancedFilterChange.bind(this);
-		this.handleResetClick = this.handleResetClick.bind(this);
-		this.handleSearchClick = this.handleSearchClick.bind(this);
-	}
-
-	handleChangeType(key) {
+const ConditionFrame = props => {
+	function handleChangeType(key) {
 		const type = key === 1 ? 'basic' : 'advanced';
-		this.props.onChange({ ...this.props.condition, type });
+		props.onChange({ ...props.condition, type });
 	}
 
-	handleBasicFilterChange(basicFilter) {
-		this.props.onChange({ ...this.props.condition, basicFilter });
+	function handleBasicFilterChange(basicFilter) {
+		props.onChange({ ...props.condition, basicFilter });
 	}
 
-	handleAdvancedFilterChange(advancedFilter) {
-		this.props.onChange({ ...this.props.condition, advancedFilter });
+	function handleAdvancedFilterChange(advancedFilter) {
+		props.onChange({ ...props.condition, advancedFilter });
 	}
 
-	handleResetClick() {
-		this.props.onChange(this.props.nullCondition());
+	function handleResetClick() {
+		props.onChange(props.nullCondition());
 	}
 
-	handleSearchClick() {
+	function handleSearchClick() {
 		let condition;
-		if (this.props.condition.type === 'basic') {
-			condition = this.props.basicFilterToMongoQuery(this.props.condition.basicFilter);
+		if (props.condition.type === 'basic') {
+			condition = props.basicFilterToMongoQuery(
+				props.condition.basicFilter
+			);
 		} else {
-			condition = conditionToMongoQuery(this.props.condition.advancedFilter);
+			condition = conditionToMongoQuery(
+				props.condition.advancedFilter
+			);
 		}
-		this.props.onSearch && this.props.onSearch(condition);
+		props.onSearch && props.onSearch(condition);
 	}
 
-	render() {
-		const {
-			basicConditionForm: BasicConditionForm,
-			formParams = {}
-		} = this.props;
-		const activeKey = this.props.condition.type === 'advanced' ? 2 : 1;
+	const {
+		basicConditionForm: BasicConditionForm,
+		formParams = {}
+	} = props;
+	const activeKey = props.condition.type === 'advanced' ? 2 : 1;
 
-		return <div>
+	return (
+		<div>
 			<Tabs
 				animation={false}
 				id='search-condition-tabs'
 				activeKey={activeKey}
-				onSelect={this.handleChangeType}
+				onSelect={handleChangeType}
 			>
 				<Tab eventKey={1} title='Basic'>
 					<BasicConditionForm
-						value={this.props.condition.basicFilter}
-						onChange={this.handleBasicFilterChange}
+						value={props.condition.basicFilter}
+						onChange={handleBasicFilterChange}
 						{...formParams}
 					/>
 				</Tab>
 				<Tab eventKey={2} title='Advanced'>
 					<ConditionEditor
-						keys={this.props.conditionKeys}
-						value={this.props.condition.advancedFilter}
-						onChange={this.handleAdvancedFilterChange}
+						keys={props.conditionKeys}
+						value={props.condition.advancedFilter}
+						onChange={handleAdvancedFilterChange}
 					/>
 				</Tab>
 			</Tabs>
 			<div className='search-buttons'>
-				<Button bsStyle='link'
-					onClick={this.handleResetClick}
-				>
+				<Button bsStyle='link' onClick={handleResetClick}>
 					Reset
 				</Button>
 				&ensp;
 				<IconButton
-					bsStyle='primary' icon='search'
-					onClick={this.handleSearchClick}
+					bsStyle='primary'
+					icon='search'
+					onClick={handleSearchClick}
 				>
 					Search
 				</IconButton>
 			</div>
-			{ /* <div>{JSON.stringify(this.props.condition)}</div> */ }
-		</div>;
-	}
+		</div>
+	);
+};
 
-}
+export default ConditionFrame;
 
 export const ProjectRenderer = props => <span>{props.projectName}</span>;
 
-export const TagRenderer = props => <Tag name={props.name} color={props.color} />;
+export const TagRenderer = props => (
+	<Tag name={props.name} color={props.color} />
+);

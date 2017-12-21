b19e35fb1bbf7ebda0ae1347dddf01bcbeff0f6d
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-21 12:19:15 +0900

    Add historyApiFallback option

--- packages/circus-web-ui/webpack.config.js
+++ packages/circus-web-ui/webpack.config.js
@@ -63,7 +63,8 @@ module.exports = {
 		})
 	],
 	devServer: {
-		contentBase: path.join(__dirname, 'public')
+		contentBase: path.join(__dirname, 'public'),
+		historyApiFallback: true
 	},
 	devtool: '#sourcemap'
 };

08f315b9f76717ee83382a2942a61d3f675d2b48
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-21 16:05:02 +0900

    Fix broken login-related code

--- packages/circus-web-ui/src/actions/login-management.js
+++ packages/circus-web-ui/src/actions/login-management.js
@@ -10,17 +10,19 @@ export function refreshUserInfo(full = false) {
     dispatch({ type: 'REQUEST_LOGIN_INFO' });
     let result;
     try {
-      result = await api('login-info' + (full ? '/full' : ''));
+      result = await api('login-info' + (full ? '/full' : ''), {
+        handleErrors: [401]
+      });
       if (typeof result.userEmail !== 'string') {
         throw Error('Server did not respond with valid user data');
       }
+      if (full) {
+        dispatch({ type: 'LOAD_FULL_LOGIN_INFO', loginUser: result });
+      } else {
+        dispatch({ type: 'CONFIRM_LOGIN_INFO' });
+      }
     } catch (err) {
       dispatch({ type: 'LOGGED_OUT' });
-    }
-    if (full) {
-      dispatch({ type: 'LOAD_FULL_LOGIN_INFO', loginUser: result });
-    } else {
-      dispatch({ type: 'CONFIRM_LOGIN_INFO' });
     }
   };
 }

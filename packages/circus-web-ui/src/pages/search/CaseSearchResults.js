b76fdc191508219359c6ae25ec9e38294dd9288c
Soichiro Miki <smiki-tky@umin.ac.jp>
2017-12-26 13:04:56 +0900

    Move PatientInfoBox

--- packages/circus-web-ui/src/pages/search/CaseSearchResults.js
+++ packages/circus-web-ui/src/pages/search/CaseSearchResults.js
@@ -3,7 +3,7 @@ import SearchResultsView, { makeSortOptions } from './SearchResultsView';
 import { Button } from 'components/react-bootstrap';
 import { Link } from 'react-router';
 import DataGrid from 'components/DataGrid';
-import PatientInfoBox from 'pages/search/PatientInfoBox';
+import PatientInfoBox from 'components/PatientInfoBox';
 import ProjectDisplay from 'components/ProjectDisplay';
 import ProjectTag from 'components/ProjectTag';
 import TimeDisplay from 'components/TimeDisplay';

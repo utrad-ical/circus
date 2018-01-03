b2a6ad9c295215dc455dd892595ba12761309816
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-01-03 14:53:53 +0900

    Allow PATCH and DELETE as cors request methods

--- packages/circus-api/src/middleware/cors.js
+++ packages/circus-api/src/middleware/cors.js
@@ -4,7 +4,10 @@
 export default function cors(origin = '*') {
   return async function cors(ctx, next) {
     ctx.response.set('Access-Control-Allow-Origin', origin);
-    ctx.response.set('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS');
+    ctx.response.set(
+      'Access-Control-Allow-Methods',
+      'POST, GET, PUT, OPTIONS, PATCH, DELETE'
+    );
     ctx.response.set('Access-Control-Allow-Credentials', 'true');
     ctx.response.set(
       'Access-Control-Allow-Headers',

b194b22c926b93667f17e502bb50023e81e61d35
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-04-05 13:59:48 +0900

    Respect voxel coordinate

--- packages/circus-rs/src/browser/annotation/PlaneFigure.ts
+++ packages/circus-rs/src/browser/annotation/PlaneFigure.ts
@@ -2,14 +2,48 @@ import Annotation from './Annotation';
 import Viewer from '../viewer/Viewer';
 import ViewState from '../ViewState';
 import Sprite from '../viewer/Sprite';
+import { Vector2, Vector3 } from 'three';
+import {
+  convertVolumeCoordinateToScreenCoordinate,
+  detectOrthogonalSection
+} from '../section-util';
 
 export type FigureType = 'rectangle' | 'circle';
 
 export default class PlaneFigure implements Annotation {
+  /**
+   * Color of the outline.
+   */
   public color: string = '#ffffff';
+
+  /**
+   * Width of the outline.
+   */
   public width: number = 3;
+
+  /**
+   * Boundary of the outline, measured in mm.
+   */
   public min?: number[];
+
+  /**
+   * Boundary of the outline, measured in mm.
+   */
   public max?: number[];
+
+  /**
+   * The Z coordinate of the outline.
+   */
+  public z?: number;
+
+  /**
+   * Displays the outline when z is below this threshold.
+   */
+  public zThreshold: number = 0.1;
+
+  public dimmedColor: string = '#ffffff55';
+  public zDimmedThreshold: number = 3;
+
   public type: FigureType = 'circle';
 
   public draw(viewer: Viewer, viewState: ViewState): Sprite | null {
@@ -19,32 +53,44 @@ export default class PlaneFigure implements Annotation {
     if (viewState.type !== 'mpr') return null;
     const ctx = canvas.getContext('2d');
     if (!ctx) return null;
+
+    // Displays only when the volume is displayed as an axial slice
+    const orientation = detectOrthogonalSection(viewState.section);
+    if (orientation !== 'axial') return null;
+
+    const resolution = new Vector2().fromArray(viewer.getResolution());
     if (!this.color || !this.min || !this.max) return null;
 
-    const min = this.min;
-    const max = this.max;
+    if (this.z === undefined) return null;
+    const zDiff = Math.abs(this.z - viewState.section.origin[2]);
+    if (zDiff > this.zDimmedThreshold) return null;
+    const color = zDiff > this.zThreshold ? this.dimmedColor : this.color;
+
+    const min = convertVolumeCoordinateToScreenCoordinate(
+      viewState.section,
+      resolution,
+      new Vector3(this.min[0], this.min[1], this.z ? this.z : 0)
+    );
+    const max = convertVolumeCoordinateToScreenCoordinate(
+      viewState.section,
+      resolution,
+      new Vector3(this.max[0], this.max[1], this.z ? this.z : 0)
+    );
+
     try {
       ctx.save();
-      ctx.strokeStyle = this.color;
+      ctx.strokeStyle = color;
       ctx.lineWidth = this.width;
       ctx.beginPath();
       if (this.type === 'circle') {
         ctx.save(); // Nested save to do path translation
-        ctx.translate(this.min[0], this.min[1]);
-        ctx.scale(
-          (this.max[0] - this.min[0]) / 2,
-          (this.max[1] - this.min[1]) / 2
-        );
+        ctx.translate(min.x, min.y);
+        ctx.scale((max.x - min.x) / 2, (max.y - min.y) / 2);
         ctx.arc(1, 1, 1, 0, 2 * Math.PI);
         ctx.restore();
         ctx.stroke();
       } else {
-        ctx.rect(
-          this.min[0],
-          this.min[1],
-          this.max[0] - this.min[0],
-          this.max[1] - this.min[1]
-        );
+        ctx.rect(min.x, min.y, max.x - min.x, max.y - min.y);
         ctx.stroke();
       }
     } finally {
--- packages/circus-rs/src/browser/tool/annotation/PlaneFigureTool.ts
+++ packages/circus-rs/src/browser/tool/annotation/PlaneFigureTool.ts
@@ -1,9 +1,12 @@
 import { Vector2, Vector3 } from 'three';
 import DraggableTool from '../DraggableTool';
 import Viewer from '../../viewer/Viewer';
-import ViewState from '../../ViewState';
+import ViewState, { MprViewState } from '../../ViewState';
 import ViewerEvent from '../../viewer/ViewerEvent';
-import { convertScreenCoordinateToVolumeCoordinate } from '../../section-util';
+import {
+  convertScreenCoordinateToVolumeCoordinate,
+  detectOrthogonalSection
+} from '../../section-util';
 import { Section, vectorizeSection } from '../../../common/geometry';
 import PlaneFigure, { FigureType } from '../../annotation/PlaneFigure';
 
@@ -19,11 +22,24 @@ export default class PlaneFigureTool extends DraggableTool {
     const viewState = ev.viewer.getState();
     if (!viewState || viewState.type !== 'mpr') return;
 
+    const section = viewState.section;
+    const orientation = detectOrthogonalSection(section);
+    if (orientation !== 'axial') return;
+
     const comp = ev.viewer.getComposition();
+    const resolution = new Vector2().fromArray(ev.viewer.getResolution());
     if (!comp) return;
     const fig = new PlaneFigure();
     fig.type = this.figureType;
-    fig.min = [ev.viewerX!, ev.viewerY!];
+
+    const min = convertScreenCoordinateToVolumeCoordinate(
+      section,
+      resolution,
+      new Vector2(ev.viewerX!, ev.viewerY!)
+    );
+    fig.min = [min.x, min.y];
+    fig.z = min.z;
+
     comp.addAnnotation(fig);
     comp.annotationUpdated();
     this.tmpAnnotation = fig;
@@ -33,12 +49,35 @@ export default class PlaneFigureTool extends DraggableTool {
     super.dragHandler(ev);
     const comp = ev.viewer.getComposition();
     if (!comp) return;
-    const fig = this.tmpAnnotation!;
-    fig.max = [ev.viewerX!, ev.viewerY!];
+    const fig = this.tmpAnnotation;
+    if (!fig) return;
+
+    const resolution = new Vector2().fromArray(ev.viewer.getResolution());
+    const max = convertScreenCoordinateToVolumeCoordinate(
+      (ev.viewer.getState() as MprViewState).section,
+      resolution,
+      new Vector2(ev.viewerX!, ev.viewerY!)
+    );
+    fig.max = [max.x, max.y];
     comp.annotationUpdated();
   }
 
   public dragEndHandler(ev: ViewerEvent): void {
+    super.dragEndHandler(ev);
+    const comp = ev.viewer.getComposition()!;
+    const fig = this.tmpAnnotation!;
+    if (!fig || !fig.min || !fig.max) return;
+    const newMin = [
+      Math.min(fig.min[0], fig.max[0]),
+      Math.min(fig.min[1], fig.max[1])
+    ];
+    const newMax = [
+      Math.max(fig.min[0], fig.max[0]),
+      Math.max(fig.min[1], fig.max[1])
+    ];
+    fig.min = newMin;
+    fig.max = newMax;
+    comp.annotationUpdated();
     this.tmpAnnotation = undefined;
   }
 }

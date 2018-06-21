c2ec962e5102a5715a29f2c6ab9e48c52de95457
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-06-22 02:01:41 +0900

    Format default.js

--- packages/circus-cs-core/config/default.js
+++ packages/circus-cs-core/config/default.js
@@ -11,73 +11,73 @@
 var path = require('path');
 
 module.exports = {
-	"temporaryDirBase": path.join(__dirname, "..", "tmp", "proc"),
-	"pluginConfigPath": path.join(__dirname, "plugins.yml"),
-	
-	"queue": {
-		"mongoURL": "mongodb://localhost:51081/cs_core",
-		"collectionTitle": "pluginJobQueue"
-	},
-	
-	"WebUI": {
-		"mongoURL": "mongodb://localhost:51081/cs_web_ui",
-		"collectionTitle": "pluginJobs",
-		"resultDir": path.join(__dirname, "..", "tmp", "result")
-	},
-	
-	// DICOM file repository is a loader that fetches the content of a DICOM file
-	// specified by a series instance UID and an image number.
-	"dicomFileRepository": {
-		"module": "StaticDicomFileRepository",
-		"options": {
-			"dataDir": path.join(__dirname, "..", "..", "var", "sample-dicom-data"),
-			"useHash": false
-		}
-	},
-	
-	"docker": {
-		"socketPath": "/var/run/docker.sock"
-	},
-	
-	"dicomDumpOptions": {
-		"dockerImage": "circus/dicom_voxel_dump:1.0",
-		"volumeIn": "/data/in",
-		"volumeOut": "/data/out"
-	},
-	
-	/**
-	 * Dequeue daemon settings (based on pm2)
-	 */
-	"daemon": {
-		// (pm2)
-		"script": "daemon.js",
-		
-		// (pm2) See interface StartOptions ( https://github.com/Unitech/pm2/blob/master/types/index.d.ts )
-		"startOptions": {
-			"name": "dequeue-daemon",
-			"cwd": path.join(__dirname, ".."),
-			"output": path.join(__dirname, "..", "logs", "daemon-pm2-output.log"),
-			"error": path.join(__dirname, "..", "logs", "daemon-pm2-error.log")
-			// env:
-		},
-		
-		"tick": 1000,
-		"waitOnFail": 60 * 1000
-	}
+  temporaryDirBase: path.join(__dirname, '..', 'tmp', 'proc'),
+  pluginConfigPath: path.join(__dirname, 'plugins.yml'),
 
-	// // Logger configurations. By default, we make use of log4js library,
-	// // so see the documentation for that project.
-	// "logger": {
-		// "module": "Log4JsLogger",
-		// "options": {
-			// "appenders": [
-				// {
-					// "type": "dateFile",
-					// "filename": path.resolve(__dirname, "../logs/debug.log"),
-					// "pattern": "-yyyyMMdd.log",
-					// "alwaysIncludePattern": true
-				// }
-			// ]
-		// }
-	// },
+  queue: {
+    mongoURL: 'mongodb://localhost:51081/cs_core',
+    collectionTitle: 'pluginJobQueue'
+  },
+
+  WebUI: {
+    mongoURL: 'mongodb://localhost:51081/cs_web_ui',
+    collectionTitle: 'pluginJobs',
+    resultDir: path.join(__dirname, '..', 'tmp', 'result')
+  },
+
+  // DICOM file repository is a loader that fetches the content of a DICOM file
+  // specified by a series instance UID and an image number.
+  dicomFileRepository: {
+    module: 'StaticDicomFileRepository',
+    options: {
+      dataDir: path.join(__dirname, '..', '..', 'var', 'sample-dicom-data'),
+      useHash: false
+    }
+  },
+
+  docker: {
+    socketPath: '/var/run/docker.sock'
+  },
+
+  dicomDumpOptions: {
+    dockerImage: 'circus/dicom_voxel_dump:1.0',
+    volumeIn: '/data/in',
+    volumeOut: '/data/out'
+  },
+
+  /**
+   * Dequeue daemon settings (based on pm2)
+   */
+  daemon: {
+    // (pm2)
+    script: 'daemon.js',
+
+    // (pm2) See interface StartOptions ( https://github.com/Unitech/pm2/blob/master/types/index.d.ts )
+    startOptions: {
+      name: 'dequeue-daemon',
+      cwd: path.join(__dirname, '..'),
+      output: path.join(__dirname, '..', 'logs', 'daemon-pm2-output.log'),
+      error: path.join(__dirname, '..', 'logs', 'daemon-pm2-error.log')
+      // env:
+    },
+
+    tick: 1000,
+    waitOnFail: 60 * 1000
+  },
+
+  // Logger configurations. By default, we make use of log4js library,
+  // so see the documentation for that project.
+  logger: {
+    module: 'Log4JsLogger',
+    options: {
+      appenders: [
+        {
+          type: 'dateFile',
+          filename: path.resolve(__dirname, '../logs/debug.log'),
+          pattern: '-yyyyMMdd.log',
+          alwaysIncludePattern: true
+        }
+      ]
+    }
+  }
 };

c24ae17140b082ff3eb003ecf808efbfcbb07edc
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-06-27 13:46:01 +0900

    Simplify test

--- packages/circus-cs-core/src/util/isDirectory.test.ts
+++ packages/circus-cs-core/src/util/isDirectory.test.ts
@@ -1,18 +1,13 @@
 import isDirectory from './isDirectory';
 
-describe('isDirectory', () => {
-  test('return true for true directory', async () => {
-    const result = await isDirectory(__dirname);
-    expect(result).toBe(true);
-  });
+test('return true for true directory', async () => {
+  expect(await isDirectory(__dirname)).toBe(true);
+});
 
-  test('return false for a file', async () => {
-    const result = await isDirectory(__filename);
-    expect(result).toBe(false);
-  });
+test('return false for a file', async () => {
+  expect(await isDirectory(__filename)).toBe(false);
+});
 
-  test('return false for nonexistent path', async () => {
-    const result = await isDirectory('/this/path/does/not/exist');
-    expect(result).toBe(false);
-  });
+test('return false for nonexistent path', async () => {
+  expect(await isDirectory('/this/path/does/not/exist')).toBe(false);
 });

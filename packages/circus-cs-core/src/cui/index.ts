698e96039b4b203cf8e3945443b18e7d56f897dd
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-06-27 07:18:40 +0000

    CUI: Lazy-load modules and print help messages

--- packages/circus-cs-core/src/cui/index.ts
+++ packages/circus-cs-core/src/cui/index.ts
@@ -1,35 +1,62 @@
 import * as minimist from 'minimist';
-import register from './register';
-import list_queue from './list_queue';
-import check_env from './check_env';
-import { start, stop, status, pm2list, pm2killall } from './daemon';
 
-const commands: { [key: string]: (argv: any) => Promise<void> } = {
-  register,
-  list_queue,
-  check_env,
-  start,
-  stop,
-  status,
-  pm2list,
-  pm2killall
+const commands: { [key: string]: any } = {
+  register: {
+    help: 'Registeres a new job.'
+  },
+  list_queue: {
+    help: 'Lists queue items.'
+  },
+  check_env: {
+    help: 'Performs a system integrity check.'
+  },
+  start: {
+    help: 'Starts the job manager.',
+    module: './daemon'
+  },
+  stop: {
+    help: 'Stops the job manager.',
+    module: './daemon'
+  },
+  status: {
+    help: 'Shows the status of the job manager.',
+    module: './daemon'
+  },
+  pm2list: {
+    help: 'Shows job manager process detail.',
+    module: './daemon'
+  },
+  pm2killall: {
+    help: 'Force-kills job manager processes.',
+    module: './daemon'
+  },
+  help: {
+    help: 'Prints this help message.'
+  }
 };
 
 async function main() {
   const argv = process.argv.slice(2);
-  const command = argv.shift();
+  const commandName = argv.shift();
   const args = minimist(argv);
-  const callFunc = command ? commands[command] : undefined;
 
-  if (!callFunc) {
-    if (command) {
-      console.error(`"${command}" is not supported.`);
-    } else {
-      console.log('Available commands:');
-      Object.keys(commands).forEach(i => console.log(' - ' + i));
-    }
+  if (!commandName || commandName === 'help') {
+    console.log('Available commands:');
+    Object.keys(commands).forEach(key =>
+      console.log(`  ${key}: ${commands[key].help}`)
+    );
     return;
   }
+  if (!(commandName in commands)) {
+    console.error(`Unknown command: "${commandName}".`);
+    return;
+  }
+
+  const command = commands[commandName];
+
+  const callFunc: (args: any) => Promise<void> = command.module
+    ? (await import(command.module))[commandName]
+    : (await import(`./${commandName}`)).default;
 
   try {
     await callFunc(args);

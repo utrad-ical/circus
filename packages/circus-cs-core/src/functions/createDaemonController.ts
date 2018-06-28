94b2047ddd70cacf91097da9c3b11d30545da4e9
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-06-28 20:55:42 +0900

    Use pify to promisify pm2 library

--- packages/circus-cs-core/src/functions/createDaemonController.ts
+++ packages/circus-cs-core/src/functions/createDaemonController.ts
@@ -3,7 +3,12 @@
  * @module
  */
 
-import * as aapm2 from '../util/aapm2';
+// import * as aapm2 from '../util/aapm2';
+
+import * as _pm2 from 'pm2';
+import * as pify from 'pify';
+
+const pm2: any = pify(_pm2);
 
 const script = 'daemon.js';
 
@@ -16,40 +21,40 @@ interface Options {
 
 export default function createDaemonController(startOptions: Options) {
   const execute = async (task: Function) => {
-    await aapm2.connect();
+    await pm2.connect();
     try {
       return await task();
     } finally {
-      aapm2.disconnect();
+      pm2.disconnect();
     }
   };
 
   const start = async () => {
     execute(async () => {
-      const processList = await aapm2.describe(startOptions.name);
+      const processList = await pm2.describe(startOptions.name);
       if (processList.length > 0) throw new Error('Daemon already running.');
-      await aapm2.start(script, startOptions);
+      await pm2.start(script, startOptions);
     });
   };
 
   const stop = async () => {
     execute(async () => {
-      const processList = await aapm2.describe(startOptions.name);
+      const processList = await pm2.describe(startOptions.name);
       if (processList.length === 0) throw Error('Daemon is not running');
-      await aapm2.delete(startOptions.name);
+      await pm2.delete(startOptions.name);
     });
   };
 
   const status: () => Promise<'running' | 'stopped'> = async () => {
     return execute(async () => {
-      const processList = await aapm2.describe(startOptions.name);
+      const processList = await pm2.describe(startOptions.name);
       return processList.length > 0 ? 'running' : 'stopped';
     });
   };
 
   const pm2list = async () => {
     return execute(async () => {
-      const processList = await aapm2.list();
+      const processList = (await pm2.list()) as _pm2.ProcessDescription[];
       processList.forEach(process => {
         const { pid, name, pm_id, monit } = process;
         const { memory, cpu } = monit || { memory: null, cpu: null };
@@ -60,13 +65,13 @@ export default function createDaemonController(startOptions: Options) {
 
   const pm2killall = async () => {
     execute(async () => {
-      const killAndPrint = async (r: aapm2.ProcessDescription) => {
+      const killAndPrint = async (r: _pm2.ProcessDescription) => {
         const { pid, name, pm_id, monit } = r;
         const { memory, cpu } = monit || { memory: null, cpu: null };
         console.log({ pid, name, pm_id, memory, cpu });
-        if (pid && pm_id) await aapm2.delete(pm_id);
+        if (pid && pm_id) await pm2.delete(pm_id);
       };
-      const processList = await aapm2.list();
+      const processList = (await pm2.list()) as _pm2.ProcessDescription[];
       await Promise.all(processList.map(r => killAndPrint(r)));
     });
   };

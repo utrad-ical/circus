317ab0873300b096b6eb2a524befdd1165871aee
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-08-13 18:31:47 +0900

    Revert making 'circus-db' subdirectory

---
+++ packages/circus-web-ui/src/components/SearchResultsView.js
@@ -0,0 +1,133 @@
+import React, { Fragment } from 'react';
+import ShrinkSelect from 'rb/ShrinkSelect';
+import { Pagination } from 'components/react-bootstrap';
+import {
+  changeSearchPage,
+  changeSearchSort,
+  changeSearchLimit,
+  refreshSearch
+} from 'actions';
+import LoadingIndicator from 'rb/LoadingIndicator';
+import { connect } from 'react-redux';
+import AutoReloadSwitch from './AutoReloadSwitch';
+import Icon from 'components/Icon';
+
+export const makeSortOptions = sortKeys => {
+  const options = {};
+  Object.keys(sortKeys).forEach(k => {
+    options[`{"${k}":-1}`] = `${sortKeys[k]} desc`;
+    options[`{"${k}":1}`] = `${sortKeys[k]} asc`;
+  });
+  return options;
+};
+
+const limitOptions = [10, 20, 50, 100, 200];
+
+const ItemsPerPageOptionRenderer = props => (
+  <Fragment>{props.caption} items</Fragment>
+);
+
+/**
+ * SearchResultsView is connected to `search` redux state and
+ * displays the search results along with pager and sort changer.
+ */
+const SearchResultsView = props => {
+  const {
+    dispatch,
+    search,
+    name,
+    sortOptions,
+    dataView: DataView,
+    active,
+    refreshable,
+    ...rest
+  } = props;
+  if (!search) return null;
+  const { isFetching, totalItems, limit, items, page, sort } = search;
+
+  if (isFetching && !Array.isArray(items))
+    return <LoadingIndicator delay={1000} />;
+
+  if (!Array.isArray(items)) return null; // This should not happen
+
+  function handleSortChange(newSort) {
+    if (newSort === sort) return;
+    dispatch(changeSearchSort(name, newSort));
+  }
+
+  function handlePageClick(newPage) {
+    if (newPage === page) return;
+    dispatch(changeSearchPage(name, newPage));
+  }
+
+  function handleLimitChange(newLimit) {
+    if (newLimit === limit) return;
+    dispatch(changeSearchLimit(name, newLimit));
+  }
+
+  function handleRefresh() {
+    if (isFetching) return;
+    dispatch(refreshSearch(name));
+  }
+
+  const pages = Math.ceil(totalItems / limit);
+  return (
+    <div className="search-results-container">
+      <div className="search-results-header">
+        {totalItems + ' Result' + (totalItems > 1 ? 's' : '')}
+        &emsp;
+        <ShrinkSelect
+          bsSize="sm"
+          options={limitOptions}
+          value={limit + ''}
+          onChange={handleLimitChange}
+          disabled={isFetching}
+          renderer={ItemsPerPageOptionRenderer}
+          numericalValue
+        />
+        {refreshable && (
+          <Fragment>
+            &emsp;
+            <AutoReloadSwitch
+              bsSize="sm"
+              onRefresh={handleRefresh}
+              disabled={isFetching}
+            />
+          </Fragment>
+        )}
+        {sortOptions && (
+          <Fragment>
+            &emsp;
+            <Icon icon="sort" />&ensp;
+            <ShrinkSelect
+              bsSize="sm"
+              options={sortOptions}
+              value={sort}
+              onChange={handleSortChange}
+              disabled={isFetching}
+            />
+          </Fragment>
+        )}
+      </div>
+      <DataView value={items} active={active} {...rest} />
+      <div className="search-results-pager">
+        <Pagination
+          prev
+          next
+          first
+          last
+          ellipsis
+          items={pages}
+          maxButtons={10}
+          activePage={page}
+          onSelect={handlePageClick}
+          disabled={isFetching}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default connect((state, ownProps) => ({
+  search: state.searches[ownProps.name]
+}))(SearchResultsView);
---
+++ packages/circus-web-ui/src/pages/case-detail/LabelSelector.js
@@ -0,0 +1,243 @@
+import React from 'react';
+import ColorPicker from 'rb/ColorPicker';
+import {
+  Popover,
+  Button,
+  OverlayTrigger
+} from '../../components/react-bootstrap';
+import { RawData, PixelFormat, VoxelCloud } from 'circus-rs';
+import classNames from 'classnames';
+import { confirm } from 'rb/modal';
+import IconButton from 'components/IconButton';
+import Icon from 'components/Icon';
+import Slider from 'rb/Slider';
+
+const labelColors = [
+  '#ff0000',
+  '#00ff00',
+  '#ffff00',
+  '#0000ff',
+  '#ff00ff',
+  '#00ffff',
+  '#ff4400',
+  '#ff0044',
+  '#88ff00',
+  '#afc6fc',
+  '#ff5e6e',
+  '#aa4433',
+  '#ff8888',
+  '#ffff88',
+  '#aaffaa',
+  '#ff88ff'
+];
+
+const LabelSelector = props => {
+  const {
+    revision,
+    onChange,
+    onChangeActiveLabel,
+    activeLabel,
+    activeSeries
+  } = props;
+
+  function changeSeries(index, newSeries) {
+    const newRev = {
+      ...revision,
+      series: [...revision.series]
+    };
+    newRev.series[index] = newSeries;
+    onChange(newRev);
+  }
+
+  return (
+    <ul className="case-series-list">
+      {revision.series.map((series, seriesIndex) => (
+        <Series
+          series={series}
+          index={seriesIndex}
+          key={series.seriesUid}
+          onChange={changeSeries}
+          onChangeActiveLabel={onChangeActiveLabel}
+          activeSeries={activeSeries}
+          activeLabel={activeLabel}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default LabelSelector;
+
+const Series = props => {
+  const {
+    index: seriesIndex,
+    series,
+    activeSeries,
+    activeLabel,
+    onChange,
+    onChangeActiveLabel
+  } = props;
+
+  function changeLabel(labelIndex, label) {
+    const newSeries = {
+      ...series,
+      labels: [...series.labels]
+    };
+    newSeries.labels[labelIndex] = label;
+    onChange(seriesIndex, newSeries);
+  }
+
+  function addLabel() {
+    const cloud = new VoxelCloud();
+    cloud.color = '#ff0000';
+    cloud.alpha = 1;
+    cloud.origin = [0, 0, 0];
+    cloud.volume = new RawData([16, 16, 16], PixelFormat.Binary);
+    const newLabel = {
+      type: 'voxel',
+      data: {}, // will be filled just before actual save
+      attributes: [],
+      cloud // cloud is the primary data storage while editing
+    };
+    const newSeries = {
+      ...series,
+      labels: [...series.labels, newLabel]
+    };
+    onChange(seriesIndex, newSeries);
+  }
+
+  async function removeLabel(labelIndex) {
+    if (!await confirm('Delete this label?')) return;
+    const newSeries = {
+      ...series,
+      labels: series.labels.filter((l, i) => labelIndex !== i)
+    };
+    onChange(seriesIndex, newSeries);
+  }
+
+  return (
+    <li
+      className={classNames('case-series-list-item', {
+        active: series === activeSeries
+      })}
+    >
+      <Icon icon="circus-series" /> Series #{seriesIndex}
+      <ul className="case-label-list">
+        {series.labels.map((label, labelIndex) => (
+          <Label
+            label={label}
+            activeLabel={activeLabel}
+            index={labelIndex}
+            key={labelIndex}
+            onChange={changeLabel}
+            onClick={() => onChangeActiveLabel(seriesIndex, labelIndex)}
+            onRemoveClick={() => removeLabel(labelIndex)}
+          />
+        ))}
+      </ul>
+      <div className="case-label-buttons">
+        <IconButton icon="plus" bsSize="xs" onClick={addLabel}>
+          Add Label
+        </IconButton>
+      </div>
+    </li>
+  );
+};
+
+export const Label = props => {
+  const {
+    label,
+    index: labelIndex,
+    activeLabel,
+    onChange,
+    onClick,
+    onRemoveClick
+  } = props;
+  const caption = label.title ? label.title : `Label #${props.index}`;
+
+  function changeLabelAlpha(alpha) {
+    label.cloud.alpha = alpha;
+    onChange(labelIndex, label);
+  }
+
+  function changeLabelColor(color) {
+    label.cloud.color = color;
+    onChange(labelIndex, label);
+  }
+
+  // console.log(`Cloud #${labelIndex}`, label.cloud);
+
+  return (
+    <li
+      className={classNames('label-list-item', {
+        active: label === activeLabel
+      })}
+      onClick={onClick}
+    >
+      <div>
+        <Icon icon="tag" /> {caption}
+      </div>
+      <div>
+        <OpacityEditor value={label.cloud.alpha} onChange={changeLabelAlpha} />
+        <ColorPicker
+          bsSize="xs"
+          value={label.cloud.color}
+          colors={labelColors}
+          onChange={changeLabelColor}
+        />
+        <IconButton icon="remove" bsSize="xs" onClick={onRemoveClick} />
+      </div>
+    </li>
+  );
+};
+
+const OpacityEditor = props => {
+  const opacityEditor = (
+    <Popover id="opacity-editor">
+      <OpacityPopover {...props} />
+    </Popover>
+  );
+  return (
+    <OverlayTrigger
+      trigger="click"
+      rootClose
+      overlay={opacityEditor}
+      placement="bottom"
+    >
+      <Button bsStyle="default" bsSize="xs">
+        {props.value * 100}%
+      </Button>
+    </OverlayTrigger>
+  );
+};
+
+const OpacityPopover = props => {
+  const { onChange, value } = props;
+  return (
+    <div className="label-list-opacity-dropdown">
+      {value === 0 ? (
+        <IconButton
+          icon="eye-close"
+          bsStyle="link"
+          bsSize="sm"
+          onClick={() => onChange(1)}
+        />
+      ) : (
+        <IconButton
+          icon="eye-open"
+          bsStyle="link"
+          bsSize="sm"
+          onClick={() => onChange(0)}
+        />
+      )}
+      <Slider
+        className="opacity-slider"
+        min={0}
+        max={100}
+        step={10}
+        value={value * 100}
+        onChange={v => props.onChange(v / 100)}
+      />
+    </div>
+  );
+};

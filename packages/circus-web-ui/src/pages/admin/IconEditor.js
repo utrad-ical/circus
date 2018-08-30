4b6bb1b218c1d5a02ea5a9d313083907c3cdb6c9
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-08-30 16:07:18 +0900

    Extract IconEditor

---
+++ packages/circus-web-ui/src/pages/admin/IconEditor.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ProjectIcon from 'components/ProjectIcon';
+import PropertyEditor from 'rb/PropertyEditor';
+import ShrinkSelect from 'rb/ShrinkSelect';
+import ColorPicker from 'rb/ColorPicker';
+
+const GlyphPicker = props => {
+  const glyphs = [
+    'stomach',
+    'brain',
+    'lung',
+    'liver',
+    'bone',
+    'breast',
+    'heart',
+    'colon',
+    'face',
+    'abdomen',
+    'joint',
+    'kidney',
+    'artery',
+    'pancreas',
+    'calc',
+    'visualize',
+    'measure',
+    'cpu',
+    'scanner',
+    'atom',
+    'person'
+  ];
+  const options = {};
+  glyphs.forEach(
+    g =>
+      (options[g] = {
+        caption: (
+          <ProjectIcon
+            icon={{ glyph: g, color: '#000000', backgroundColor: '#ffffff' }}
+            size="lg"
+          />
+        )
+      })
+  );
+  return <ShrinkSelect options={options} {...props} />;
+};
+
+const iconProperties = [
+  { key: 'glyph', caption: 'Image', editor: GlyphPicker },
+  { key: 'color', caption: 'Color', editor: ColorPicker },
+  { key: 'backgroundColor', caption: 'Background', editor: ColorPicker }
+];
+
+const IconEditor = props => (
+  <PropertyEditor properties={iconProperties} {...props} />
+);
+
+export default IconEditor;

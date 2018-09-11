53c68ce031667517f15d6cd110d16c1ea3e1101f
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-09-11 23:59:02 +0900

    Add stub plugin-job detail page

---
+++ packages/circus-web-ui/src/pages/plugin-job-detail/LesionCandidates.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ImageViewer from 'components/ImageViewer';
+import * as rs from 'circus-rs';
+import { connect } from 'react-redux';
+
+const Candidate = props => {
+  const {
+    value,
+    index,
+    onFeedbackChange,
+    feedbackListener: FeedbackListener,
+    feedback,
+    composition
+  } = props;
+  return (
+    <div className="lesion-candidate">
+      <div>Rank: {value.rank}</div>
+      <div>
+        Loc: [{value.location_z}, {value.location_y}, {value.location_z}]
+      </div>
+      <div>Confidence: {value.confidence}</div>
+      <ImageViewer
+        className="lesion-candidate-viewer"
+        composition={composition}
+        tool="pager"
+        initialTool="pager"
+      />
+      <FeedbackListener
+        value={feedback}
+        options={['TP', 'FP', 'pending']}
+        onChange={val => onFeedbackChange(index, val)}
+      />
+    </div>
+  );
+};
+
+class LesionCandidatesView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { compositoin: null };
+  }
+
+  async componentDidMount() {
+    const { user, job } = this.props;
+    const seriesUid = job.series[0].seriesUid;
+    const server = user.data.dicomImageServer;
+    const client = new rs.RsHttpClient(server);
+    const src = new rs.HybridImageSource({ client, series: seriesUid });
+    const composition = new rs.Composition(src);
+    this.setState({ composition });
+  }
+
+  handleFeedbackChange = (index, selected) => {
+    const { onFeedbackChange, feedback } = this.props;
+    const newFeedback = feedback.slice();
+    newFeedback[index] = selected;
+    onFeedbackChange(newFeedback);
+  };
+
+  render() {
+    const { value, feedbackListener, feedback } = this.props;
+    return (
+      <div className="lesion-candidates">
+        {value.map((cand, i) => (
+          <Candidate
+            key={i}
+            value={cand}
+            feedbackListener={feedbackListener}
+            feedback={feedback[i]}
+            index={i}
+            onFeedbackChange={this.handleFeedbackChange}
+            composition={this.state.composition}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+const LesionCandidates = connect(state => ({ user: state.loginUser }))(
+  LesionCandidatesView
+);
+
+export default LesionCandidates;
---
+++ packages/circus-web-ui/src/pages/plugin-job-detail/PluginJobDetail.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import PatientInfoBox from 'components/PatientInfoBox';
+import { api } from '../../utils/api';
+import LoadingIndicator from 'rb/LoadingIndicator';
+import LesionCandidates from './LesionCandidates';
+import ProjectIcon from 'components/ProjectIcon';
+import SelectionFeedbackListener from './feedback-listener/SelectionFeedbackListener';
+import FeedbackSwitcher from './FeedbackSwitcher';
+import { connect } from 'react-redux';
+
+const PluginJobDetailView = props => {
+  const { job, onFeedbackChange, feedback } = props;
+  return (
+    <div>
+      <LesionCandidates
+        job={job}
+        value={job.result.lesionCandidates}
+        feedbackListener={SelectionFeedbackListener}
+        feedback={feedback}
+        onFeedbackChange={onFeedbackChange}
+      />
+    </div>
+  );
+};
+
+const PluginJobDetailPage = props => {
+  const { job, seriesData, plugin } = props;
+  const primarySeriesUid = job.series[0].seriesUid;
+  return (
+    <div>
+      <PatientInfoBox value={seriesData[primarySeriesUid].patientInfo} />
+      <div className="project-name">
+        <ProjectIcon size="lg" icon={plugin.icon} />
+        {plugin.pluginName} v{plugin.pluginVersion}
+      </div>
+      <FeedbackSwitcher {...props} />
+    </div>
+  );
+};
+
+class ConnectedPluginJobDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { job: null };
+  }
+
+  async componentDidMount() {
+    // const { jobId } = this.props;
+    // const job = await api(`plugin-job/${jobId}`);
+
+    const job = {
+      jobId: 'aaabbbccc',
+      pluginId: 'pluginid',
+      status: 'finished',
+      userEmail: 'circus@circus.example.com',
+      series: [
+        {
+          seriesUid: (await api('series', { params: { limit: 1 } })).items[0]
+            .seriesUid,
+          start: 1,
+          end: 150,
+          delta: 1
+        }
+      ],
+      result: {
+        lesionCandidates: [
+          {
+            rank: 1,
+            location_x: 10,
+            location_y: 20,
+            location_z: 20,
+            confidence: 0.9
+          },
+          {
+            rank: 2,
+            location_x: 10,
+            location_y: 20,
+            location_z: 40,
+            confidence: 0.8
+          },
+          {
+            rank: 3,
+            location_x: 10,
+            location_y: 20,
+            location_z: 50,
+            confidence: 0.7
+          }
+        ]
+      },
+      feedback: [],
+      startedAt: new Date('2018-03-03T11:01:00Z'),
+      finishedAt: new Date('2018-03-03T11:02:00Z'),
+      createdAt: new Date('2018-03-03T11:00:30Z'),
+      updatedAt: new Date('2018-03-03T15:15:15Z')
+    };
+    const seriesData = {};
+    for (const s of job.series) {
+      const seriesUid = s.seriesUid;
+      if (seriesUid in seriesData) continue;
+      seriesData[seriesUid] = await api(`series/${seriesUid}`);
+    }
+
+    const plugin = await api(`plugins/${job.pluginId}`);
+
+    this.setState({ job, seriesData, plugin });
+  }
+
+  render() {
+    if (this.state.job) {
+      return (
+        <PluginJobDetailPage
+          jobRenderer={PluginJobDetailView}
+          job={this.state.job}
+          seriesData={this.state.seriesData}
+          plugin={this.state.plugin}
+        />
+      );
+    } else {
+      return <LoadingIndicator />;
+    }
+  }
+}
+
+const PluginJobDetail = connect(state => ({ user: state.loginUser.data }))(
+  ConnectedPluginJobDetail
+);
+
+export default PluginJobDetail;

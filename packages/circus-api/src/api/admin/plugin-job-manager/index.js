fec7c40751c8521a8477a5e94dccfc45f117f7e6
Masayuki Kano <kano@genuine-ltd.com>
2018-10-10 01:30:49 +0900

    Fix start bug

--- packages/circus-api/src/api/admin/plugin-job-manager/index.js
+++ packages/circus-api/src/api/admin/plugin-job-manager/index.js
@@ -7,17 +7,12 @@ export const handleGet = ({ cs }) => {
   };
 };
 
-export const handlePost = ({ cs, db }) => {
+export const handlePost = ({ cs }) => {
   return async (ctx, next) => {
     const currentStatus = await cs.daemon.status();
     const newStatus = ctx.request.body.status;
     if (currentStatus !== newStatus) {
       if (newStatus === 'running') {
-        const pluginDefs = await db
-          .collection('plugins')
-          .find({})
-          .toArray();
-        await cs.plugin.update(pluginDefs);
         await cs.daemon.start();
       } else if (newStatus === 'stopped') {
         await cs.daemon.stop();

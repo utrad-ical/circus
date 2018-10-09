0387e2a4b9ea1e469b85c96a174a09d28ac03468
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-10-09 10:32:22 +0900

    Makde sidebar collapsible

---
+++ packages/circus-web-ui/src/pages/case-detail/SideContainer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import classNames from 'classnames';
+import { Glyphicon } from 'components/react-bootstrap';
+
+const StyledSideBar = styled.div`
+  overflow: hidden;
+  height: 100%;
+  flex: 0 0 30px;
+  display: flex;
+  flex-direction: column;
+  .bar {
+    text-align: right;
+  }
+  &.open {
+    flex: 0 0 250px;
+    > .bar > .triangle {
+      transform: rotate(180deg);
+    }
+  }
+`;
+
+export default class SideContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { open: true };
+  }
+
+  handleToggle = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    const { open } = this.state;
+    return (
+      <StyledSideBar className={classNames({ open })}>
+        <div className="bar" onClick={this.handleToggle}>
+          <Glyphicon className="triangle" glyph="triangle-right" />
+        </div>
+        {open && this.props.children}
+      </StyledSideBar>
+    );
+  }
+}

72a450091341da6dd1362436ebc86be39581f0af
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-10-09 21:56:03 +0900

    Introduce error boundaries

---
+++ packages/circus-web-ui/src/components/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="alert alert-danger">
+          An error occurred while rendering UI. Please reload the page.
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}

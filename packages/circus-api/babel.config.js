77f02cbcbb5f72dbfe8f98f62a6ba614b9df37e5
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-10-18 18:13:14 +0900

    Update eslint and enable async generator

--- packages/circus-api/babel.config.js
+++ packages/circus-api/babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-async-generators',
     'babel-plugin-dynamic-import-node'
   ],
   sourceMaps: 'inline'

3c8aff1ae42ad72811d432cbde460cc557507c9a
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-11-09 00:26:54 +0900

    Introduce withPreference

--- packages/circus-web-ui/src/pages/search/SearchPanel.js
+++ packages/circus-web-ui/src/pages/search/SearchPanel.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Button } from 'components/react-bootstrap';
 import IconButton from 'components/IconButton';
-import Collapser from 'components/Collapser';
+import { CollapserView } from 'components/Collapser';
+import withPreference from 'components/withPreference';
+
+const StateSavingCollapser = withPreference('searchPanelOpen', true)(props => {
+  const {
+    searchPanelOpen,
+    searchPanelOpenChange,
+    searchPanelOpenBusy,
+    ...rest
+  } = props;
+  return (
+    <CollapserView
+      open={props.searchPanelOpen}
+      onToggleClick={() => props.searchPanelOpenChange(!props.searchPanelOpen)}
+      {...rest}
+    />
+  );
+});
 
 const SearchPanel = props => {
   const { onResetClick, onSearchClick, onSavePresetClick, children } = props;
   return (
-    <Collapser title="Search Condition" framed>
+    <StateSavingCollapser title="Search Condition" framed>
       {children}
       <div className="search-buttons">
         <IconButton bsStyle="link" icon="save" onClick={onSavePresetClick}>
@@ -21,7 +38,7 @@ const SearchPanel = props => {
           Search
         </IconButton>
       </div>
-    </Collapser>
+    </StateSavingCollapser>
   );
 };
 

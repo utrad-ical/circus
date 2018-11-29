de2440dd1a9f408ebb83d8e8e7537c01fa0acdbb
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-11-29 18:53:17 +0900

    Merge pull request fix-pointer-lock-of-figure-annotation
    Fix pointer lock of figure annotation

--- packages/circus-rs/src/browser/tool/annotation/PlaneFigureTool.ts
+++ packages/circus-rs/src/browser/tool/annotation/PlaneFigureTool.ts
@@ -1,6 +1,7 @@
-import { Vector2, Vector3 } from 'three';
-import DraggableTool from '../DraggableTool';
+import { Vector2 } from 'three';
 import ViewerEvent from '../../viewer/ViewerEvent';
+import Tool from '../Tool';
+import { MprViewState } from '../../ViewState';
 import {
   convertScreenCoordinateToVolumeCoordinate,
   detectOrthogonalSection
@@ -10,13 +11,18 @@ import PlaneFigure, { FigureType } from '../../annotation/PlaneFigure';
 /**
  * PlaneFigureTool creates and edits PlaneFigure.
  */
-export default class PlaneFigureTool extends DraggableTool {
-  private tmpAnnotation?: PlaneFigure;
+export default class PlaneFigureTool extends Tool {
+  private focusedFigure?: PlaneFigure;
   protected usePointerLockAPI = false;
   protected figureType: FigureType = 'circle';
 
+  private totalMovementX: number | undefined = undefined;
+  private totalMovementY: number | undefined = undefined;
+
   public dragStartHandler(ev: ViewerEvent): void {
-    super.dragStartHandler(ev);
+    const comp = ev.viewer.getComposition();
+    if (!comp) return;
+
     const viewState = ev.viewer.getState();
     if (!viewState || viewState.type !== 'mpr') return;
 
@@ -24,58 +30,91 @@ export default class PlaneFigureTool extends DraggableTool {
     const orientation = detectOrthogonalSection(section);
     if (orientation !== 'axial') return;
 
-    const comp = ev.viewer.getComposition();
-    const resolution = new Vector2().fromArray(ev.viewer.getResolution());
-    if (!comp) return;
+    ev.viewer.canvas.requestPointerLock();
+
+    const resolution: [number, number] = ev.viewer.getResolution();
+    const screenPoint: [number, number] = [ev.viewerX!, ev.viewerY!];
+
+    this.totalMovementX = 0;
+    this.totalMovementY = 0;
+
+    // Create figure
     const fig = new PlaneFigure();
     fig.type = this.figureType;
-
     const min = convertScreenCoordinateToVolumeCoordinate(
       section,
-      resolution,
-      new Vector2(ev.viewerX!, ev.viewerY!)
+      new Vector2().fromArray(resolution),
+      new Vector2().fromArray(screenPoint)
     );
     fig.min = [min.x, min.y];
+    fig.max = [min.x, min.y];
     fig.z = min.z;
-    
+
     comp.addAnnotation(fig);
     comp.annotationUpdated();
-    this.tmpAnnotation = fig;
+    this.focusedFigure = fig;
   }
 
   public dragHandler(ev: ViewerEvent): void {
-    super.dragHandler(ev);
     const comp = ev.viewer.getComposition();
     if (!comp) return;
-    const fig = this.tmpAnnotation;
-    if (!fig) return;
 
-    const resolution = new Vector2().fromArray(ev.viewer.getResolution());
+    const viewState = ev.viewer.getState();
+    if (!viewState || viewState.type !== 'mpr') return;
+
+    const section = viewState.section;
+    const orientation = detectOrthogonalSection(section);
+    if (orientation !== 'axial') return;
+
+    if (!this.focusedFigure) return;
+
+    const resolution: [number, number] = ev.viewer.getResolution();
+    const screenPoint: [number, number] = [
+      ev.viewerX! + this.totalMovementX!,
+      ev.viewerY! + this.totalMovementY!
+    ];
+
+    this.totalMovementX += ev.original.movementX;
+    this.totalMovementY += ev.original.movementY;
+
+    // Update figure
     const max = convertScreenCoordinateToVolumeCoordinate(
       (ev.viewer.getState() as MprViewState).section,
-      resolution,
-      new Vector2(ev.viewerX!, ev.viewerY!)
+      new Vector2().fromArray(resolution),
+      new Vector2().fromArray(screenPoint)
     );
-    fig.max = [max.x, max.y];
+    this.focusedFigure.max = [max.x, max.y];
     comp.annotationUpdated();
   }
 
   public dragEndHandler(ev: ViewerEvent): void {
-    super.dragEndHandler(ev);
-    const comp = ev.viewer.getComposition()!;
-    const fig = this.tmpAnnotation!;
-    if (!fig || !fig.min || !fig.max) return;
-    const newMin = [
-      Math.min(fig.min[0], fig.max[0]),
-      Math.min(fig.min[1], fig.max[1])
-    ];
-    const newMax = [
-      Math.max(fig.min[0], fig.max[0]),
-      Math.max(fig.min[1], fig.max[1])
-    ];
-    fig.min = newMin;
-    fig.max = newMax;
+    document.exitPointerLock();
+
+    const comp = ev.viewer.getComposition();
+    if (!comp) return;
+
+    const fig = this.focusedFigure;
+    if (!fig) return;
+
+    if(fig.min && fig.max && fig.min[0] !== fig.max[0] && fig.min[1] !== fig.max[1]) {
+      const newMin = [
+        Math.min(fig.min[0], fig.max[0]),
+        Math.min(fig.min[1], fig.max[1])
+      ];
+      const newMax = [
+        Math.max(fig.min[0], fig.max[0]),
+        Math.max(fig.min[1], fig.max[1])
+      ];
+      fig.min = newMin;
+      fig.max = newMax;
+    } else {
+      comp.removeAnnotation(fig);
+    }
+
+    this.totalMovementX = undefined;
+    this.totalMovementY = undefined;
+    this.focusedFigure = undefined;
+
     comp.annotationUpdated();
-    this.tmpAnnotation = undefined;
   }
 }

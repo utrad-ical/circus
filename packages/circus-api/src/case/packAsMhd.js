4c3d9e293ab3830832ed30641f3f3d2d3fd88f4c
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-12-06 22:25:10 +0900

    Improve export as MHD

--- packages/circus-api/src/case/packAsMhd.js
+++ packages/circus-api/src/case/packAsMhd.js
@@ -1,19 +1,29 @@
 import JSZip from 'jszip';
 
-export const packAsMhd = async ({ models, volumeProvider }, caseId) => {
-  const caseData = await models.clinicalCase.findByIdOrFail(caseId);
+export const packAsMhd = async (deps, caseId) => {
   const zip = new JSZip();
+  await putCaseData(deps, caseId, zip);
+  return zip.generateNodeStream();
+};
+
+/**
+ * Inserts a single case data into a root zip foler or a subdirectory.
+ * @param {JSZip} zip The JSZip root object or a subdirectory made by `folder`
+ */
+const putCaseData = async ({ models, volumeProvider }, caseId, zip) => {
+  const caseData = await models.clinicalCase.findByIdOrFail(caseId);
   const rev = caseData.latestRevision;
   zip.file(`${caseId}.mhd`, 'file');
   zip.file(`${caseId}.json`, JSON.stringify(prepareExportObject(caseData)));
   for (let volId = 0; volId < rev.series.length; volId++) {
     const series = rev.series[volId];
-    zip.file(`${volId}.vol`, 'volume data dummy');
+    const volumeAccessor = await volumeProvider(series.seriesUid);
+    await volumeAccessor.load(volumeAccessor.images);
+    zip.file(`${volId}.raw`, volumeAccessor.volume.data);
     for (let labelId = 0; labelId < series.labels.length; labelId++) {
-      //
+      zip.file(`vol${volId}-label${labelId}.raw`, 'dummy label data');
     }
   }
-  return zip.generateNodeStream();
 };
 
 const prepareExportObject = caseData => {

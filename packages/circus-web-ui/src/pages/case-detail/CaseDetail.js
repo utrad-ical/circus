75dde9c722fe924187412898b1e0b0682a937753
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-12-11 22:03:30 +0900

    Split ToolBar of case detail

--- packages/circus-web-ui/src/pages/case-detail/CaseDetail.js
+++ packages/circus-web-ui/src/pages/case-detail/CaseDetail.js
@@ -9,7 +9,6 @@ import {
   Button,
   DropdownButton,
   Glyphicon,
-  SplitButton,
   MenuItem
 } from '../../components/react-bootstrap';
 import Icon from 'components/Icon';
@@ -17,7 +16,6 @@ import LabelSelector from './LabelSelector';
 import { store } from 'store';
 import * as rs from 'circus-rs';
 import { alert, prompt, confirm } from 'rb/modal';
-import ShrinkSelect from 'rb/ShrinkSelect';
 import merge from 'merge';
 import classNames from 'classnames';
 import EventEmitter from 'events';
@@ -30,6 +28,7 @@ import TimeDisplay from 'components/TimeDisplay';
 import Tag from 'components/Tag';
 import { connect } from 'react-redux';
 import { toolFactory } from 'circus-rs/tool/tool-initializer';
+import ToolBar from './ToolBar';
 
 class CaseDetailView extends React.Component {
   constructor(props) {
@@ -517,96 +516,3 @@ export class RevisionData extends React.Component {
     );
   }
 }
-
-const ToolBar = props => {
-  const {
-    active,
-    changeTool,
-    showReferenceLine,
-    toggleReferenceLine,
-    brushEnabled,
-    lineWidth,
-    setLineWidth,
-    windowPresets = [],
-    selectWindowPreset
-  } = props;
-
-  const widthOptions = [1, 3, 5, 7];
-
-  return (
-    <div className="case-detail-toolbar">
-      <ToolButton name="pager" changeTool={changeTool} active={active} />
-      <ToolButton name="zoom" changeTool={changeTool} active={active} />
-      <ToolButton name="hand" changeTool={changeTool} active={active} />
-      <ToolButton name="window" changeTool={changeTool} active={active}>
-        {windowPresets.map((p, i) => (
-          <MenuItem
-            key={i + 1}
-            eventKey={i + 1}
-            onClick={() => selectWindowPreset(p)}
-          >
-            <b>{p.label}</b> {`(L: ${p.level} / W: ${p.width})`}
-          </MenuItem>
-        ))}
-      </ToolButton>
-      <ToolButton
-        name="brush"
-        changeTool={changeTool}
-        active={active}
-        disabled={!brushEnabled}
-      />
-      <ToolButton
-        name="eraser"
-        changeTool={changeTool}
-        active={active}
-        disabled={!brushEnabled}
-      />
-      <ShrinkSelect
-        options={widthOptions}
-        value={'' + lineWidth}
-        onChange={setLineWidth}
-      />
-      <ToolButton
-        name="bucket"
-        changeTool={changeTool}
-        active={active}
-        disabled={!brushEnabled}
-      />
-      &ensp;
-      <label>
-        <input
-          type="checkbox"
-          checked={showReferenceLine}
-          onChange={ev => toggleReferenceLine(ev.target.checked)}
-        />
-        Reference line
-      </label>
-    </div>
-  );
-};
-
-const ToolButton = props => {
-  const { name, active, changeTool, disabled, children } = props;
-  const style = active === name ? 'primary' : 'default';
-  const icon = <span className={'case-detail-tool-icon rs-icon-' + name} />;
-  const onClick = () => !disabled && changeTool(name);
-  if (children) {
-    return (
-      <SplitButton
-        id={`toolbutton-${name}`}
-        title={icon}
-        bsStyle={style}
-        onClick={onClick}
-        disabled={disabled}
-      >
-        {children}
-      </SplitButton>
-    );
-  } else {
-    return (
-      <Button bsStyle={style} onClick={onClick} disabled={disabled}>
-        {icon}
-      </Button>
-    );
-  }
-};

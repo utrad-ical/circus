1220108896fe04d8d22f4417ab44f0e186ad030f
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-12-19 02:48:31 +0900

    Implement basic undo/redo

--- packages/circus-web-ui/src/pages/case-detail/revisionHistory.js
+++ packages/circus-web-ui/src/pages/case-detail/revisionHistory.js
@@ -1,90 +1,56 @@
-import React from 'react';
-import wrapDisplayName from 'rb/utils/wrapDisplayName';
-
 /**
- * Creates a redux-like store object that can handle undo/redo.
+ * Creates a container that handles undo/redo.
  */
-export const createHistoryStore = () => {
+export const createHistoryStore = (maxHistoryLength = 10) => {
   let history = [];
   let currentIndex = 0;
-  let callbacks = [];
-
-  const handleUpdate = () => {
-    callbacks.forEach(cb => cb());
-  };
 
   const registerNew = revision => {
     history = [revision];
-    handleUpdate();
-  };
-
-  const undo = () => {
-    if (currentIndex > 0) {
-      currentIndex--;
-      handleUpdate();
-    }
   };
 
   const push = revision => {
     history = history.slice(0, currentIndex + 1);
     history.push(revision);
+    currentIndex++;
+    if (history.length > maxHistoryLength) {
+      history = history.slice(0, maxHistoryLength);
+      currentIndex = history.length - 1;
+    }
+  };
+
+  const canUndo = () => {
+    return currentIndex > 0;
+  };
+
+  const undo = () => {
+    if (!canUndo()) return;
+    currentIndex--;
+  };
+
+  const canRedo = () => {
+    return currentIndex < history.length - 1;
   };
 
   const redo = () => {
-    if (currentIndex < history.length - 1) {
-      currentIndex++;
-      handleUpdate();
-    }
+    if (!canRedo()) return;
+    currentIndex++;
   };
 
   const current = () => {
     return history[currentIndex];
   };
 
-  const unsubscribe = cb => {
-    callbacks = callbacks.filter(c => c !== cb);
-  };
+  const getHistoryLength = () => history.length;
 
-  const subscribe = cb => {
-    callbacks = callbacks.filter(c => c !== cb);
-    callbacks.unshift(cb);
-  };
-
-  return { registerNew, undo, redo, push, current, subscribe, unsubscribe };
-};
-
-export const connectHistoryStore = historyStore => {
-  return Base => {
-    const Enhanced = class extends React.PureComponent {
-      componentDidMount() {
-        historyStore.subscribe(this.handleUpdate);
-        this.setState({ revision: historyStore.current() });
-      }
-
-      componentWillUnmount() {
-        historyStore.unsubscribe(this.handleUpdate);
-      }
-
-      handleUpdate = () => {
-        this.setState({ revision: historyStore.current() });
-      };
-
-      render() {
-        const { revision } = this.state;
-        const provideProps = {
-          revision,
-          historyRegisterNew: historyStore.registerNew,
-          historyPush: historyStore.push,
-          historyUndo: historyStore.undo,
-          historyRedo: historyStore.redo
-        };
-        return <Base {...this.props} {...provideProps} />;
-      }
-    };
-    Enhanced.displayName = wrapDisplayName(
-      'connectHistoryStore',
-      Base.displayName
-    );
-    return Enhanced;
+  return {
+    registerNew,
+    push,
+    canUndo,
+    undo,
+    canRedo,
+    redo,
+    current,
+    getHistoryLength
   };
 };

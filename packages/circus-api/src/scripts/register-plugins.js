475f91665f72ecaf76f34a858f9a6fff883355dd
Soichiro Miki <smiki-tky@umin.ac.jp>
2018-12-25 15:33:13 +0900

    Prettier

--- packages/circus-api/src/scripts/register-plugins.js
+++ packages/circus-api/src/scripts/register-plugins.js
@@ -5,22 +5,21 @@ import connectDb from '../db/connectDb';
 import createValidator from '../createValidator';
 import createModels from '../db/createModels';
 
-
 export function help() {
   console.log('Up plugins.\n');
 }
 
 export default async function exec(files) {
-
   const paths = files.map(p => path.resolve(process.cwd(), p));
   if (!paths.length) {
     console.log(chalk.red('No file specified.'));
     return;
   }
-  const definitions = (await Promise.all(paths.map( p => fs.readFile(p, 'utf8'))))
-    .reduce( (p,i) => {
-      return p.concat(JSON.parse(i, 'utf8'));
-    }, []);
+  const definitions = (await Promise.all(
+    paths.map(p => fs.readFile(p, 'utf8'))
+  )).reduce((p, i) => {
+    return p.concat(JSON.parse(i, 'utf8'));
+  }, []);
 
   const db = await connectDb();
   try {
@@ -34,7 +33,7 @@ export default async function exec(files) {
     console.log(`Deleted ${flushRes.result.n} plugin(s).`);
     const registerRes = await models.plugin.insertMany(definitions);
     console.log(`Insert ${registerRes.result.n} plugin(s).`);
-  } catch(err) {
+  } catch (err) {
     console.error(err);
   } finally {
     await db.close();

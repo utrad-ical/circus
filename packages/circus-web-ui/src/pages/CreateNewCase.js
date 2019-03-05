0a4de4ccd0396f55fbf7e218d492ecb0dede3bf8
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-03-06 02:25:20 +0900

    Refactor CreateNewCase

--- packages/circus-web-ui/src/pages/CreateNewCase.js
+++ packages/circus-web-ui/src/pages/CreateNewCase.js
@@ -1,60 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import ProjectSelector from 'components/ProjectSelector';
-import { connect } from 'react-redux';
 import IconButton from 'components/IconButton';
 import MultiTagSelect from 'components/MultiTagSelect';
-import { api } from 'utils/api';
+import { useApi } from 'utils/api';
 import browserHistory from 'browserHistory';
 import SeriesSelector from 'components/SeriesSelector';
 import { multirange } from 'multi-integer-range';
+import useLoginUser from 'utils/useLoginUser';
 
-class CreateNewCaseView extends React.Component {
-  constructor(props) {
-    super(props);
-    const writableProjects = this.writableProjects(props);
-    if (writableProjects.length) {
-      this.state = {
-        selectedProject: writableProjects[0].projectId,
-        selectedSeries: [],
-        selectedTags: [],
-        busy: false
-      };
-    }
-  }
+const CreateNewCase = props => {
+  const user = useLoginUser();
+  const writableProjects = useMemo(
+    () => user.accessibleProjects.filter(p => p.roles.indexOf('write') >= 0),
+    [user]
+  );
+  const [selectedProject, setSelectedProject] = useState(
+    writableProjects[0].projectId
+  );
+  const [selectedSeries, setSelectedSeries] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [busy, setBusy] = useState(false);
+  const api = useApi();
+  const seriesUid = props.match.params.seriesUid;
 
-  async componentDidMount() {
-    this.setState({ busy: true });
-    const seriesUid = this.props.match.params.seriesUid;
+  const load = async () => {
+    setBusy(true);
     const series = await api('series/' + seriesUid);
-    this.setState({
-      selectedSeries: [{ ...series, range: series.images }],
-      busy: false
-    });
-  }
+    setSelectedSeries([{ ...series, range: series.images }]);
+    setBusy(false);
+  };
 
-  writableProjects() {
-    return this.props.user.accessibleProjects.filter(
-      p => p.roles.indexOf('write') >= 0
-    );
-  }
+  useEffect(() => {
+    load();
+  }, []);
 
-  handleProjectSelect = projectId => {
-    const { user } = this.props;
+  const handleProjectSelect = projectId => {
     const prj = user.accessibleProjects.find(p => p.projectId === projectId);
-    const newTags = this.state.selectedTags.filter(t =>
+    const newTags = selectedTags.filter(t =>
       prj.project.tags.find(tt => tt.name === t)
     );
-    this.setState({
-      selectedProject: projectId,
-      selectedTags: newTags
-    });
+    setSelectedProject(projectId);
+    setSelectedTags(newTags);
   };
 
-  handleTagChange = value => {
-    this.setState({ selectedTags: value });
-  };
-
-  handleCreate = async () => {
+  const handleCreate = async () => {
     // TODO: Check if a similar case exists
 
     const defaultPartialVolumeDescriptor = images => {
@@ -66,14 +55,14 @@ class CreateNewCaseView extends React.Component {
     const res = await api('cases', {
       method: 'post',
       data: {
-        projectId: this.state.selectedProject,
-        series: this.state.selectedSeries.map(s => ({
+        projectId: selectedProject,
+        series: selectedSeries.map(s => ({
           seriesUid: s.seriesUid,
           partialVolumeDescriptor: s.partialVolumeDescriptor
             ? s.partialVolumeDescriptor
             : defaultPartialVolumeDescriptor(s.images)
         })),
-        tags: this.state.selectedTags
+        tags: selectedTags
       }
     });
     if (res.caseId) {
@@ -81,67 +70,51 @@ class CreateNewCaseView extends React.Component {
     }
   };
 
-  handleSeriesChange = value => {
-    this.setState({ selectedSeries: value });
-  };
+  const canCreate = !busy && selectedSeries.length;
 
-  render() {
-    const { busy, selectedSeries } = this.state;
-    const writableProjects = this.writableProjects();
-    const canCreate = !busy && selectedSeries.length;
-
-    if (!writableProjects.length) {
-      return (
-        <div className="alert alert-danger">
-          You do not belong to any writable project.
-        </div>
-      );
-    }
-
-    const prj = writableProjects.find(
-      p => p.projectId === this.state.selectedProject
-    );
-    const tags = prj.project.tags;
-
+  if (!writableProjects.length) {
     return (
-      <div>
-        <h1>
-          <span className="circus-icon-case" />New Case
-        </h1>
-        <SeriesSelector
-          value={selectedSeries}
-          onChange={this.handleSeriesChange}
-        />
-        <div>
-          Project:&ensp;
-          <ProjectSelector
-            projects={writableProjects}
-            value={this.state.selectedProject}
-            onChange={this.handleProjectSelect}
-          />
-          &ensp; Tags:&ensp;
-          <MultiTagSelect
-            tags={tags}
-            value={this.state.selectedTags}
-            onChange={this.handleTagChange}
-          />
-        </div>
-        <div className="text-right">
-          <IconButton
-            disabled={!canCreate}
-            icon="circus-case"
-            bsStyle="primary"
-            onClick={this.handleCreate}
-          >
-            Create case for <b>{prj.project.projectName}</b>
-          </IconButton>
-        </div>
+      <div className="alert alert-danger">
+        You do not belong to any writable project.
       </div>
     );
   }
-}
 
-const stateToProps = state => ({ user: state.loginUser.data });
-const CreateNewCase = connect(stateToProps)(CreateNewCaseView);
+  const prj = writableProjects.find(p => p.projectId === selectedProject);
+  const tags = prj.project.tags;
+
+  return (
+    <div>
+      <h1>
+        <span className="circus-icon-case" />New Case
+      </h1>
+      <SeriesSelector value={selectedSeries} onChange={setSelectedSeries} />
+      <div>
+        Project:&ensp;
+        <ProjectSelector
+          projects={writableProjects}
+          value={selectedProject}
+          onChange={handleProjectSelect}
+        />
+        &ensp; Tags:&ensp;
+        <MultiTagSelect
+          tags={tags}
+          value={selectedTags}
+          onChange={setSelectedTags}
+        />
+      </div>
+      <div className="text-right">
+        <IconButton
+          disabled={!canCreate}
+          icon="circus-case"
+          bsStyle="primary"
+          onClick={handleCreate}
+        >
+          Create case for <b>{prj.project.projectName}</b>
+        </IconButton>
+      </div>
+    </div>
+  );
+};
 
 export default CreateNewCase;

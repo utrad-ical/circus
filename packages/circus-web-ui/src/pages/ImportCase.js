41c361d920f18e8e534d875503bdaf94ea149524
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-03-05 23:41:44 +0900

    Convert to function component

--- packages/circus-web-ui/src/pages/ImportCase.js
+++ packages/circus-web-ui/src/pages/ImportCase.js
@@ -2,17 +2,17 @@ import React from 'react';
 import FileUpload from 'components/FileUpload';
 import Icon from 'components/Icon';
 
-export default class ImportCase extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>
-          <Icon icon="open" /> Case Import
-        </h1>
-        <FileUpload multiple={false}>
-          <p>Upload a case archive file.</p>
-        </FileUpload>
-      </div>
-    );
-  }
-}
+const ImportCase = props => {
+  return (
+    <div>
+      <h1>
+        <Icon icon="open" /> Case Import
+      </h1>
+      <FileUpload multiple={false}>
+        <p>Upload a case archive file.</p>
+      </FileUpload>
+    </div>
+  );
+};
+
+export default ImportCase;

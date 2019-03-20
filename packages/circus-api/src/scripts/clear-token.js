b8d01ec85453908a4f44f3dd1af3fcf0aa3b1d63
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-03-21 00:00:51 +0900

    Add --all flag to clear-token script
    This prevents permanent tokens from accidentally deleted

--- packages/circus-api/src/scripts/clear-token.js
+++ packages/circus-api/src/scripts/clear-token.js
@@ -2,19 +2,35 @@ import connectDb from '../db/connectDb';
 import createValidator from '../createValidator';
 import createModels from '../db/createModels';
 
-export function help() {
+export const options = () => {
+  return [
+    {
+      names: ['all', 'a'],
+      help: 'Also deletes permanent tokens.',
+      type: 'bool'
+    }
+  ];
+};
+
+export const help = optionText => {
   console.log('Removes all access tokens.\n');
   console.log('Usage: node circus.js clear-token');
-}
+  console.log(optionText);
+};
 
-export async function exec() {
+export const exec = async options => {
+  const { all } = options;
   const db = await connectDb();
   try {
     const validator = await createValidator();
     const models = await createModels(db, validator);
-    const res = await models.token.deleteMany({});
+    const where = all ? {} : { permanentTokenId: null };
+    const res = await models.token.deleteMany(where);
     console.log(`Deleted ${res.result.n} access token(s).`);
+    if (!all) {
+      console.log('Use --all flag to also delete permanent tokens.');
+    }
   } finally {
     await db.close();
   }
-}
+};

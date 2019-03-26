33e4cc3c70870ea8fc29cfa281d90ce2d6ff540f
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-03-26 15:39:59 +0900

    Introduce useBodyClass

---
+++ packages/circus-web-ui/src/utils/useBodyClass.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+/**
+ * This adds the specified class to the `<body>` while the component is mounted.
+ */
+const useBodyClass = bodyClass => {
+  useEffect(
+    () => {
+      document.body.classList.add(bodyClass);
+      return () => {
+        document.body.classList.remove(bodyClass);
+      };
+    },
+    [bodyClass]
+  );
+};
+
+export default useBodyClass;

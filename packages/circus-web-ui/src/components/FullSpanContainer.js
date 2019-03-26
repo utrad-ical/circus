e92606f75afc67bad715c236c642d2f86581eeea
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-03-26 15:39:59 +0900

    Introduce useBodyClass

--- packages/circus-web-ui/src/components/FullSpanContainer.js
+++ packages/circus-web-ui/src/components/FullSpanContainer.js
@@ -1,15 +1,11 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { Fragment } from 'react';
+import useBodyClass from 'utils/useBodyClass';
 
 /**
  * This adds "full-span-container" body class from within a component.
  */
 const FullSpanContainer = props => {
-  useEffect(() => {
-    document.body.classList.add('full-span-container');
-    return () => {
-      document.body.classList.remove('full-span-container');
-    };
-  }, []);
+  useBodyClass('full-span-container');
   return <Fragment>{props.children}</Fragment>;
 };
 

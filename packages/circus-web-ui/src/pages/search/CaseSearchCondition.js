394d5db376a15757d01fb01deb0da6122a088640
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-04-16 23:49:21 +0900

    Rewrite CaseSearchCondition using hooks

--- packages/circus-web-ui/src/pages/search/CaseSearchCondition.js
+++ packages/circus-web-ui/src/pages/search/CaseSearchCondition.js
@@ -1,37 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import DateRangePicker, { dateRangeToMongoQuery } from 'rb/DateRangePicker';
 import { modalities } from 'modalities';
 import * as et from 'rb/editor-types';
 import ConditionFrame from './ConditionFrame';
 import { escapeRegExp } from 'utils/util';
 import { ControlLabel } from 'components/react-bootstrap';
-import { connect } from 'react-redux';
 import ProjectSelectorMultiple from 'components/ProjectSelectorMultiple';
 import { conditionToMongoQuery } from 'rb/ConditionEditor';
 import SearchPanel from 'pages/search/SearchPanel';
 import sendSearchCondition from 'pages/search/sendSearchCondition';
+import useLoginUser from 'utils/useLoginUser';
 
 const modalityOptions = { all: 'All' };
 modalities.forEach(m => (modalityOptions[m] = m));
-
-const basicConditionPropertiesTemplate = () => {
-  return [
-    { key: 'caseId', caption: 'Case ID', editor: et.text() },
-    { key: 'patientId', caption: 'Patient ID', editor: et.text() },
-    { key: 'patientName', caption: 'Patient Name', editor: et.text() },
-    {
-      key: 'createdAt',
-      caption: 'Create Time',
-      editor: DateRangePicker
-    },
-    {
-      key: 'updatedAt',
-      caption: 'Update Time',
-      editor: DateRangePicker
-    },
-    { key: 'tags', caption: 'Tags', editor: et.multiSelect({}) }
-  ];
-};
 
 const basicConditionToMongoQuery = condition => {
   const members = [];
@@ -86,88 +67,96 @@ const conditionToFilter = condition => {
   }
 };
 
-const advancedConditionKeysTemplate = () => {
-  return {
-    caseId: { caption: 'case ID', type: 'text' },
-    createdAt: { caption: 'case created at', type: 'date' },
-    updatedAt: { caption: 'case updated at', type: 'date' }
-  };
-};
+const CaseSearchCondition = props => {
+  const { condition, onChange } = props;
 
-class CaseSearchConditionView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.advancedConditionKeys = {
+  const [availableTags, setAvailableTags] = useState({});
+  const { accessibleProjects } = useLoginUser();
+
+  const basicConditionProperties = useMemo(
+    () => [
+      { key: 'caseId', caption: 'Case ID', editor: et.text() },
+      { key: 'patientId', caption: 'Patient ID', editor: et.text() },
+      { key: 'patientName', caption: 'Patient Name', editor: et.text() },
+      {
+        key: 'createdAt',
+        caption: 'Create Time',
+        editor: DateRangePicker
+      },
+      {
+        key: 'updatedAt',
+        caption: 'Update Time',
+        editor: DateRangePicker
+      },
+      { key: 'tags', caption: 'Tags', editor: et.multiSelect(availableTags) }
+    ],
+    [availableTags]
+  );
+
+  const advancedConditionKeys = useMemo(
+    () => ({
       caseId: { caption: 'case ID', type: 'text' },
-      tag: { caption: 'Tag', type: 'select', spec: { options: [] } }
-    };
-
-    this.state = {
-      basicConditionProperties: basicConditionPropertiesTemplate(),
-      advancedConditionKeys: advancedConditionKeysTemplate()
-    };
-  }
-
-  selectedProjectsChange = projects => {
-    const { accessibleProjects, condition } = this.props;
-    const availableTags = {};
-    for (const pid of projects) {
-      const p = accessibleProjects.find(p => p.projectId === pid);
-      p.project.tags.forEach(t => {
-        if (availableTags[t.name]) return;
-        availableTags[t.name] = { caption: t.name, color: t.color };
-      });
-    }
-    const basicConditionProperties = basicConditionPropertiesTemplate();
-    basicConditionProperties[5].editor = et.multiSelect(availableTags);
-    const newTags = condition.basic.tags.filter(t => availableTags[t]);
-
-    const advancedConditionKeys = advancedConditionKeysTemplate();
-    if (Object.keys(availableTags).length) {
-      advancedConditionKeys.tags = {
-        caption: 'tag',
+      tag: {
+        caption: 'Tag',
         type: 'select',
         spec: { options: Object.keys(availableTags) }
-      };
-    }
+      }
+    }),
+    [availableTags]
+  );
 
-    this.setState({ basicConditionProperties, advancedConditionKeys });
-    this.props.onChange({
+  const updateTagList = useCallback(
+    projectIds => {
+      const tags = {};
+      for (const pid of projectIds) {
+        const p = accessibleProjects.find(p => p.projectId === pid);
+        p.project.tags.forEach(t => {
+          if (tags[t.name]) return;
+          tags[t.name] = { caption: t.name, color: t.color };
+        });
+      }
+      setAvailableTags(tags);
+    },
+    [accessibleProjects]
+  );
+
+  useEffect(() => updateTagList(condition.projects), [
+    condition.projects,
+    updateTagList
+  ]);
+
+  const handleSelectedProjectsChange = projectIds => {
+    updateTagList(projectIds);
+    const newTags = condition.basic.tags.filter(t => availableTags[t]);
+
+    onChange({
       ...condition,
-      projects,
-      basic: {
-        ...condition.basic,
-        tags: newTags
-      },
+      projects: projectIds,
+      basic: { ...condition.basic, tags: newTags },
       advanced: { $and: [] }
     });
   };
 
-  render() {
-    const { accessibleProjects, condition } = this.props;
-    const { basicConditionProperties, advancedConditionKeys } = this.state;
-
-    return (
-      <SearchPanel {...this.props}>
-        <div style={{ marginBottom: '10px' }}>
-          <ControlLabel>Project:&ensp;</ControlLabel>
-          <ProjectSelectorMultiple
-            projects={accessibleProjects}
-            noneText="(All projects)"
-            value={condition.projects}
-            onChange={this.selectedProjectsChange}
-          />
-        </div>
-        <ConditionFrame
-          condition={condition}
-          onChange={this.props.onChange}
-          basicConditionProperties={basicConditionProperties}
-          advancedConditionKeys={advancedConditionKeys}
+  return (
+    <SearchPanel {...props}>
+      <div style={{ marginBottom: '10px' }}>
+        <ControlLabel>Project:&ensp;</ControlLabel>
+        <ProjectSelectorMultiple
+          projects={accessibleProjects}
+          noneText="(All projects)"
+          value={condition.projects}
+          onChange={handleSelectedProjectsChange}
         />
-      </SearchPanel>
-    );
-  }
-}
+      </div>
+      <ConditionFrame
+        condition={condition}
+        onChange={onChange}
+        basicConditionProperties={basicConditionProperties}
+        advancedConditionKeys={advancedConditionKeys}
+      />
+    </SearchPanel>
+  );
+};
 
 const nullCondition = () => {
   return {
@@ -177,10 +166,6 @@ const nullCondition = () => {
     advanced: { $and: [] }
   };
 };
-
-const CaseSearchCondition = connect(state => ({
-  accessibleProjects: state.loginUser.data.accessibleProjects
-}))(CaseSearchConditionView);
 
 export default sendSearchCondition({
   searchName: 'case',

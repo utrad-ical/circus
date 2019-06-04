8c2ebc46798ca20a06ae066159b619bea46ef83a
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-06-04 13:57:26 +0900

    Add loosened UID checking

--- packages/circus-lib/src/validation.test.ts
+++ packages/circus-lib/src/validation.test.ts
@@ -6,4 +6,6 @@ test('isDicomUid', () => {
   expect(isDicomUid('11118')).toBe(false);
   expect(isDicomUid('1.3333.'.repeat(10) + '555')).toBe(false);
   expect(isDicomUid('1.3333.55555')).toBe(true);
+  expect(isDicomUid('1.3333.00.55555')).toBe(true);
+  expect(isDicomUid('1.3333.00.55555', true)).toBe(false);
 });
--- packages/circus-lib/src/validation.ts
+++ packages/circus-lib/src/validation.ts
@@ -1,11 +1,12 @@
 /**
  * Validator function which checks the input is
- * a valid UID (eg, series instance UID)
+ * a valid UID (eg, series instance UID).
+ * @param input The input string.
+ * @param strict If false, nonstandard components like `00` will be accepted.
  */
-export function isDicomUid(input: string): boolean {
-  return (
-    typeof input === 'string' &&
-    !!input.match(/^((0|[1-9]\d*)\.)+(0|[1-9]\d*)$/) &&
-    input.length <= 64
-  );
+export function isDicomUid(input: string, strict: boolean = false): boolean {
+  const reg = strict
+    ? /^((0|[1-9]\d*)\.)+(0|[1-9]\d*)$/
+    : /^[0-9]+(\.[0-9]+)+$/;
+  return typeof input === 'string' && reg.test(input) && input.length <= 64;
 }

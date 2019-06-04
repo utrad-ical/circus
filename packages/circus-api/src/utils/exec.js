39dda6155745093cfa9ad4a8979032c613a1f337
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-06-04 13:10:06 +0900

    Ignore stderr

--- packages/circus-api/src/utils/exec.js
+++ packages/circus-api/src/utils/exec.js
@@ -4,13 +4,14 @@ import * as cp from 'child_process';
  * Executes a command as a child process and gets the output.
  * @param {string} command
  * @param {string[]} args
+ * @param {boolean} stopOnStderr
  * @returns {Promise<string>}
  */
-export default function exec(command, args) {
+export default function exec(command, args, stopOnStderr = false) {
   return new Promise((resolve, reject) => {
     cp.execFile(command, args, (err, stdout, stderr) => {
       if (err) reject(err);
-      else if (stderr) reject(err);
+      else if (stopOnStderr && stderr) reject(stderr);
       else resolve(stdout);
     });
   });

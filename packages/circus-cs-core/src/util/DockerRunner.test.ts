436fbdf073158e2ccfe59778d4e5064b85f895ff
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-06-16 17:25:17 +0900

    Conditionally skip DockerRunner tests

--- packages/circus-cs-core/src/util/DockerRunner.test.ts
+++ packages/circus-cs-core/src/util/DockerRunner.test.ts
@@ -1,23 +1,28 @@
 import DockerRunner from './DockerRunner';
 import Dockerode from 'dockerode';
+import sleep from './sleep';
 
-test('run hello-world docker image', async () => {
-  const runner = new DockerRunner();
-  const result = await runner.run({ Image: 'hello-world' });
-  expect(result).toMatch('Hello from Docker!');
-});
+if (process.env.SKIP_DOCKER_TESTS) {
+  test.skip('skipping docker tests', () => {});
+} else {
+  test('run hello-world docker image', async () => {
+    const runner = new DockerRunner();
+    const result = await runner.run({ Image: 'hello-world' });
+    expect(result).toMatch('Hello from Docker!');
+  });
 
-test('load text file from an image', async () => {
-  const runner = new DockerRunner();
-  const result = await runner.loadFromTextFile(
-    'circus-mock/succeeds:1.0',
-    '/etc/os-release'
-  );
-  expect(result).toMatch(/Alpine Linux/);
-});
+  test('load text file from an image', async () => {
+    const runner = new DockerRunner();
+    const result = await runner.loadFromTextFile(
+      'circus-mock/succeeds:1.0',
+      '/etc/os-release'
+    );
+    expect(result).toMatch(/Alpine Linux/);
+  });
 
-test('connect to Docker public API', async () => {
-  const rode = new Dockerode();
-  const results = await rode.listImages();
-  expect(results.length).toBeGreaterThanOrEqual(1);
-});
+  test('connect to Docker public API', async () => {
+    const rode = new Dockerode();
+    const results = await rode.listImages();
+    expect(results.length).toBeGreaterThanOrEqual(1);
+  });
+}

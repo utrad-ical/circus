6efc8469c54a9f3283c35b94fe6f7c064ab0dd21
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-06-25 19:40:45 +0900

    Remove unused `finally`

--- packages/circus-cs-core/src/cui/register.ts
+++ packages/circus-cs-core/src/cui/register.ts
@@ -9,29 +9,25 @@ const register: FunctionService<
 > = async (options, deps) => {
   const { pluginJobRegisterer } = deps;
   return async (commandName, args) => {
-    try {
-      const {
-        _: [pluginId, ...series],
-        jobId = generateUniqueId(),
-        environment = undefined,
-        priority = 0
-      } = args;
+    const {
+      _: [pluginId, ...series],
+      jobId = generateUniqueId(),
+      environment = undefined,
+      priority = 0
+    } = args;
 
-      // Each series definition must be in the form of
-      // `seriesUid(:startImgNum(:endImgNum(:imageDelta)))`.
-      const seriesList = (series as string[]).map(parseSeries);
+    // Each series definition must be in the form of
+    // `seriesUid(:startImgNum(:endImgNum(:imageDelta)))`.
+    const seriesList = (series as string[]).map(parseSeries);
 
-      if (!pluginId || !series.length) {
-        console.log('Usage: cui register <pluginId> <series0> [<series1> ...]');
-        return;
-      }
-
-      const payload = { pluginId, series: seriesList, environment };
-      await pluginJobRegisterer.register(jobId, payload, priority);
-      console.log(`Registered Job, ID: ${jobId}`);
-    } finally {
-      // await dispose();
+    if (!pluginId || !series.length) {
+      console.log('Usage: cui register <pluginId> <series0> [<series1> ...]');
+      return;
     }
+
+    const payload = { pluginId, series: seriesList, environment };
+    await pluginJobRegisterer.register(jobId, payload, priority);
+    console.log(`Registered Job, ID: ${jobId}`);
   };
 };
 

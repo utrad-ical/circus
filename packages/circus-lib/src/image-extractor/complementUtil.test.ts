c187570d219a61da8010109f4d8269b33f00409b
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-09-26 05:22:43 +0000

    Improve comments of complementUtil

--- packages/circus-lib/src/image-extractor/complementUtil.test.ts
+++ packages/circus-lib/src/image-extractor/complementUtil.test.ts
@@ -1,19 +1,25 @@
 import { convertComplement } from './complementUtil';
 
 describe('convertComplement', () => {
+  // S: sign bit = high bit
+  // N: remaining significant bits
+  // x: unused bits (must be ignored)
   test('bitsStored = 2, highBit = 1', () => {
     expect(convertComplement(0b00, 2, 1)).toBe(0);
     expect(convertComplement(0b01, 2, 1)).toBe(1);
     expect(convertComplement(0b10, 2, 1)).toBe(-2);
     expect(convertComplement(0b11, 2, 1)).toBe(-1);
+    //                         SN
   });
   test('bitsStored = 2, highBit = 3', () => {
     expect(convertComplement(0b1000, 2, 3)).toBe(-2);
     expect(convertComplement(0b1100, 2, 3)).toBe(-1);
+    //                         SNxx
   });
   test('bitsStored = 8, highBit = 7', () => {
     expect(convertComplement(0b11111111, 8, 7)).toBe(-1);
     expect(convertComplement(0b11011100, 8, 7)).toBe(-36);
+    //                         SNNNNNNN
   });
   test('bitsStored = 14, highBit = 13', () => {
     expect(convertComplement(0b0000000000000000, 14, 13)).toBe(0);
@@ -21,11 +27,12 @@ describe('convertComplement', () => {
     expect(convertComplement(0b0011111111111111, 14, 13)).toBe(-1);
     expect(convertComplement(0b0011111111111110, 14, 13)).toBe(-2);
     expect(convertComplement(0b0011111111111101, 14, 13)).toBe(-3);
+    //                           SNNNNNNNNNNNNN
   });
   test('ignore unused bits', () => {
     expect(convertComplement(0b111111000, 2, 3)).toBe(-2);
-    //                         ^^^^ unused bits
+    //                         xxxxxSNxx
     expect(convertComplement(0b1011, 2, 3)).toBe(-2);
-    //                           ^^ unused bits
+    //                         SNxx
   });
 });
--- packages/circus-lib/src/image-extractor/complementUtil.ts
+++ packages/circus-lib/src/image-extractor/complementUtil.ts
@@ -1,34 +1,37 @@
 /**
- * Converts a given number with an arbitrary high bit position
- * into an ordinary Int16.
+ * Converts a given number with an arbitrary sign bit position
+ * into an ordinary (signed) JavaScript integer.
+ * For example, if `highBit` is 6 and `bitsStores` = 5,
+ * the number is stored like `0bxSNNNNxx`, where `S` is the sign bit
+ * and `x` is the unused bits, which will be ignored.
  * @param input A number using 2's complement
- * @param bitsStored DICOM Bits Stored,
- * which describes how many bits are used,
- * counting from `highBit` (between 1 to `highBit` + 1).
- * @param highBit DICOM High Bit,
- * which is the position of the sign bit (between 1 and 32)
+ * @param bitsStored DICOM "Bits Stored" value,
+ * which describes how many bits are used to represent a number,
+ * counting from `highBit` (between 2 to `highBit` + 1).
+ * @param highBit DICOM "High Bit" value,
+ * which is the position of the sign bit (between 0 and 31)
  */
 export const convertComplement = (
   input: number,
   bitsStored: number,
   highBit: number
 ) => {
-  // for example, if highBit == 6 and bitsStored = 5...
-  // The number is stored like 0SNNNN00, where S is the sign bit
+  // for example, if highBit == 6, bitsStored = 5, input = 0x1111100,
+  // the function must return -1.
 
-  // Remove unused low bits (0SNNNN00 => 000SNNNN)
+  // Remove unused low bits (01111100 => 00011111)
   input = input >> (highBit - bitsStored + 1);
 
   // 00100000 : 1 << bitsStored
   // 00011111 : (1 << bitsStored) - 1
   const bitmask = (1 << bitsStored) - 1;
 
-  // Check the high (i.e., sing) bit.
-  // 000SNNNN
+  // Check the high (i.e., sign) bit
+  // 000S0000
   if (input & (1 << (bitsStored - 1))) {
-    // input is negative
+    // Input is negative
 
-    // 00011111 = -1
+    // 00011111 (= -1)
     // 00011110 : (input - 1)
     // 11100001 : ~(input - 1)
     // 00000001 : ~(input - 1) & bitmask

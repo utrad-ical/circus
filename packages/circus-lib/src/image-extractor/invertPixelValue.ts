326a1ef31e1882be3a0a7670d7460bdc0711cf5c
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-12-17 04:02:28 +0000

    Fix invertPixelValue

--- packages/circus-lib/src/image-extractor/invertPixelValue.ts
+++ packages/circus-lib/src/image-extractor/invertPixelValue.ts
@@ -1,8 +1,12 @@
 /**
- * Inverts the stored pixel value according to the DICOM PhotometricInterpretation spec.
- * @param input
- * @param bitsStored
- * @param pixelRepresentation
+ * Inverts the stored pixel value.
+ * Used when DICOM "PhotometricInterpretation" is set to MONOCHROME1
+ * (= minimum value is white).
+ * The minimum representable value will become the maximum representable value.
+ * @param input The input value (ordinary JavaScript integer).
+ * @param bitsStored DICOM "Bits Stored" value,
+ * which describes how many bits are used to represent a number.
+ * @param pixelRepresentation 0 if unsigned, 1 if signed.
  */
 export const invertPixelValue = (
   input: number,

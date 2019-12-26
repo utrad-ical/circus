85d914f34fe12d56f0de9824e66945afd1469645
Soichiro Miki <smiki-tky@umin.ac.jp>
2019-12-26 13:53:32 +0900

    Improve login screen

--- packages/circus-web-ui/src/pages/LoginScreen.js
+++ packages/circus-web-ui/src/pages/LoginScreen.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useLoginManager } from 'utils/loginManager';
 import {
-  Row,
-  Col,
   Panel,
   FormControl,
   FormGroup,
@@ -11,23 +9,25 @@ import {
 } from 'components/react-bootstrap';
 import styled from 'styled-components';
 import browserHistory from 'browserHistory';
+import classnames from 'classnames';
 
 const StyledDiv = styled.div`
-  .login-panel {
-    .panel {
-      box-shadow: 3px 3px 30px silver;
-    }
-    margin-top: 3em;
-    h1 {
-      text-shadow: 2px 2px 3px silver;
-      font-size: 36px;
-      border: none;
-    }
-    input.form-control {
-      margin: 1em 0;
-      background-color: #d0e6e4;
-      border-radius: 7px;
-    }
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  .panel {
+    box-shadow: 3px 3px 30px silver;
+  }
+  margin-top: 3em;
+  h1 {
+    text-shadow: 2px 2px 3px silver;
+    font-size: 36px;
+    border: none;
+  }
+  input.form-control {
+    margin: 1em 0;
+    background-color: #d0e6e4;
+    border-radius: 7px;
   }
 `;
 
@@ -58,45 +58,42 @@ const LoginScreen = props => {
   const disabled = input.id.length === 0 || input.password.length === 0;
 
   return (
-    <StyledDiv>
-      <Row className={'login-panel' + (error ? ' has-error' : '')}>
-        <Col sm={6} smOffset={3}>
-          <Panel bsStyle="primary">
-            <Panel.Body>
-              <h1 className="text-center">
-                <span className="circus-icon-logo" />
-                <span hidden>CIRCUS DB</span>
-              </h1>
-              <FormGroup>
-                <FormControl
-                  placeholder="User ID or E-mail"
-                  autoFocus
-                  value={input.id}
-                  onChange={ev => handleChange('id', ev.target.value)}
-                />
-                <FormControl
-                  placeholder="Password"
-                  type="password"
-                  value={input.password}
-                  onChange={ev => handleChange('password', ev.target.value)}
-                  onKeyDown={ev => ev.keyCode == 13 && handleLoginClick()}
-                />
-              </FormGroup>
-              {error && <p className="text-danger">{error}</p>}
-            </Panel.Body>
-            <Panel.Footer className="text-center">
-              <Button
-                disabled={disabled}
-                bsStyle="primary"
-                bsSize="lg"
-                onClick={handleLoginClick}
-              >
-                <Glyphicon glyph="ok-sign" />&ensp;Login
-              </Button>
-            </Panel.Footer>
-          </Panel>
-        </Col>
-      </Row>
+    <StyledDiv className={classnames({ 'has-error': error })}>
+      <Panel bsStyle="primary">
+        <Panel.Body>
+          <h1 className="text-center">
+            <span className="circus-icon-logo" />
+            <span hidden>CIRCUS DB</span>
+          </h1>
+          <FormGroup>
+            <FormControl
+              placeholder="User ID or E-mail"
+              autoFocus
+              value={input.id}
+              onChange={ev => handleChange('id', ev.target.value)}
+            />
+            <FormControl
+              placeholder="Password"
+              type="password"
+              value={input.password}
+              onChange={ev => handleChange('password', ev.target.value)}
+              onKeyDown={ev => ev.keyCode == 13 && handleLoginClick()}
+            />
+          </FormGroup>
+          {error && <p className="text-danger">{error}</p>}
+        </Panel.Body>
+        <Panel.Footer className="text-center">
+          <Button
+            disabled={disabled}
+            bsStyle="primary"
+            bsSize="lg"
+            onClick={handleLoginClick}
+          >
+            <Glyphicon glyph="ok-sign" />
+            &ensp;Login
+          </Button>
+        </Panel.Footer>
+      </Panel>
     </StyledDiv>
   );
 };

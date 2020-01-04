744c3030e428daaaa0ef7ff4693f26335a0a2a62
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-01-04 17:21:41 +0000

    Fix DicomImporter test so that it contains at least one `test`

--- packages/circus-api/src/DicomImporter.test.ts
+++ packages/circus-api/src/DicomImporter.test.ts
@@ -5,49 +5,50 @@ import createValidator from './createValidator';
 import createModels, { Models } from './db/createModels';
 import DicomImporter from './DicomImporter';
 
-if (!process.env.DICOM_UTILITY) {
-  // Skip this test if dicom_utility is not available
-  describe.skip('DicomImporter', () => {});
-} else {
-  describe('DicomImporter', function() {
-    const dbPromise = usingMongo();
+describe('DicomImporter', () => {
+  if (!process.env.DICOM_UTILITY) {
+    // Skip this test if dicom_utility is not available
+    test.skip('Skipping DicomImporter tests', () => {});
+    return;
+  }
 
-    let repository: MemoryDicomFileRepository,
-      importer: DicomImporter,
-      models: Models;
-    const file = path.join(__dirname, '../test/dicom/CT-MONO2-16-brain.dcm');
+  const dbPromise = usingMongo();
 
-    beforeAll(async () => {
-      const db = await dbPromise;
-      const validator = await createValidator();
-      models = createModels(db, validator);
-    });
+  let repository: MemoryDicomFileRepository,
+    importer: DicomImporter,
+    models: Models;
+  const file = path.join(__dirname, '../test/dicom/CT-MONO2-16-brain.dcm');
 
-    beforeEach(async () => {
-      const db = await dbPromise;
-      repository = new MemoryDicomFileRepository({});
-      await setUpMongoFixture(db, ['series']);
-      importer = new DicomImporter(repository, models, {
-        utility: process.env.DICOM_UTILITY!
-      });
-    });
+  beforeAll(async () => {
+    const db = await dbPromise;
+    const validator = await createValidator();
+    models = createModels(db, validator);
+  });
 
-    describe('#readDicomTagsFromFile', () => {
-      it('should correctly read DICOM tags', async () => {
-        const tags = await importer.readDicomTagsFromFile(file);
-        expect(tags.patientName).toBe('Anonymized');
-        expect(tags.instanceNumber).toBe('8');
-      });
-    });
-
-    describe('#importFromFile', () => {
-      it('should import a DICOM file', async () => {
-        await importer.importFromFile(file, 'someDomain');
-        const seriesLoader = await repository.getSeries(
-          '2.16.840.1.113662.2.1.2519.21582.2990505.2105152.2381633.20'
-        );
-        expect(await seriesLoader.load(8)).not.toBeUndefined();
-      });
+  beforeEach(async () => {
+    const db = await dbPromise;
+    repository = new MemoryDicomFileRepository({});
+    await setUpMongoFixture(db, ['series']);
+    importer = new DicomImporter(repository, models, {
+      utility: process.env.DICOM_UTILITY!
     });
   });
-}
+
+  describe('#readDicomTagsFromFile', () => {
+    it('should correctly read DICOM tags', async () => {
+      const tags = await importer.readDicomTagsFromFile(file);
+      expect(tags.patientName).toBe('Anonymized');
+      expect(tags.instanceNumber).toBe('8');
+    });
+  });
+
+  describe('#importFromFile', () => {
+    it('should import a DICOM file', async () => {
+      await importer.importFromFile(file, 'someDomain');
+      const seriesLoader = await repository.getSeries(
+        '2.16.840.1.113662.2.1.2519.21582.2990505.2105152.2381633.20'
+      );
+      expect(await seriesLoader.load(8)).not.toBeUndefined();
+    });
+  });
+});

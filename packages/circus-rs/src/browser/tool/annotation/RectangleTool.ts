0f870bb418b492c879af135209531d718db86534
Mari Ueda <uedamari0922@gmail.com>
2020-01-07 18:46:33 +0900

    Fix class name mistake

--- packages/circus-rs/src/browser/tool/annotation/RectangleTool.ts
+++ packages/circus-rs/src/browser/tool/annotation/RectangleTool.ts
@@ -1,6 +1,6 @@
 import PlaneFigureTool from './PlaneFigureTool';
 
-export default class CircleTool extends PlaneFigureTool {
+export default class RectangleTool extends PlaneFigureTool {
   constructor() {
     super();
     this.figureType = 'rectangle';

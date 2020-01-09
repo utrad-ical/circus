1f1d88ac933870723d909e89ca7fb25e90f29dfb
Masayuki Kano <kano@genuine-ltd.com>
2020-01-10 00:35:13 +0900

    Fix examples

--- packages/circus-rs/demo/solid-figure-annotation.js
+++ packages/circus-rs/demo/solid-figure-annotation.js
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const toolbar = rs.createToolbar(container, [
     'hand',
-    // 'window',
+    'window',
     'zoom',
     'pager',
     'celestialRotate',
@@ -109,7 +109,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // 'eraser',
     // 'bucket',
     'circle',
-    // 'rectangle',
+    'rectangle',
     'point',
     'ellipsoid',
     'cuboid'
@@ -120,106 +120,16 @@ document.addEventListener('DOMContentLoaded', () => {
   toolbar.bindViewer(viewer3);
   toolbar.bindViewer(viewer4);
 
-  setTimeout(() => {
-    const fig = new rs.Ellipsoid();
-    fig.editable = true;
-    fig.color = '#ff0000';
-    fig.width = 3;
-    fig.min = [93.58974358974362, 152.56410256410254, 67.21153846153844];
-    fig.max = [306.41025641025647, 243.5897435897436, 286.44230769230774];
-    fig.boundingBoxOutline = { width: 1, color: '#ff0000' };
-    fig.boundingBoxCrossHair = { width: 1, color: '#ffff00' };
-    fig.boundingBoxCrossSectionalShape = { width: 1, color: '#00ffff' };
+  const fig = new rs.Ellipsoid();
+  fig.editable = true;
+  fig.color = '#ff0000';
+  fig.width = 3;
+  fig.min = [93.58974358974362, 152.56410256410254, 67.21153846153844];
+  fig.max = [306.41025641025647, 243.5897435897436, 286.44230769230774];
+  fig.boundingBoxOutline = { width: 1, color: '#ff0000' };
+  fig.boundingBoxCrossHair = { width: 1, color: '#ffff00' };
+  fig.boundingBoxCrossSectionalShape = { width: 1, color: '#00ffff' };
 
-    comp.addAnnotation(fig);
-    comp.annotationUpdated();
-
-    const state1 = {
-      type: 'mpr',
-      window: {
-        level: 40,
-        width: 350
-      },
-      interpolationMode: 'trilinear',
-      section: {
-        origin: [5.036015358780475, -2.200536920129224, 128.73052401114236],
-        xAxis: [373.4315973229449, 71.90199062638484, -124.01187791819329],
-        yAxis: [-1.7436810353162562, 348.29543247375403, 196.6907504036925]
-      }
-    };
-
-    const state2 = {
-      type: 'mpr',
-      window: {
-        level: 40,
-        width: 350
-      },
-      interpolationMode: 'trilinear',
-      section: {
-        origin: [84.08155204502285, 135.83069150762358, 397.3361010570532],
-        xAxis: [324.2730903751294, -111.87876577175689, -205.74281185001618],
-        yAxis: [-143.33806785805604, 183.08575985554955, -325.4747345660419]
-      }
-    };
-
-    const state3 = {
-      type: 'mpr',
-      window: {
-        level: 40,
-        width: 350
-      },
-      interpolationMode: 'trilinear',
-      section: {
-        origin: [0, 0, 178.75],
-        xAxis: [400, 0, 0],
-        yAxis: [0, 400, 0]
-      }
-    };
-
-    let count = 0;
-    const moveToRight = () => {
-      if (count++ < 30) {
-        const state = {
-          ...state3,
-          section: {
-            ...state3.section,
-            origin: [-count * 30, 0, 178.75]
-          }
-        };
-        viewer4.setState(state);
-        setTimeout(moveToRight, 500);
-      }
-    };
-    // moveToRight();
-
-    // const state = {
-    //   type: 'mpr',
-    //   window: {
-    //     level: 40,
-    //     width: 350
-    //   },
-    //   interpolationMode: 'trilinear',
-    //   section: {
-    //     origin: [245.34625671492938, 254.0390248586219, 392.8100703338916],
-    //     xAxis: [1.7436810353164667, -348.29543247375346, -196.69075040369282],
-    //     yAxis: [-143.33806785805604, 183.08575985554955, -325.47473456604183]
-    //   }
-    // };
-
-    // const state = {
-    //   type: 'mpr',
-    //   window: {
-    //     level: 40,
-    //     width: 350
-    //   },
-    //   interpolationMode: 'trilinear',
-    //   section: {
-    //     origin: [122.51334467251235, 368.5115300104794, -40.25846539213779],
-    //     xAxis: [-288.37834606672317, -192.78731556102412, 199.17575273761034],
-    //     yAxis: [276.2387699148572, -223.74309361725403, 183.38803138292852]
-    //   }
-    // };
-
-    viewer4.setState(state2);
-  }, 500);
+  comp.addAnnotation(fig);
+  comp.annotationUpdated();
 });

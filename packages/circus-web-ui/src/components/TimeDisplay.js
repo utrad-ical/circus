48e2c012dd14c3eb7986d8023dc5d91f3a677896
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-01-21 18:05:36 +0900

    Fix basic search for plugin job search

--- packages/circus-web-ui/src/components/TimeDisplay.js
+++ packages/circus-web-ui/src/components/TimeDisplay.js
@@ -2,8 +2,9 @@ import React from 'react';
 import moment from 'moment';
 
 const TimeDisplay = props => {
-  const { value } = props;
+  const { value, invalidLabel = '' } = props;
   const m = moment(value);
+  if (!m.isValid()) return <span>{invalidLabel}</span>;
   return <span title={value}>{m.format('YYYY-MM-DD HH:mm')}</span>;
 };
 

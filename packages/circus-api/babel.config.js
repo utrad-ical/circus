0e8ec2b4a5a837c6517080a3e4b4cb27110e6b5c
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-02-05 11:05:01 +0900

    Update babel to 7.8 (`??`, `?.`, `await import()`, etc)

--- packages/circus-api/babel.config.js
+++ packages/circus-api/babel.config.js
@@ -3,12 +3,5 @@ module.exports = {
     ['@babel/preset-env', { targets: { node: '8' } }],
     '@babel/preset-typescript'
   ],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-async-generators',
-    'babel-plugin-dynamic-import-node'
-  ],
   sourceMaps: true
 };

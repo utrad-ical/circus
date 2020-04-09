940d95135c47bbfb9c7bdb063bff0f0d44da80d3
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-04-09 13:14:37 +0900

    Use unified config loader

--- packages/circus-cs-core/src/config/index.ts
+++ packages/circus-cs-core/src/config/index.ts
@@ -1,23 +1,10 @@
-import merge from 'merge';
-import cosmiconfig from 'cosmiconfig';
-import defaults from './default';
 import * as circus from '../interface';
+import path from 'path';
+import loadConfig from '@utrad-ical/circus-lib/lib/config/loadConfig';
 
-const loadConfig = (
-  configTitle: string,
-  overwrite: Partial<circus.Configuration> = {}
-) => {
-  const explorer = cosmiconfig(configTitle);
-  const result = explorer.searchSync() || { config: {} };
-  const config = merge.recursive(
-    {},
-    defaults,
-    result.config,
-    overwrite
-  ) as circus.Configuration;
-  return config;
-};
-
-const config = loadConfig('circus');
+const config = loadConfig(
+  [path.join(__dirname, './default')],
+  'circus'
+) as circus.Configuration;
 
 export default config;

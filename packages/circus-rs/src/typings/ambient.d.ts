646b5b213381b9a03d102bf57dd7b089b0d700e7
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-04-21 17:31:34 +0900

    Use axios instead of supertest for testing

--- packages/circus-rs/src/typings/ambient.d.ts
+++ packages/circus-rs/src/typings/ambient.d.ts
@@ -11,4 +11,3 @@ declare module 'gl-matrix/src/gl-matrix/vec4.js';
 declare module 'gl-matrix/src/gl-matrix/mat4.js';
 
 declare module 'axios/lib/adapters/http';
-declare module 'supertest';
--- packages/circus-rs/test/Server.test.ts
+++ packages/circus-rs/test/Server.test.ts
@@ -1,4 +1,3 @@
-import supertest from 'supertest';
 import createServiceLoader, {
   RsServices
 } from '../src/server/helper/createServiceLoader';
@@ -9,6 +8,7 @@ import adapter from 'axios/lib/adapters/http';
 import { Server } from 'http';
 import { ServiceLoader } from '@utrad-ical/circus-lib';
 import { DicomFileRepository } from '@utrad-ical/circus-lib/lib/dicom-file-repository';
+import status from 'http-status';
 
 const port = 1024;
 
@@ -52,212 +52,184 @@ const fillMockImages = async (dicomFileRepository: DicomFileRepository) => {
   }
 };
 
-describe('always', () => {
-  /** @type Server */
-  let httpServer: Server;
-  let loader: ServiceLoader<RsServices>;
-  beforeAll(async () => {
-    loader = createServiceLoader(testConfig as any);
-    const dicomFileRepository = await loader.get('dicomFileRepository');
-    await fillMockImages(dicomFileRepository);
-    const app = await loader.get('rsServer');
-    app.proxy = true;
-    await new Promise(resolve => {
-      httpServer = app.listen(port);
-      httpServer = httpServer.on('listening', resolve);
-    });
-  });
-  afterAll(async () => {
-    await new Promise(resolve => {
-      httpServer.on('close', resolve);
-      httpServer.close();
-    });
-    await loader.dispose();
-  });
+interface TestServer {
+  loader: ServiceLoader<RsServices>;
+  server: Server;
+}
 
-  it('must return JSON for status', async () => {
+const startTestServer = async (config: any) => {
+  const loader = createServiceLoader(config);
+  const dicomFileRepository = await loader.get('dicomFileRepository');
+  await fillMockImages(dicomFileRepository);
+  const app = await loader.get('rsServer');
+  app.proxy = true;
+  return new Promise<TestServer>(resolve => {
+    const server = app.listen(port);
+    server.on('listening', () => resolve({ loader, server }));
+  });
+};
+
+const stopHttpd = (server: Server) => {
+  return new Promise<void>(resolve => {
+    server.on('close', () => {
+      server.off('close', resolve);
+      resolve();
+    });
+    server.close();
+  });
+};
+
+const stopTestServer = async (testServer: TestServer) => {
+  await stopHttpd(testServer.server);
+  await testServer.loader.dispose();
+};
+
+describe('without auth', () => {
+  let testServer: TestServer;
+
+  beforeAll(async () => (testServer = await startTestServer(testConfig)));
+
+  afterAll(() => stopTestServer(testServer));
+
+  test('status', async () => {
     const res = await axios.get('/status');
-    expect(res.status).toBe(200);
+    expect(res.status).toBe(status.OK);
     expect(res.headers['content-type']).toMatch('application/json');
     expect(res.data).toMatchObject({ status: 'Running' });
   });
 
-  it('must reject invalid access using globalIpFilter', function(done: Function) {
-    supertest(httpServer)
-      .get('/status')
-      .set('X-Forwarded-For', '127.0.0.11') // change IP
-      .expect(401)
-      .end(done);
+  test('globalIpFilter', async () => {
+    const res = await axios.get('/status', {
+      headers: { 'X-Forwarded-For': '127.0.0.11 ' }
+    });
+    expect(res.status).toBe(status.UNAUTHORIZED);
   });
 
-  it('must return 404 for nonexistent route', function(done: Function) {
-    supertest(httpServer)
-      .get('/foobar')
-      .expect(404)
-      .end(done);
+  test('must return 404 for nonexistent route', async () => {
+    const res = await axios.get('/foobar');
+    expect(res.status).toBe(status.NOT_FOUND);
   });
 
-  describe('series middleware', function() {
-    it('must return valid access-control headers', function(done: Function) {
-      supertest(httpServer)
-        .options('/series/1.2.3.4.5/metadata')
-        .expect((res: any) => {
-          const { header } = res;
-          if (
-            !('access-control-allow-origin' in header) ||
-            header['access-control-allow-methods'] !== 'GET'
-          )
-            throw new Error('Invalid access-control headers');
-        })
-        .end(done);
+  describe('series middleware', () => {
+    test('CORS headers', async () => {
+      const res = await axios.request({
+        method: 'options',
+        url: '/series/1.2.3.4.5/metadata'
+      });
+      expect(res.headers['access-control-allow-methods']).toBe('GET');
     });
 
     // it.skip('must return 404 for nonexistent series');
 
     describe('metadata', () => {
-      test('return metadata without estimated window by default', async () => {
+      test('returns no window by default', async () => {
         const res = await axios.get('/series/1.2.3.4.5/metadata');
-        expect(res.status).toBe(200);
-
-        // supertest(httpServer)
-        //   .get('/series/1.2.3.4.5/metadata')
-        //   .expect(200)
-        //   .expect((res: any) => {
-        //     if (res.body.estimatedWindow !== undefined)
-        //       throw new Error('Invalid implemention');
-        //   })
-        //   .expect('Content-Type', /application\/json/)
-        //   .end(done);
+        expect(res.status).toBe(status.OK);
+        expect(res.data.estimatedWindow).toBeUndefined();
       });
 
-      test('must return metadata without estimated window if estimatedWindow paramator is "none"', function(done: Function) {
-        supertest(httpServer)
-          .get('/series/1.2.3.4.5/metadata')
-          .expect(200)
-          .expect((res: any) => {
-            if (res.body.estimatedWindow !== undefined)
-              throw new Error('Invalid implemention');
-          })
-          .expect('Content-Type', /application\/json/)
-          .end(done);
+      test('returns no window if window=none', async () => {
+        const res = await axios.get(
+          '/series/1.2.3.4.5/metadata?estimateWindow=none'
+        );
+        expect(res.status).toBe(status.OK);
+        expect(res.data.estimatedWindow).toBeUndefined();
       });
 
-      it('must return metadata with estimated window which was calculated by "first" algorythm', function(done: Function) {
-        supertest(httpServer)
-          .get('/series/1.2.3.4.5/metadata?estimateWindow=first')
-          .expect(200)
-          .expect((res: any) => {
-            if (res.body.estimatedWindow === undefined)
-              throw new Error('Invalid implemention');
-          })
-          .expect('Content-Type', /application\/json/)
-          .end(done);
+      test('returns estimated window with window=first', async () => {
+        const res = await axios.get(
+          '/series/1.2.3.4.5/metadata?estimateWindow=first'
+        );
+        expect(res.status).toBe(status.OK);
+        expect(res.data.estimatedWindow).toMatchObject({
+          level: 192,
+          width: 2498
+        });
       });
 
-      it('must return metadata with estimated window which was calculated by "center" algorythm', function(done: Function) {
-        supertest(httpServer)
-          .get('/series/1.2.3.4.5/metadata?estimateWindow=center')
-          .expect(200)
-          .expect((res: any) => {
-            if (res.body.estimatedWindow === undefined)
-              throw new Error('Invalid implemention');
-          })
-          .expect('Content-Type', /application\/json/)
-          .end(done);
+      test('returns estimated window with window=center', async () => {
+        const res = await axios.get(
+          '/series/1.2.3.4.5/metadata?estimateWindow=center'
+        );
+        expect(res.status).toBe(status.OK);
+        expect(res.data.estimatedWindow).toMatchObject({
+          level: 192,
+          width: 2498
+        });
       });
 
-      it('must return metadata with estimated window which was calculated by "full" algorythm', function(done: Function) {
-        supertest(httpServer)
-          .get('/series/1.2.3.4.5/metadata?estimateWindow=full')
-          .expect(200)
-          .expect((res: any) => {
-            if (res.body.estimatedWindow === undefined)
-              throw new Error('Invalid implemention');
-          })
-          .expect('Content-Type', /application\/json/)
-          .end(done);
+      it('returns estimated window with window=full', async () => {
+        const res = await axios.get(
+          '/series/1.2.3.4.5/metadata?estimateWindow=full'
+        );
+        expect(res.status).toBe(status.OK);
+        expect(res.data.estimatedWindow).toMatchObject({
+          level: 192,
+          width: 2498
+        });
       });
 
-      it('must return partial metadata', function(done: Function) {
-        supertest(httpServer)
-          .get('/series/1.2.3.4.5/metadata?start=5&end=15&delta=2')
-          .expect(200)
-          .expect((res: any) => {
-            const { voxelCount } = res.body;
-            if (voxelCount[2] !== [5, 7, 9, 11, 13, 15].length)
-              throw new Error('Invalid partial metadata');
-          })
-          .expect('Content-Type', /application\/json/)
-          .end(done);
+      test('returns partial metadata', async () => {
+        const res = await axios.get(
+          '/series/1.2.3.4.5/metadata?start=5&end=15&delta=2'
+        );
+        expect(res.status).toBe(status.OK);
+        // 6 slices (5, 7, 9, 11, 13, 15)
+        expect(res.data.voxelCount).toEqual([512, 512, 6]);
       });
     });
 
     describe('volume', () => {
-      let fullVolumeSize = Infinity;
-
-      it('must return volume', function(done: Function) {
-        supertest(httpServer)
-          .get('/series/1.2.3.4.5/volume')
-          .expect(200)
-          .expect((res: any) => (fullVolumeSize = res.header['content-length']))
-          .expect('Content-Type', 'application/octet-stream')
-          .end(done);
+      test('returns raw volume', async () => {
+        const res = await axios.get('/series/1.2.3.4.5/volume');
+        expect(res.status).toBe(status.OK);
+        expect(res.headers['content-type']).toBe('application/octet-stream');
+        expect(res.headers['content-length']).toBe(String(512 * 512 * 20 * 2));
       });
 
-      it('must return partial volume', function(done: Function) {
-        supertest(httpServer)
-          .get('/series/1.2.3.4.5/volume?start=5&end=15&delta=2')
-          .expect(200)
-          .expect('Content-Type', 'application/octet-stream')
-          .expect((res: any) => {
-            const a = res.header['content-length'] / fullVolumeSize;
-            const b = [5, 7, 9, 11, 13, 15].length / MOCK_IMAGE_COUNT;
-            if (a !== b) throw new Error('Invalid implemention');
-          })
-          .end(done);
+      test('returns partial volume', async () => {
+        const res = await axios.get(
+          '/series/1.2.3.4.5/volume?start=5&end=15&delta=2'
+        );
+        expect(res.status).toBe(status.OK);
+        expect(res.headers['content-type']).toBe('application/octet-stream');
+        expect(res.headers['content-length']).toBe(String(512 * 512 * 6 * 2));
       });
     });
 
     describe('scan', () => {
-      it('must return oblique image in binary format', function(done: Function) {
-        const test = supertest(httpServer).get('/series/1.2.3.4.5/scan');
-        // if (token) test.set('Authorization', 'Bearer ' + token);
-        test
-          .query({
+      test('return oblique image in binary format', async () => {
+        const res = await axios.get('/series/1.2.3.4.5/scan', {
+          params: {
             origin: '200,200,50',
             xAxis: '512,0,0',
             yAxis: '0,512,0',
             size: '50,50'
-          })
-          .expect(200)
-          .expect('Content-Type', 'application/octet-stream')
-          .end(done);
+          }
+        });
+        expect(res.status).toBe(status.OK);
+        expect(res.headers['content-type']).toBe('application/octet-stream');
       });
 
-      it('must return oblique image from partial volume', function(done: Function) {
-        const test = supertest(httpServer).get('/series/1.2.3.4.5/scan');
-        // if (token) test.set('Authorization', 'Bearer ' + token);
-        test
-          .query({
+      test('return oblique image from partial volume', async () => {
+        const res = await axios.get('/series/1.2.3.4.5/scan', {
+          params: {
             start: 5,
             end: 15,
             delta: 2,
-
             origin: '200,200,50',
             xAxis: '512,0,0',
             yAxis: '0,512,0',
             size: '50,50'
-          })
-          .expect(200)
-          .expect('Content-Type', 'application/octet-stream')
-          .end(done);
+          }
+        });
+        expect(res.status).toBe(status.OK);
+        expect(res.headers['content-type']).toBe('application/octet-stream');
       });
 
-      it('must return oblique image in PNG format', function(done: Function) {
-        const test = supertest(httpServer).get('/series/1.2.3.4.5/scan');
-        // if (token) test.set('Authorization', 'Bearer ' + token);
-        test
-          .query({
+      it('return oblique image in PNG', async () => {
+        const res = await axios.get('/series/1.2.3.4.5/scan', {
+          params: {
             origin: '200,200,50',
             xAxis: '512,0,0',
             yAxis: '0,512,0',
@@ -265,18 +237,17 @@ describe('always', () => {
             format: 'png',
             ww: 50,
             wl: 50
-          })
-          .expect(200)
-          .expect('Content-Type', 'image/png')
-          .end(done);
+          }
+        });
+        expect(res.status).toBe(status.OK);
+        expect(res.headers['content-type']).toBe('image/png');
       });
     });
   });
 });
 
 describe('with authentication', () => {
-  let httpServer: Server;
-  let loader: ServiceLoader<RsServices>;
+  let testServer: TestServer;
   beforeAll(async () => {
     const config = {
       ...testConfig,
@@ -291,70 +262,47 @@ describe('with authentication', () => {
         }
       }
     };
-    loader = createServiceLoader(config);
-    const dicomFileRepository = await loader.get('dicomFileRepository');
-    await fillMockImages(dicomFileRepository);
-    const app = await loader.get('rsServer');
-    app.proxy = true;
-    await new Promise(resolve => {
-      httpServer = app.listen(port);
-      httpServer = httpServer.on('listening', resolve);
+    testServer = await startTestServer(config);
+  });
+  afterAll(() => stopTestServer(testServer));
+
+  let tokenHeader: any;
+
+  test('reject token request from invalid IP', async () => {
+    const res = await axios.get('/token', {
+      headers: { 'X-Forwarded-For': '127.0.0.2' }
     });
+    expect(res.status).toBe(status.UNAUTHORIZED);
   });
-  afterAll(async done => {
-    await new Promise(resolve => {
-      httpServer.on('close', resolve);
-      httpServer.close();
+
+  test('issue valid token', async () => {
+    const res = await axios.get('/token', {
+      params: { series: '1.2.3.4.5' }
     });
-    await loader.dispose();
+    expect(res.status).toBe(status.OK);
+    expect(res.data).toMatchObject({ result: 'OK' });
+    tokenHeader = { Authorization: `Bearer ${res.data.token}` };
   });
 
-  let token: string;
-  it('must reject token request from invalid IP', function(done: Function) {
-    supertest(httpServer)
-      .get('/token')
-      .set('X-Forwarded-For', '127.0.0.2')
-      .expect(401)
-      .end(done);
+  test('return error if token not passed', async () => {
+    const res = await axios.get('/series/1.2.3.4.5/metadata');
+    expect(res.status).toBe(status.UNAUTHORIZED);
   });
 
-  it('must issue valid token', function(done: Function) {
-    supertest(httpServer)
-      .get('/token')
-      .query({ series: '1.2.3.4.5' })
-      .expect((res: any) => {
-        let result;
-        ({ result, token } = res.body);
-        if (result !== 'OK') throw new Error('Issuing token failed');
-      })
-      .expect(200)
-      .end(done);
+  test('return error for unmatched token', async () => {
+    const res = await axios.get('/series/8.8.8.8.8/metadata', {
+      headers: tokenHeader
+    });
+    expect(res.status).toBe(status.UNAUTHORIZED);
   });
 
-  it('must return authentication error if token not passed', function(done: Function) {
-    supertest(httpServer)
-      .get('/series/1.2.3.4.5/metadata')
-      .expect(401)
-      .end(done);
-  });
-
-  it('must return authentication error if passed token is not matched', function(done: Function) {
-    supertest(httpServer)
-      .get('/series/8.8.8.8.8/metadata')
-      .set('Authorization', 'Bearer ' + token)
-      .expect(401)
-      .end(done);
-  });
-
-  it('must return content if token is valid', function(done: Function) {
-    supertest(httpServer)
-      .get('/series/1.2.3.4.5/metadata')
-      .set('Authorization', 'Bearer ' + token)
-      .expect(200)
-      .end(done);
+  test('return content for valid token', async () => {
+    const res = await axios.get('/series/1.2.3.4.5/metadata', {
+      headers: tokenHeader
+    });
+    expect(res.status).toBe(status.OK);
   });
 });
 
 // Todo:
 // requireEstimatedWindow=false
-// partialVolume

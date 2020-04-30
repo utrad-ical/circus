8029656004d5598f434ee41f1e4790f3d9e0f1ae
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-04-30 17:40:04 +0900

    Fix path resolution

--- packages/circus-api/src/config/index.ts
+++ packages/circus-api/src/config/index.ts
@@ -1,8 +1,12 @@
 import loadConfig from '@utrad-ical/circus-lib/lib/config/loadConfig';
 import path from 'path';
 
+const csDefault = require.resolve(
+  '@utrad-ical/circus-cs-core/src/config/default'
+);
+
 const config = loadConfig([
-  '@utrad-ical/circus-cs-core/src/config/default', // CS Core Defaults
+  csDefault, // CS Core Defaults
   path.join(__dirname, 'default') // API server defaults
 ]);
 

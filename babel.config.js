d123ff98e2b8b6db3e0d80db98cb5c6adb570905
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-05-06 06:16:41 +0900

    Install @babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: '8' } }],
+    ['@babel/preset-env', { targets: { node: '10' } }],
     '@babel/preset-typescript'
   ],
+  plugins: ['@babel/plugin-proposal-class-properties'],
   sourceMaps: true
 };

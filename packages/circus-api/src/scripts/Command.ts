eeb5a0c5388bb867b734060d573d33f53dc45903
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-05-05 19:44:53 +0900

    Migrate eslint in circus-api

--- packages/circus-api/src/scripts/Command.ts
+++ packages/circus-api/src/scripts/Command.ts
@@ -3,4 +3,5 @@ import { FunctionService } from '@utrad-ical/circus-lib';
 export type CommandFunc = (options: any) => Promise<void>;
 type Command<D> = FunctionService<CommandFunc, D>;
 
+// eslint-disable-next-line
 export default Command;

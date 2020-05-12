213a4bcf38f0ea33e81a34dafca7020ddab5311b
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-05-12 11:08:16 +0900

    Fix warning from 'history'

--- packages/circus-web-ui/src/browserHistory.js
+++ packages/circus-web-ui/src/browserHistory.js
@@ -1,6 +1,6 @@
 // https://github.com/ReactTraining/react-router/blob/master/FAQ.md#how-do-i-access-the-history-object-outside-of-components
 
-import createBrowserHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 export default history;

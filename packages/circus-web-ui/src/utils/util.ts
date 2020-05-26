a00b82a4dc8d00d4b7e23df8352e191ad966c2d5
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-05-26 13:04:30 +0900

    Convert some modules to TS

--- packages/circus-web-ui/src/utils/util.ts
+++ packages/circus-web-ui/src/utils/util.ts
@@ -1,11 +1,11 @@
 import { createHash } from 'crypto';
 
-export const escapeRegExp = str => {
+export const escapeRegExp = (str: string) => {
   str = str + '';
   return str.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&');
 };
 
-export const sha1 = arrayBuf => {
+export const sha1 = (arrayBuf: ArrayBuffer) => {
   const sha = createHash('sha1');
   sha.update(Buffer.from(arrayBuf));
   return sha.digest('hex');

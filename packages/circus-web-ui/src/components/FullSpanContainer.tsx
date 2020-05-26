647a40f26e3ad1d34cf7b2b6a9218d95d7ca2a8d
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-05-26 15:09:53 +0900

    Convert some comps to TS

--- packages/circus-web-ui/src/components/FullSpanContainer.tsx
+++ packages/circus-web-ui/src/components/FullSpanContainer.tsx
@@ -4,7 +4,7 @@ import useBodyClass from 'utils/useBodyClass';
 /**
  * This adds "full-span-container" body class from within a component.
  */
-const FullSpanContainer = props => {
+const FullSpanContainer: React.FC = props => {
   useBodyClass('full-span-container');
   return <Fragment>{props.children}</Fragment>;
 };

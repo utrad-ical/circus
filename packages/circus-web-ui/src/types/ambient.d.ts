6f2a04f367c64a0b842d76f2bff6e373ea8ee995
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-05-28 12:25:31 +0900

    Remove @types/styled-components to avoid build issue

--- packages/circus-web-ui/src/types/ambient.d.ts
+++ packages/circus-web-ui/src/types/ambient.d.ts
@@ -1,3 +1,7 @@
+// There is @types/styled-components, but it causes errors.
+// See: https://github.com/styled-components/styled-components/issues/2383
+declare module 'styled-components';
+
 declare module 'react-bootstrap/lib/Button';
 declare module '@smikitky/rb-components';
 declare module '@smikitky/rb-components/lib/Icon';

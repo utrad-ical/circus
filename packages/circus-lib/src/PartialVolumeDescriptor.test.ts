ef95bce7593a77449cb1444bb325554abbd2422e
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-05-29 13:47:20 +0900

    Add function to check if multirange includes PVD

--- packages/circus-lib/src/PartialVolumeDescriptor.test.ts
+++ packages/circus-lib/src/PartialVolumeDescriptor.test.ts
@@ -1,7 +1,9 @@
 import PartialVolumeDescriptor, {
   describePartialVolumeDescriptor,
-  isValidPartialVolumeDescriptor
+  isValidPartialVolumeDescriptor,
+  rangeHasPartialVolume
 } from './PartialVolumeDescriptor';
+import { multirange } from 'multi-integer-range';
 
 describe('describePartialVolumeDescriptor', () => {
   const a: PartialVolumeDescriptor = { start: 1, end: 11, delta: 2 };
@@ -14,7 +16,7 @@ describe('describePartialVolumeDescriptor', () => {
     [b, 3, '5, 4, ..., 1'],
     [b, 10, '5, 4, 3, 2, 1'],
     [{ start: 1, end: 0, delta: 1 }, undefined, 'Invalid']
-  ])('%s, %s -> %s', (pvd, count, expected) => {
+  ])('%s, %s -> %p', (pvd, count, expected) => {
     expect(describePartialVolumeDescriptor(pvd, count)).toBe(expected);
   });
 });
@@ -36,5 +38,37 @@ describe('isValidPartialVolumeDescriptor', () => {
     expect(isValidPartialVolumeDescriptor({ start, end, delta })).toBe(
       expected
     );
+  });
+});
+
+describe('rangeHasPartialVolume', () => {
+  test.each([
+    ['1-50', 1, 5, 1, true],
+    ['1-50', 1, 5, 2, true],
+    ['1-50', 10, 70, 1, false],
+    ['1-50', 10, 70, 2, false],
+    ['1-50', 3, 1, -1, true],
+    ['1-50', 70, 10, -1, false],
+    ['1,3,5,7,9', 1, 9, 2, true],
+    ['1,3,5,7,9', 9, 1, -2, true],
+    ['1,3,9', 1, 9, 1, false],
+    ['1,3,9', 1, 9, 2, false],
+    ['1,3,9', 9, 1, -2, false]
+  ])(
+    '%p in (%d, %d, %d) should be %p',
+    (range, start, end, delta, expected) => {
+      const mr = multirange(range);
+      expect(rangeHasPartialVolume(mr, { start, end, delta })).toBe(expected);
+    }
+  );
+
+  test('invalid PVD throws', () => {
+    expect(() =>
+      rangeHasPartialVolume(multirange('1-100'), {
+        start: 1,
+        end: 10,
+        delta: 2
+      })
+    ).toThrow('Invalid');
   });
 });
--- packages/circus-lib/src/PartialVolumeDescriptor.ts
+++ packages/circus-lib/src/PartialVolumeDescriptor.ts
@@ -1,3 +1,5 @@
+import MultiRange from 'multi-integer-range';
+
 /**
  * Defines a set of integers to construct a partial volume
  * from a DICOM series.
@@ -57,4 +59,25 @@ export const isValidPartialVolumeDescriptor = (
   if (!isNatural((end - start) / delta + 1)) return false;
   if (start === end && delta === -1) return false;
   return true;
+};
+
+/**
+ * Returns true if the partial volume described with `descriptor`
+ * is includd in the images described sith `range`.
+ * @param range The image range to check.
+ * @param desciptor The
+ */
+export const rangeHasPartialVolume = (
+  range: MultiRange,
+  descriptor: PartialVolumeDescriptor
+) => {
+  if (!isValidPartialVolumeDescriptor(descriptor))
+    throw new Error('Invalid partial volume descriptor');
+  const { start, end, delta } = descriptor;
+  if (range.has([[Math.min(start, end), Math.max(start, end)]])) return true;
+  if (delta !== 1 && delta !== -1) {
+    for (let i = start; i !== end; i += delta) if (!range.has(i)) return false;
+    return true;
+  }
+  return false;
 };

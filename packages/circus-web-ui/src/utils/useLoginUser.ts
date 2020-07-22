e1dd5b88cf79959400002e1f5fcc4046261b92ea
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-07-22 22:33:06 +0900

    Assume loginUser.data exists

--- packages/circus-web-ui/src/utils/useLoginUser.ts
+++ packages/circus-web-ui/src/utils/useLoginUser.ts
@@ -2,7 +2,8 @@ import { useSelector } from 'react-redux';
 
 /**
  * A React cutsom hook to fetch login user from redux store.
+ * This assumes you are using this hook after a successful login.
  */
-const useLoginUser = () => useSelector(state => state.loginUser.data);
+const useLoginUser = () => useSelector(state => state.loginUser.data!);
 
 export default useLoginUser;

e6c8ba2c2a20a873d1e620b83df94398c2c3f4e1
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-07-24 09:38:11 +0900

    Improve Admin page layout

--- packages/circus-web-ui/src/pages/admin/AdminIndex.tsx
+++ packages/circus-web-ui/src/pages/admin/AdminIndex.tsx
@@ -3,21 +3,31 @@ import { Link } from 'react-router-dom';
 import { Button } from 'components/react-bootstrap';
 import AdminContainer from './AdminContainer';
 import Icon from 'components/Icon';
+import styled from 'styled-components';
 
 const Btn: React.FC<{
   to: string;
   link: string;
   glyph: string;
 }> = ({ to, link, glyph }) => (
-  <div className="item">
-    <Link to={`admin/${to}`}>
-      <Button block bsStyle="primary" bsSize="lg">
-        <Icon icon={glyph} />
-        &ensp;{link}
-      </Button>
-    </Link>
-  </div>
+  <Link to={`admin/${to}`}>
+    <Button block bsStyle="primary" bsSize="lg">
+      <Icon icon={glyph} />
+      &ensp;{link}
+    </Button>
+  </Link>
 );
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  > a {
+    display: block;
+    margin-bottom: 20px;
+    margin-right: 10px;
+    flex: 0 0 320px;
+  }
+`;
 
 const AdminIndex: React.FC = () => {
   return (
@@ -26,25 +36,25 @@ const AdminIndex: React.FC = () => {
       icon="circus-administration"
       className="admin-index"
     >
-      <div className="row">
+      <Row>
         <Btn to="general" link="Server Configuration" glyph="th-large" />
-      </div>
-      <div className="row">
+      </Row>
+      <Row>
         <Btn to="group" link="Groups" glyph="record" />
         <Btn to="user" link="Users" glyph="user" />
-      </div>
+      </Row>
       <h3>CIRCUS DB</h3>
-      <div className="row">
+      <Row>
         <Btn to="project" link="Projects" glyph="education" />
-      </div>
+      </Row>
       <h3>CIRCUS CS</h3>
-      <div className="row">
+      <Row>
         <Btn to="plugin-job-manager" link="Job Manager" glyph="list-alt" />
         <Btn to="plugins" link="CAD Plug-ins" glyph="circus-app" />
-      </div>
-      <div className="row">
+      </Row>
+      <Row>
         <Btn to="plugin-job-queue" link="Job Queue" glyph="list" />
-      </div>
+      </Row>
     </AdminContainer>
   );
 };

30f8d7a06f0f2dfb3c3e3740a120ce3539fe88af
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-09-13 18:36:11 +0900

    Improve loading indicator

--- packages/circus-rs/src/browser/viewer/defaultLoadingIndicator.ts
+++ packages/circus-rs/src/browser/viewer/defaultLoadingIndicator.ts
@@ -2,16 +2,18 @@ import LoadingIndicator from '../interface/LoadingIndicator';
 
 const defaultLoadingIndicator: LoadingIndicator = (ctx, time) => {
   const size = 48;
+  const lineWidth = 8;
   const { width, height } = ctx.canvas;
   ctx.clearRect(0, 0, width, height);
   ctx.save();
   try {
-    const angle = time / 200;
-    ctx.lineWidth = 8;
+    const angle = time / 150;
+    const angleDiff = (Math.sin(time / 400) * 0.5 + 1) * Math.PI;
+    ctx.lineWidth = lineWidth;
     ctx.lineCap = 'round';
     ctx.strokeStyle = '#ffffff';
     ctx.beginPath();
-    ctx.arc(width / 2, height / 2, size / 2, angle, angle + Math.PI);
+    ctx.arc(width / 2, height / 2, size / 2, angle, angle + angleDiff);
     ctx.stroke();
   } finally {
     ctx.restore();

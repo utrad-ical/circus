78abf4054270ffb27d9f7657c7763b6d244a0195
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-09-23 02:18:02 +0900

    Add "id" property to base Annotation interface

--- packages/circus-rs/src/browser/annotation/Annotation.ts
+++ packages/circus-rs/src/browser/annotation/Annotation.ts
@@ -4,6 +4,13 @@ import Viewer from '../viewer/Viewer';
 export interface DrawOption {
   hover: boolean;
 }
+
 export default interface Annotation {
   draw(viewer: Viewer, viewState: ViewState, options: DrawOption): void;
+
+  /**
+   * This ID is not used by CIRCUS RS itself, but your application can use
+   * this property to identify this annotation.
+   */
+  id?: string;
 }

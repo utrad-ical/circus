e56a88f0cd28743ae2931f79f81f560ea49dff71
Masaya Akiyama <makiyama-tky@umin.ac.jp>
2020-09-28 01:46:16 +0000

    Add a handler for download and add fields to save

--- packages/circus-api/src/api/tasks/index.test.ts
+++ packages/circus-api/src/api/tasks/index.test.ts
@@ -8,31 +8,42 @@ beforeAll(async () => {
   apiTest = await setUpAppForRoutesTest();
   axios = apiTest.axiosInstances.alice;
 });
-afterAll(async () => await apiTest.tearDown());
+afterAll(async () => apiTest.tearDown());
 
-it('should return the list of tasks of the user', async () => {
-  const res = await axios.get('api/tasks');
-  expect(res.status).toBe(200);
-  expect(res.data.items).toHaveLength(2);
+describe('search', () => {
+  test('should return the list of tasks of the user', async () => {
+    const res = await axios.get('api/tasks');
+    expect(res.status).toBe(200);
+    expect(res.data.items).toHaveLength(3);
+  });
+
+  test('search with taskId', async () => {
+    const res = await axios.get('api/tasks', {
+      params: { filter: JSON.stringify({ taskId: 'aaaabbbbcccc1111' }) }
+    });
+    expect(res.status).toBe(200);
+    expect(res.data.items).toHaveLength(1);
+    expect(res.data.items[0].name).toBe('Importing DICOM files');
+  });
+
+  test('should not show tasks of other user in search results', async () => {
+    const res = await axios.get('api/tasks', {
+      params: { filter: JSON.stringify({ taskId: 'bbbbccccdddd1111' }) } // Bob's task
+    });
+    expect(res.status).toBe(200);
+    expect(res.data.items).toHaveLength(0);
+  });
+
+  test('should throw 400 for invalid request', async () => {
+    const res = await axios.get('api/tasks', {
+      params: { filter: JSON.stringify({ invalid: 'invalid' }) }
+    });
+    expect(res.status).toBe(400);
+    expect(res.data.error).toBe('Bad filter.');
+  });
 });
 
-it.skip('should return the information of the specified task', async () => {
-  const res = await axios.get('api/tasks/alice@example.com');
-  expect(res.status).toBe(200);
-  expect(res.data.owner).toBe('alice@example.com');
-});
-
-it.skip('should return 404 for nonexistent task', async () => {
-  const res = await axios.get('api/tasks/aaaabbbbcccc0000');
-  expect(res.status).toBe(404);
-});
-
-it.skip("should return unauthorized for someone else's task", async () => {
-  const res = await axios.get('api/tasks/aaaabbbbcccc2222');
-  expect(res.status).toBe(403);
-});
-
-test.only('report', async () => {
+test('report', async () => {
   const { taskId, emitter } = await apiTest.taskManager.register(
     { body: null } as any,
     { name: 'Dummy', userEmail: 'alice@example.com' }
@@ -48,5 +59,20 @@ test.only('report', async () => {
   const data = await streamFinish();
   expect(data).toMatch(taskId);
   expect(data).toMatch('Working!');
-  emitter.emit('finish', 'Finishing this.');
+  emitter.emit('finish');
+});
+
+describe('download', () => {
+  test('Returns 401 for unauthorized task', async () => {
+    const res = await axios.get('api/tasks/bbbbccccdddd1111/download'); //Bob's task
+    expect(res.status).toBe(401);
+  });
+  test('Returns 400 when task has no downloadable file', async () => {
+    const res = await axios.get('api/tasks/aaaabbbbcccc1111/download');
+    expect(res.status).toBe(400);
+  });
+  test('Returns 409 when a task is still in progress', async () => {
+    const res = await axios.get('api/tasks/aaaabbbbcccc3333/download');
+    expect(res.status).toBe(409);
+  });
 });

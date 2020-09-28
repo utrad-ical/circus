8750d4f3e5fe1dbb9a2b4512f98e476d521b6892
Masaya Akiyama <makiyama-tky@umin.ac.jp>
2020-09-28 01:46:16 +0000

    Add a handler for download and add fields to save

--- packages/circus-api/src/api/tasks/index.ts
+++ packages/circus-api/src/api/tasks/index.ts
@@ -1,25 +1,28 @@
 import performSearch from '../performSearch';
-import status from 'http-status';
+import httpStatus from 'http-status';
 import { RouteMiddleware } from '../../typings/middlewares';
+import checkFilter from '../../utils/checkFilter';
+import { EJSON } from 'bson';
 
-export const handleGet: RouteMiddleware = ({ models, taskManager }) => {
+export const handleSearch: RouteMiddleware = ({
+  models,
+  taskManager,
+  logger
+}) => {
   return async (ctx, next) => {
-    const taskId = ctx.params.taskId;
-    console.log(taskId);
-    // const task = await models.task.findByIdOrFail(taskId);
-    // if (task.userEmail !== ctx.user.userEmail) {
-    //   ctx.throw(status.FORBIDDEN);
-    // }
-    // ctx.body = task;
-  };
-};
-
-export const handleSearch: RouteMiddleware = ({ models, taskManager }) => {
-  return async (ctx, next) => {
-    const userEmail = ctx.user.userEmail;
-    const filter = { userEmail };
     // custom filter provided by users
+    const urlQuery = ctx.request.query;
+    let customFilter: object;
+    try {
+      customFilter = urlQuery.filter ? EJSON.parse(urlQuery.filter) : {};
+    } catch (err) {
+      ctx.throw(httpStatus.BAD_REQUEST, 'Filter string could not be parsed.');
+    }
     const fields = ['taskId', 'status'];
+    if (!checkFilter(customFilter!, fields))
+      ctx.throw(httpStatus.BAD_REQUEST, 'Bad filter.');
+    const userEmail = ctx.user.userEmail;
+    const filter = { $and: [customFilter!, { userEmail }] };
 
     const opts = {
       transform: (data: any) => {
@@ -28,10 +31,11 @@ export const handleSearch: RouteMiddleware = ({ models, taskManager }) => {
           !taskManager.isTaskInProgress(data.taskId)
         ) {
           // This is a "zombine" task
-          models.task.modifyOne(data.taskId, {
-            status: 'error',
-            message: 'Task interrupted for unknown reason.'
-          }); // does not await!
+          models.task
+            .modifyOne(data.taskId, {
+              status: 'error'
+            })
+            .catch(err => logger.error(err)); // does not await!
           return { ...data, status: 'error' };
         }
         return data;
@@ -46,5 +50,24 @@ export const handleReport: RouteMiddleware = ({ taskManager }) => {
   return async (ctx, next) => {
     const userEmail = ctx.user.userEmail;
     taskManager.report(ctx, userEmail);
+  };
+};
+
+export const handleDownload: RouteMiddleware = ({ models, taskManager }) => {
+  return async (ctx, next) => {
+    const userEmail = ctx.user.userEmail;
+    const taskId = ctx.params.taskId;
+    const task = await models.task.findByIdOrFail(taskId);
+    if (userEmail !== task.userEmail) {
+      ctx.throw(httpStatus.UNAUTHORIZED, 'You cannot access this task.');
+    }
+    if (!task.downloadFileType)
+      ctx.throw(
+        httpStatus.BAD_REQUEST,
+        'This task is not associated with a downloadable file.'
+      );
+    if (task.status !== 'finished')
+      ctx.throw(httpStatus.CONFLICT, 'This task has not finished.');
+    taskManager.download(ctx, taskId);
   };
 };

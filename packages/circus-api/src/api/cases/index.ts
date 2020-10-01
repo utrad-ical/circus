7b45cd343b892cbe08f1b04dd1f0336d51061cac
Masaya Akiyama <makiyama-tky@umin.ac.jp>
2020-10-01 07:59:37 +0000

    Modularize a function for create a new case

--- packages/circus-api/src/api/cases/index.ts
+++ packages/circus-api/src/api/cases/index.ts
@@ -1,13 +1,10 @@
 import status from 'http-status';
 import { performAggregationSearch } from '../performSearch';
-import generateUniqueId from '../../utils/generateUniqueId';
 import { EJSON } from 'bson';
-import { fetchAccessibleSeries, UserPrivilegeInfo } from '../../privilegeUtils';
 import { packAsMhd } from '../../case/packAsMhd';
 import checkFilter from '../../utils/checkFilter';
 import { RouteMiddleware, CircusContext } from '../../typings/middlewares';
-import { Models } from '../../interface';
-import { SeriesEntry } from '../../typings/circus';
+import makeNewCase from '../../case/makeNewCase';
 
 const maskPatientInfo = (ctx: CircusContext) => {
   return (caseData: any) => {
@@ -51,63 +48,6 @@ export const handleGet: RouteMiddleware = () => {
     delete aCase.latestRevision; // Remove redundant data
     ctx.body = maskPatientInfo(ctx)(aCase);
   };
-};
-
-const makeNewCase = async (
-  models: Models,
-  user: any,
-  userPrivileges: UserPrivilegeInfo,
-  project: any,
-  series: SeriesEntry[],
-  tags: string[]
-) => {
-  const caseId = generateUniqueId();
-
-  const domains: { [domain: string]: boolean } = {};
-
-  // Check write access for the project.
-  const ok = userPrivileges.accessibleProjects.some(
-    p => p.roles.indexOf('write') >= 0 && p.projectId === project.projectId
-  );
-  if (!ok) {
-    throw new Error('You do not have write privilege for this project.');
-  }
-
-  const seriesData = await fetchAccessibleSeries(
-    models,
-    userPrivileges,
-    series
-  );
-  seriesData.forEach(i => (domains[i.domain] = true));
-
-  if (seriesData.slice(1).some(s => s.domain !== seriesData[0].domain)) {
-    const error = new Error('All series must belong to the same domain.');
-    error.status = 400;
-    throw error;
-  }
-
-  const revision = {
-    creator: user.userEmail,
-    date: new Date(),
-    description: 'Created new case.',
-    attributes: {},
-    status: 'draft',
-    series: seriesData.map(s => ({
-      seriesUid: s.seriesUid,
-      partialVolumeDescriptor: s.partialVolumeDescriptor,
-      labels: []
-    }))
-  };
-
-  await models.clinicalCase.insert({
-    caseId,
-    projectId: project.projectId,
-    tags,
-    latestRevision: revision,
-    revisions: [revision],
-    domains: Object.keys(domains)
-  });
-  return caseId;
 };
 
 export const handlePost: RouteMiddleware = ({ models }) => {

8f822b582f64efe4b78c298bbec2d557113e0de7
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-10-08 13:18:59 +0900

    Rewrite TaskList with redux-based search

--- packages/circus-web-ui/src/pages/TaskList.tsx
+++ packages/circus-web-ui/src/pages/TaskList.tsx
@@ -1,9 +1,15 @@
-import LoadingIndicator from '@smikitky/rb-components/lib/LoadingIndicator';
+import DataGrid, { DataGridColumnDefinition } from 'components/DataGrid';
 import Icon from 'components/Icon';
-import { Button } from 'components/react-bootstrap';
+import IconButton from 'components/IconButton';
+import SearchResultsView, {
+  makeSortOptions
+} from 'components/SearchResultsView';
 import TimeDisplay from 'components/TimeDisplay';
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import { useApi } from 'utils/api';
+import { newSearch } from '../store/searches';
 
 interface Task {
   taskId: string;
@@ -17,78 +23,151 @@ interface Task {
   downloadFileType?: string;
 }
 
+const sortOptions = makeSortOptions({
+  createdAt: 'task create time',
+  name: 'name'
+});
+
+const useDismiss = (taskId: string) => {
+  const api = useApi();
+  const [dismissed, setDismissed] = useState(false);
+  const dismiss = async () => {
+    await api(`/tasks/${taskId}`, {
+      method: 'patch',
+      data: { dismissed: true }
+    });
+    setDismissed(true);
+  };
+  return [dismiss, dismissed];
+};
+
+const columns: DataGridColumnDefinition<Task>[] = [
+  { caption: 'Name', key: 'name' },
+  {
+    caption: 'Status',
+    key: 'status',
+    renderer: ({ value }) => (
+      <span className={value.status}>{value.status}</span>
+    )
+  },
+  {
+    caption: 'Message',
+    key: 'message',
+    renderer: ({ value }) => {
+      return value.errorMessage ? (
+        <span className="text-danger">{value.errorMessage}</span>
+      ) : (
+        <span>{value.finishedMessage}</span>
+      );
+    }
+  },
+  {
+    caption: 'Start / End',
+    key: 'createdAt',
+    renderer: ({ value }) => {
+      return (
+        <>
+          <TimeDisplay value={value.createdAt} />
+          <br />
+          {value.endedAt ? <TimeDisplay value={value.endedAt} /> : <>&ensp;</>}
+        </>
+      );
+    }
+  },
+  {
+    caption: 'Download',
+    key: 'download',
+    renderer: ({ value }) => {
+      return value.status === 'finished' && value.downloadFileType ? (
+        <a href={``}>
+          <Icon icon="glyphicon-download" />
+        </a>
+      ) : (
+        <>-</>
+      );
+    }
+  },
+  {
+    caption: 'Dismiss',
+    key: 'dismiss',
+    renderer: (({ value }) => {
+      const [dismiss, dismissed] = useDismiss(value.taskId);
+      return value.dismissed ? null : (
+        <IconButton
+          disabled={dismissed}
+          bsStyle="primary"
+          onClick={dismiss}
+          icon="glyphicon-check"
+        >
+          Done
+        </IconButton>
+      );
+    }) as React.FC<any>
+  }
+];
+
+const DataView: React.FC<{ value: Task[] }> = props => {
+  const { value } = props;
+  return (
+    <StyledDataGrid
+      className="task-search-result"
+      columns={columns}
+      itemPrimaryKey="taskId"
+      value={value}
+    />
+  );
+};
+
+const StyledDataGrid = styled(DataGrid)`
+  .finished {
+    color: green;
+  }
+  .error {
+    color: red;
+    font-size: bold;
+  }
+  .dismiss {
+    text-align: right;
+  }
+`;
+
 const TaskList: React.FC = props => {
-  const [tasks, setTasks] = useState([]);
+  const [showDismissed, setShowDismissed] = useState(false);
+  const dispatch = useDispatch();
   const api = useApi();
 
   useEffect(() => {
-    const refresh = async () => {
-      const items = (await api('tasks')).items;
-      setTasks(items);
-    };
-    refresh();
-  }, [api]);
+    dispatch(
+      newSearch(api, 'task', {
+        resource: { endPoint: 'tasks', primaryKey: 'taskId' },
+        filter: showDismissed ? {} : { dismissed: false },
+        condition: { showDismissed },
+        sort: '{"createdAt":-1}'
+      })
+    );
+  }, [api, dispatch, showDismissed]);
 
-  if (!Array.isArray(tasks)) {
-    return <LoadingIndicator />;
-  }
   return (
     <div>
       <h1>
         <Icon icon="tasks" /> Tasks
       </h1>
-      <TaskItems items={tasks} />
+      <label>
+        <input
+          type="checkbox"
+          checked={showDismissed}
+          onChange={ev => setShowDismissed(ev.target.checked)}
+        />{' '}
+        Show dismissed items
+      </label>
+      <SearchResultsView
+        sortOptions={sortOptions}
+        dataView={DataView}
+        refreshable
+        name="task"
+      />
     </div>
   );
 };
 
 export default TaskList;
-
-const TaskItems: React.FC<{ items: Task[] }> = props => {
-  const table = (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Status</th>
-          <th>Message</th>
-          <th>Started at</th>
-          <th>Ended at</th>
-          <th>Download</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.items.map(item => (
-          <tr key={item.taskId}>
-            <td>{item.name}</td>
-            <td>{item.status}</td>
-            <td>{item.finishedMessage ?? item.errorMessage}</td>
-            <td>
-              <TimeDisplay value={item.createdAt} />
-            </td>
-            <td>{item.endedAt ? <TimeDisplay value={item.endedAt} /> : '-'}</td>
-            <td>
-              {item.downloadFileType ? (
-                <a href={`tasks/${item.taskId}/download`}>
-                  <Button>Download</Button>
-                </a>
-              ) : (
-                '-'
-              )}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-
-  const count = props.items.length;
-  const countStr = count === 1 ? `${count} result.` : `${count} results.`;
-
-  return (
-    <div>
-      <p>{countStr}</p>
-      {table}
-    </div>
-  );
-};

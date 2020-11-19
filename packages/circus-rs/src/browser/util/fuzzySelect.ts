3084c3506810aa27fcf5a74d105e58cfb591ba98
Masayuki Kano <kano@genuine-ltd.com>
2020-11-19 09:49:35 +0900

    extract line marker

--- packages/circus-rs/src/browser/util/fuzzySelect.ts
+++ packages/circus-rs/src/browser/util/fuzzySelect.ts
@@ -1,5 +1,6 @@
 import { Vector3, Box3 } from 'three';
 import { RawData, Vector3D } from '..';
+import { bitsMarker, bufferMarker } from './line-marker';
 
 /**
  * bluh bluh bluh bluh bluh bluh
@@ -31,9 +32,6 @@ export default function fuzzySelect(
 
   console.log('BEGIN');
   console.time('floodFill3D w/o write to partial cloud');
-  // const marker = stupidMarker();
-  // const marker = objectMarker();
-  const marker = bufferMarker(bounding);
   const binarize = (p: Vector3) => {
     const value = volume.getPixelAt(p.x, p.y, p.z);
     return minValue <= value && value <= maxValue;
@@ -52,7 +50,7 @@ export default function fuzzySelect(
       );
     }
   };
-  floodFill3D(startPoint, bounding, binarize, fillLine, marker);
+  floodFill3D(startPoint, bounding, binarize, fillLine);
   console.timeEnd('floodFill3D w/o write to partial cloud');
   console.log('call: ' + callCounter.toString());
   console.log(
@@ -73,134 +71,6 @@ function adjustSizeToDivisible8(size: Vector3D) {
   }
 }
 
-interface LineMarker {
-  marked(p: Vector3): boolean;
-  marks(p1: Vector3, xend: number): void;
-}
-const markStateDumper = (marked: LineMarker['marked']) => {
-  return (boundary: Box3): string => {
-    const { min, max } = boundary;
-    let dumpString = '';
-    for (let z = min.z; z <= max.z; z++) {
-      dumpString += '[' + z.toString() + ']\n';
-      for (let y = min.y; y <= max.y; y++) {
-        for (let x = min.x; x <= max.x; x++) {
-          dumpString += marked(new Vector3(x, y, z)) ? '*' : '-';
-        }
-        dumpString += '\n';
-      }
-    }
-    return dumpString;
-  };
-};
-const markStateColDumper = (marked: LineMarker['marked']) => {
-  return (boundary: Box3): string => {
-    const { min, max } = boundary;
-    let dumpString = new Array(max.y - min.y + 2).fill('').join('\n');
-
-    dumpString = dumpString
-      .split('\n')
-      .map(
-        (v, i) =>
-          v +
-          (0 < i
-            ? (min.y + i - 1).toString().padStart(5, ' ')
-            : ''.padStart(5, ' '))
-      )
-      .join('\n');
-
-    for (let z = min.z; z <= max.z; z++) {
-      let slice = '[' + z.toString() + ']';
-      while (slice.length <= max.x - min.x) {
-        slice += ' ';
-      }
-      slice += '\n';
-
-      for (let y = min.y; y <= max.y; y++) {
-        for (let x = min.x; x <= max.x; x++) {
-          slice += marked(new Vector3(x, y, z)) ? '*' : '-';
-        }
-        slice += '\n';
-      }
-
-      const sliceLines = slice.split('\n');
-      dumpString = dumpString
-        .split('\n')
-        .map((v, i) => v + '|' + sliceLines[i])
-        .join('\n');
-    }
-    dumpString = dumpString
-      .split('\n')
-      .map((v, i) => v + '|')
-      .join('\n');
-    return 'x: ' + min.x + ' - ' + max.x + '\n' + dumpString;
-  };
-};
-const bufferMarker = (boundary: Box3): LineMarker => {
-  const { min } = boundary;
-  const [width, height, depth] = boundary
-    .getSize(new Vector3())
-    .addScalar(1)
-    .toArray();
-
-  const buffer = new Uint8Array(width * height * depth);
-  const getOffset = (p: Vector3) =>
-    (p.z - min.z) * height * width + (p.y - min.y) * width + (p.x - min.x);
-
-  const marks = (p: Vector3, xend: number) => {
-    let offset = getOffset(p);
-    for (let x = p.x; x <= xend; x++) {
-      // console.error(
-      //   'marks: ' +
-      //     new Vector3(x, p.y, p.z).toArray().toString() +
-      //     '(' +
-      //     offset.toString() +
-      //     ')'
-      // );
-      buffer[offset++] = 1;
-    }
-  };
-  const marked = (p: Vector3) => buffer[getOffset(p)] === 1;
-  // const marked = (p: Vector3) => {
-  //   const o = getOffset(p);
-  //   const r = buffer[getOffset(p)] === 1;
-  //   if (r) {
-  //     console.error('marked: '+p.toArray().toString() + '(' + o.toString()+')');
-  //   }
-  //   return r;
-  // };
-
-  return { marked, marks };
-};
-const objectMarker = (): LineMarker => {
-  const collection: Record<string, true> = {};
-  const key = (p: Vector3): string =>
-    p.x.toString() + ',' + p.y.toString() + ',' + p.z.toString();
-
-  const marks = (p: Vector3, xend: number) => {
-    const cur = p.clone();
-    for (; cur.x <= xend; cur.x++) {
-      collection[key(cur)] = true;
-    }
-  };
-  const marked = (p: Vector3) => key(p) in collection;
-
-  return { marked, marks };
-};
-const stupidMarker = (): LineMarker => {
-  const collection: [number, number, number][] = [];
-
-  const marks = (p: Vector3, xend: number) => {
-    for (let x = p.x; x <= xend; x++) {
-      collection.push([x, p.y, p.z]);
-    }
-  };
-  const marked = (p: Vector3) =>
-    collection.some(q => q[0] === p.x && q[1] === p.y && q[2] === p.z);
-
-  return { marked, marks };
-};
-
 // This function does NOT take into account that the specified functions
 // might modify the argument points.
 // DON'T do that.
@@ -208,14 +78,16 @@ function floodFill3D(
   startPoint: Vector3,
   boundary: Box3,
   binarize: (p: Vector3) => boolean,
-  fillLine: (p1: Vector3, p2: Vector3) => void,
-  specfiedMarker?: LineMarker
+  fillLine: (p1: Vector3, p2: Vector3) => void
 ) {
   if (!binarize(startPoint)) return;
 
   const stack: Vector3[] = [startPoint];
   const { min, max } = boundary;
-  const { marks, marked } = specfiedMarker || bufferMarker(boundary);
+  // const { marks, marked } = bufferMarker(boundary);
+  // const { marks, marked } = stupidMarker();
+  // const { marks, marked } = objectMarker();
+  const { marks, marked } = bitsMarker(boundary);
 
   const stackPoints = (walker: Vector3, xend: number) => {
     let isScanLine = false;
@@ -290,56 +162,6 @@ function floodFill3D(
   console.log('maxStackLength');
   console.log(maxStackLength);
 }
-
-// https://github.com/ijpb/MorphoLibJ/blob/master/src/main/java/inra/ijpb/morphology/FloodFill3D.java
-const test = (o: Vector3, s: Vector3, markArguments: [Vector3, number][]) => {
-  const boundary = new Box3(o, o.clone().add(s));
-  const check = ({ marks, marked }: LineMarker) => {
-    markArguments.forEach(a => marks(...a));
-    const dumper = markStateColDumper(marked);
-    return dumper(boundary);
-  };
-  // const a = check(stupidMarker());
-  const b = check(objectMarker());
-  const c = check(bufferMarker(boundary));
-  if (b !== c) {
-    console.error('NOT MATCH MARKED');
-    console.log(b);
-    console.log(c);
-  }
-  return JSON.stringify([o, s, markArguments]);
-};
-
-// test(...randArg());
-
-function randArg() {
-  const r = (min: number, max: number) =>
-    min + Math.round(Math.random() * (max - min));
-  const o = new Vector3(r(0, 1000), r(0, 1000), r(0, 1000));
-  const s = new Vector3(r(5, 12), r(5, 15), r(5, 10));
-  const ufo = (a: number, b: number) => (a === b ? 0 : a < b ? -1 : 1);
-  const points = new Array(20)
-    .fill(null)
-    .map(
-      () => new Vector3(r(o.x, o.x + s.x), r(o.y, o.y + s.y), r(o.z, o.z + s.z))
-    )
-    .sort((a, b) => ufo(a.z, b.z) || ufo(a.y, b.y) || ufo(a.x, b.x));
-  const markArguments: [Vector3, number][] = points.map(p => [
-    p,
-    r(p.x, o.x + s.x)
-  ]);
-
-  return [o, s, markArguments] as [typeof o, typeof s, typeof markArguments];
-}
-
-function testParam([o, s, markArguments]: any) {
-  return [
-    new Vector3(o.x, o.y, o.z),
-    new Vector3(s.x, s.y, s.z),
-    markArguments.map(([p, n]: any) => [new Vector3(p.x, p.y, p.z), n])
-  ] as ReturnType<typeof randArg>;
-}
-
 // const start = new Vector3(2, 2, 2);
 // const marker = stupidMarker();
 // const dumper = markStateColDumper(marker.marked);

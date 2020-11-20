a0505000cd00c107d1914ef29b002da406235d0d
Masayuki Kano <kano@genuine-ltd.com>
2020-11-20 13:33:35 +0900

    Fix floodFill3d and add it's test

--- packages/circus-rs/src/browser/util/fuzzySelect.ts
+++ packages/circus-rs/src/browser/util/fuzzySelect.ts
@@ -1,6 +1,6 @@
 import { Vector3, Box3 } from 'three';
 import { AnisotropicRawData, RawData } from '..';
-import voxelMarker from './voxelMarker';
+import floodFill3d from './floodFill3d';
 
 type FuzzySelectTarget = '3d' | 'axial' | 'sagittal' | 'coronal';
 export default function fuzzySelect(
@@ -39,7 +39,7 @@ export default function fuzzySelect(
     }
   };
 
-  return fuzzySelectWithFloodFill3D(startPoint, offsetBox, binarizer, fillLine);
+  floodFill3d(startPoint, offsetBox, binarizer, fillLine);
 }
 
 /**
@@ -89,118 +89,4 @@ function detectOffsetBox(
   );
 
   return maxDistanceOffsetBox.intersect(mprBoudaryOffsetBox);
-}
-
-/**
- * bluh bluh bluh bluh bluh bluh
- *
- * This function does NOT take into account that the specified functions
- * might modify the argument points(Vector3).
- * DON'T do that.
- */
-function fuzzySelectWithFloodFill3D(
-  startPoint: Vector3,
-  offsetBox: Box3,
-  binarizer: (p: Vector3) => boolean,
-  fillLine: (p1: Vector3, p2: Vector3) => void
-) {
-  if (!binarizer(startPoint)) return;
-
-  const report = {
-    _start: () => (report.start = new Date().getTime()),
-    _finish: () => {
-      report.finish = new Date().getTime();
-      report.procTime = report.finish - report.start;
-      delete (report as any).start;
-      delete (report as any).finish;
-      delete (report as any)._start;
-      delete (report as any)._finish;
-      report.matchedRate =
-        Math.round((report.matchedVoxels / report.targetVoxels) * 10000) / 100;
-    },
-    start: 0,
-    finish: 0,
-    targetVoxels:
-      (offsetBox.max.x - offsetBox.min.x + 1) *
-      (offsetBox.max.y - offsetBox.min.y + 1) *
-      (offsetBox.max.z - offsetBox.min.z + 1),
-    matchedVoxels: 0,
-    matchedRate: 0.0,
-    fillLineCalled: 0,
-    maxStackLength: 0,
-    procTime: 0
-  };
-  report._start();
-
-  const stack: Vector3[] = [startPoint];
-  const { min, max } = offsetBox;
-  const { marks, marked } = voxelMarker(offsetBox);
-
-  const stackPoints = (walker: Vector3, xend: number) => {
-    let isScanLine = false;
-    for (; walker.x <= xend; walker.x++) {
-      const mustStack = !marked(walker) && binarizer(walker);
-      if (!isScanLine && mustStack) {
-        stack.push(walker.clone());
-        isScanLine = true;
-      } else if (isScanLine && !mustStack) {
-        isScanLine = false;
-      }
-    }
-  };
-
-  while (stack.length > 0) {
-    report.maxStackLength = Math.max(report.maxStackLength, stack.length);
-    const cur = stack.shift()!;
-    if (marked(cur)) continue;
-
-    // x adjacent points of scan-line's endpoint
-    let left = cur.clone();
-    let right = cur.clone();
-
-    // find start of scan-line
-    do {
-      left.x--;
-    } while (min.x <= left.x && !marked(left) && binarizer(left));
-    // find start of scan-line
-    do {
-      right.x++;
-    } while (right.x <= max.x && !marked(right) && binarizer(right));
-
-    // adjust the points to endpoint from theirs neighbor.
-    left.x++;
-    right.x--;
-
-    // mark as scaned
-    marks(left, right.x);
-
-    // process the line
-    report.fillLineCalled++;
-    report.matchedVoxels += right.x - left.x + 1;
-    fillLine(left, right);
-
-    // find scan-lines above the current one
-    if (min.y < cur.y) {
-      const walker = left.clone().setY(cur.y - 1);
-      stackPoints(walker, right.x);
-    }
-    // find scan-lines below the current one
-    if (cur.y < max.y) {
-      const walker = left.clone().setY(cur.y + 1);
-      stackPoints(walker, right.x);
-    }
-    // find scan-lines front the current one
-    if (min.z < cur.z) {
-      const walker = left.clone().setZ(cur.z - 1);
-      stackPoints(walker, right.x);
-    }
-    // find scan-lines behind the current one
-    if (cur.z < max.z) {
-      const walker = left.clone().setZ(cur.z + 1);
-      stackPoints(walker, right.x);
-    }
-  }
-
-  report._finish();
-  return report;
 }
--- packages/circus-rs/src/browser/util/voxelMarker.test.ts
+++ packages/circus-rs/src/browser/util/voxelMarker.test.ts
@@ -1,4 +1,5 @@
 import { Vector3, Box3 } from 'three';
+import createMarkedStateDumper from './createMarkedStateDumper';
 import voxelMarker, { VoxelMarker } from './voxelMarker';
 
 test.skip('voxelMarker must work exactly', () => {
@@ -87,48 +88,4 @@ const objectMarker = (): VoxelMarker => {
   const marked = (p: Vector3) => key(p) in collection;
 
   return { marked, marks };
-};
-
-const createMarkedStateDumper = (marked: VoxelMarker['marked']) => {
-  return (boundary: Box3): string => {
-    const { min, max } = boundary;
-    let dumpString = new Array(max.y - min.y + 2).fill('').join('\n');
-
-    dumpString = dumpString
-      .split('\n')
-      .map(
-        (v, i) =>
-          v +
-          (0 < i
-            ? (min.y + i - 1).toString().padStart(5, ' ')
-            : ''.padStart(5, ' '))
-      )
-      .join('\n');
-
-    for (let z = min.z; z <= max.z; z++) {
-      let slice = '[' + z.toString() + ']';
-      while (slice.length <= max.x - min.x) {
-        slice += ' ';
-      }
-      slice += '\n';
-
-      for (let y = min.y; y <= max.y; y++) {
-        for (let x = min.x; x <= max.x; x++) {
-          slice += marked(new Vector3(x, y, z)) ? '*' : '-';
-        }
-        slice += '\n';
-      }
-
-      const sliceLines = slice.split('\n');
-      dumpString = dumpString
-        .split('\n')
-        .map((v, i) => v + '|' + sliceLines[i])
-        .join('\n');
-    }
-    dumpString = dumpString
-      .split('\n')
-      .map((v, i) => v + '|')
-      .join('\n');
-    return 'x: ' + min.x + ' - ' + max.x + '\n' + dumpString;
-  };
 };

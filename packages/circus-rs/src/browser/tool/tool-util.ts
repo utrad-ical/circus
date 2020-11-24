785e93dd2b1c69490179ff557da515549cde05cb
Mari Ueda <uedamari0922@gmail.com>
2020-11-24 18:33:10 +0900

    Add AnnotationToolBase

--- packages/circus-rs/src/browser/tool/tool-util.ts
+++ packages/circus-rs/src/browser/tool/tool-util.ts
@@ -1,3 +1,7 @@
+import { Vector2, Vector3 } from 'three';
+import { convertScreenCoordinateToVolumeCoordinate } from '../section-util';
+import ViewerEvent from '../viewer/ViewerEvent';
+
 /**
  * Utility method which works in the same way as Math.sign().
  * Private polyfill.
@@ -9,3 +13,21 @@ export function sign(val: number): number {
   if (val === 0 || isNaN(val)) return val;
   return val > 0 ? 1 : -1;
 }
+
+/**
+ * Converts 2D point in viewer event (screen coordinate) to 3D point in volume coordinate space.
+ * @param ev target
+ */
+export const getVolumeCoordinateFromViewerEvent = (
+  ev: ViewerEvent
+): Vector3 => {
+  const screenPoint = [ev.viewerX!, ev.viewerY!];
+  const resolution = ev.viewer.getResolution();
+  const viewState = ev.viewer.getState();
+  const section = viewState.section;
+  return convertScreenCoordinateToVolumeCoordinate(
+    section,
+    new Vector2().fromArray(resolution),
+    new Vector2().fromArray(screenPoint)
+  );
+};

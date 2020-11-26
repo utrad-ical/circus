499cd72e21885bcb8b4cdb7158710ec8fb7c8219
Masayuki Kano <kano@genuine-ltd.com>
2020-11-26 09:43:46 +0900

    Fix typo

--- packages/circus-rs/src/browser/image-source/MprImageSourceWithDicomVolume.ts
+++ packages/circus-rs/src/browser/image-source/MprImageSourceWithDicomVolume.ts
@@ -4,7 +4,7 @@ export default interface MprImageSourceWithDicomVolume {
   getLoadedDicomVolume: () => DicomVolume | undefined;
 }
 
-export function isMprImageSourceWithVolumeLoader(
+export function isMprImageSourceWithDicomVolume(
   src: any
 ): src is MprImageSourceWithDicomVolume {
   return src.getLoadedDicomVolume !== undefined;

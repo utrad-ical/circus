134cf3d346b4ee074ae70641951967e6f7786ee6
Soichiro Miki <smiki-tky@umin.ac.jp>
2020-11-26 18:59:59 +0900

    Use worker_threds to extract DICOM pixel data

---
+++ packages/circus-rs/src/server/helper/extractor-worker/createDicomExtractorWorker.ts
@@ -0,0 +1,51 @@
+import { DicomImageData, FunctionService } from '@utrad-ical/circus-lib';
+import { Worker } from 'worker_threads';
+import { Pool } from 'tarn';
+import path from 'path';
+import os from 'os';
+
+export type DicomExtractorWorker = (
+  input: ArrayBuffer
+) => Promise<DicomImageData>;
+
+interface Options {
+  /**
+   * The number of maximum worker threds.
+   * Defaults to the number of CPUs retreived using `os.cpus().length`;
+   */
+  maxConcurrency?: number;
+}
+
+const workerMain = path.join(__dirname, 'workerMain.js');
+
+const createDicomExtractorWorker: FunctionService<
+  DicomExtractorWorker,
+  {},
+  Options
+> = async (options = {}) => {
+  const { maxConcurrency = os.cpus().length } = options;
+
+  const pool = new Pool({
+    create: () => new Worker(workerMain),
+    destroy: async (worker: Worker) => await worker.terminate(),
+    min: 0,
+    max: maxConcurrency,
+    propagateCreateError: true
+  });
+
+  return (input: ArrayBuffer) => {
+    return new Promise<DicomImageData>((resolve, reject) => {
+      pool.acquire().promise.then(worker => {
+        const cb = (result: DicomImageData) => {
+          resolve(result);
+          worker.off('message', cb);
+          pool.release(worker);
+        };
+        worker.on('message', cb);
+        worker.postMessage(input);
+      });
+    });
+  };
+};
+
+export default createDicomExtractorWorker;

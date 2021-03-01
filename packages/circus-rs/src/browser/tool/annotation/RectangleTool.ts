c11faa539f32a7a735f3e245c1b67601ec288a12
Mari Ueda <uedamari0922@gmail.com>
2021-03-01 18:28:22 +0900

    Fix figureType of RectangleTool

--- packages/circus-rs/src/browser/tool/annotation/RectangleTool.ts
+++ packages/circus-rs/src/browser/tool/annotation/RectangleTool.ts
@@ -2,5 +2,5 @@ import { FigureType } from '../../annotation/PlaneFigure';
 import PlaneFigureTool from './PlaneFigureTool';
 
 export default class RectangleTool extends PlaneFigureTool {
-  protected figureType: FigureType = 'circle';
+  protected figureType: FigureType = 'rectangle';
 }

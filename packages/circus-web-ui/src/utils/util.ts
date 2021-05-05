863a33fa12e1250e27a8a612a1f5c7b294bb6f8b
Soichiro Miki <smiki-tky@umin.ac.jp>
2021-05-05 20:05:57 +0900

    Fix sha-1 digst

--- packages/circus-web-ui/src/utils/util.ts
+++ packages/circus-web-ui/src/utils/util.ts
@@ -4,7 +4,7 @@ export const escapeRegExp = (str: string) => {
 };
 
 export const sha1 = async (arrayBuf: ArrayBuffer) => {
-  const hash = await crypto.subtle.digest('sha1', arrayBuf);
+  const hash = await crypto.subtle.digest('sha-1', arrayBuf);
   let hex = '';
   const h = '0123456789abcdef';
   new Uint8Array(hash).forEach(v => (hex += h[v >> 4] + h[v & 15]));

3ef3b241f5039de6b1cea1e7f388011a5b3dadf9
Soichiro Miki <smiki-tky@umin.ac.jp>
2021-05-24 09:50:05 +0900

    Introduce rusha (pure-js SHA-1 implementation)

--- packages/circus-web-ui/src/utils/util.ts
+++ packages/circus-web-ui/src/utils/util.ts
@@ -1,14 +1,14 @@
+import rusha from 'rusha';
+
 export const escapeRegExp = (str: string) => {
   str = str + '';
   return str.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&');
 };
 
-export const sha1 = async (arrayBuf: ArrayBuffer) => {
-  const hash = await crypto.subtle.digest('sha-1', arrayBuf);
-  let hex = '';
-  const h = '0123456789abcdef';
-  new Uint8Array(hash).forEach(v => (hex += h[v >> 4] + h[v & 15]));
-  return hex;
+export const sha1 = async (arrayBuffer: ArrayBuffer) => {
+  const hash = rusha.createHash();
+  hash.update(arrayBuffer);
+  return hash.digest('hex');
 };
 
 export const withCommas = (x: number) => {

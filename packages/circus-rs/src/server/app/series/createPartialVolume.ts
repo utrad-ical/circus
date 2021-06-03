e46914065d0df68506713b828dd7f969ffdcf6bd
Soichiro Miki <smiki-tky@umin.ac.jp>
2021-06-03 11:39:42 +0900

    Fix 500 error when PVD's delta is negative
    closes #147

--- packages/circus-rs/src/server/app/series/createPartialVolume.ts
+++ packages/circus-rs/src/server/app/series/createPartialVolume.ts
@@ -1,20 +1,19 @@
 import { VolumeAccessor } from '../../helper/createVolumeProvider';
 import DicomVolume from '../../../common/DicomVolume';
-import { PartialVolumeDescriptor } from '@utrad-ical/circus-lib';
+import {
+  PartialVolumeDescriptor,
+  partialVolumeDescriptorToArray
+} from '@utrad-ical/circus-lib';
 
 export default async function createPartialVolume(
   volumeAccessor: VolumeAccessor,
   partialVolumeDescriptor: PartialVolumeDescriptor,
   loadPriority: number = 0
 ): Promise<DicomVolume> {
-  const loadImages: number[] = [];
-  const { start, end, delta } = partialVolumeDescriptor;
-  for (let i = start; i <= end; i += delta) {
-    loadImages.push(i);
-  }
+  const loadImages = partialVolumeDescriptorToArray(partialVolumeDescriptor);
   await volumeAccessor.load(loadImages, loadPriority); // set higher priority and wait for loaded.
   const { columns, rows, pixelFormat } = volumeAccessor.imageMetadata.get(
-    start
+    partialVolumeDescriptor.start
   )!;
   const partialVolume = new DicomVolume(
     [columns, rows, loadImages.length],

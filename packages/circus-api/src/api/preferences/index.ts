a15c3f69d7d5960891557748c3da9b59883029b6
Soichiro Miki <smiki-tky@umin.ac.jp>
2021-07-19 17:04:52 +0900

    Fix unstable behavior around preferences, remove PUT

--- packages/circus-api/src/api/preferences/index.ts
+++ packages/circus-api/src/api/preferences/index.ts
@@ -1,24 +1,24 @@
+import { defaultPreferences } from '../../utils/preferenceUtils';
 import { RouteMiddleware } from '../../typings/middlewares';
+import httpStatus from 'http-status';
 
 export const handleGet: RouteMiddleware = () => {
   return async (ctx, next) => {
-    ctx.body = ctx.user.preferences;
-  };
-};
-
-export const handlePut: RouteMiddleware = ({ models }) => {
-  return async (ctx, next) => {
-    await models.user.modifyOne(ctx.user.userEmail, {
-      preferences: ctx.request.body
-    });
-    ctx.body = null;
+    ctx.body = {
+      ...defaultPreferences(),
+      ...ctx.user.preferences
+    };
   };
 };
 
 export const handlePatch: RouteMiddleware = ({ models }) => {
   return async (ctx, next) => {
+    const updates = ctx.request.body;
+    if (Object.keys(updates).length === 0) {
+      ctx.throw(httpStatus.BAD_REQUEST, 'The specified object is empty.');
+    }
     await models.user.modifyOne(ctx.user.userEmail, {
-      preferences: { ...ctx.user.preferences, ...ctx.request.body }
+      preferences: { ...ctx.user.preferences, ...updates }
     });
     ctx.body = null;
   };

6bdad34d6844c9b5bf875dc453f3a5af914d2006
Soichiro Miki <smiki-tky@umin.ac.jp>
2021-07-19 17:04:52 +0900

    Fix unstable behavior around preferences, remove PUT

--- packages/circus-api/src/api/preferences/index.test.ts
+++ packages/circus-api/src/api/preferences/index.test.ts
@@ -8,29 +8,12 @@ beforeAll(async () => {
 });
 afterAll(async () => await apiTest.tearDown());
 
-it('should return the preference of the current user', async () => {
+test('return the preference of the current user', async () => {
   const res = await axios.get('api/preferences');
   expect(res.data.theme).toBe('mode_white');
 });
 
-it('should modify the preference of the current user using PUT', async () => {
-  const res1 = await axios.request({
-    url: 'api/preferences',
-    method: 'put',
-    data: {
-      theme: 'mode_black',
-      personalInfoView: false,
-      seriesSearchPresets: [],
-      caseSearchPresets: [],
-      pluginJobSearchPresets: []
-    }
-  });
-  expect(res1.status).toBe(204);
-  const res2 = await axios.get('api/preferences');
-  expect(res2.data.theme).toBe('mode_black');
-});
-
-it('should modify the preference of the current user using PATCH', async () => {
+test('modify the preference of the current user using PATCH', async () => {
   const res1 = await axios.request({
     url: 'api/preferences',
     method: 'patch',
@@ -41,11 +24,20 @@ it('should modify the preference of the current user using PATCH', async () => {
   expect(res2.data.theme).toBe('mode_black');
 });
 
-it('should reject invalid preference update', async () => {
+test('reject invalid preference update', async () => {
   const res = await axios.request({
     url: 'api/preferences',
-    method: 'put',
+    method: 'patch',
     data: { theme: 'mode_pink', personalInfoView: false }
+  });
+  expect(res.status).toBe(400);
+});
+
+test('reject nonexistent preference key', async () => {
+  const res = await axios.request({
+    url: 'api/preferences',
+    method: 'patch',
+    data: { pineapple: true }
   });
   expect(res.status).toBe(400);
 });

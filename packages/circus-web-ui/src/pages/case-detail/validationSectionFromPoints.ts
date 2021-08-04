9cfff1321bb387b091a36dcae63f044f890bf3bd
ttakenaga <takenaga-tky@umin.ac.jp>
2021-08-04 08:35:00 +0000

    Move the getSectionFromPoints function

--- packages/circus-web-ui/src/pages/case-detail/validationSectionFromPoints.ts
+++ packages/circus-web-ui/src/pages/case-detail/validationSectionFromPoints.ts
@@ -1,42 +1,35 @@
+import { Vector3D } from '@utrad-ical/circus-lib/src/generateMhdHeader';
 import { InternalLabelOf } from './labelData';
-import createSectionFromPoints from './createSectionFromPoints';
 
 const validationSectionFromPoints = (
   points: InternalLabelOf<'point'>[],
   activePointName: string
 ) => {
   if (activePointName.indexOf('[') < 0) {
-    throw new Error(
-      'The name of the three point annotations must be suffixed by [1], [2] and [3]'
-    );
+    return 'The name of the three point annotations must be suffixed by [1], [2] and [3]';
   }
   const baseName = activePointName.slice(0, activePointName.indexOf('['));
 
-  const targetPoints: number[][] = [];
+  const targetPoints: Vector3D[] = [];
   for (let i = 1; i <= 3; i++) {
+    const labelName = baseName + '[' + i.toString() + ']';
     if (
       points.filter(label => {
-        return new RegExp('^' + baseName + '\\[' + i + '\\]' + '$').test(
-          label.name!
-        );
+        return label.name!.indexOf(labelName) === 0;
       }).length > 0
     ) {
       targetPoints.push(
         points.filter(label => {
-          return new RegExp('^' + baseName + '\\[' + i + '\\]' + '$').test(
-            label.name!
-          );
+          return label.name!.indexOf(labelName) === 0;
         })[0].data.location
       );
     }
   }
 
   if (targetPoints.length !== 3) {
-    throw new Error(
-      'The name of the three point annotations must be suffixed by [1], [2] and [3]'
-    );
+    return 'The name of the three point annotations must be suffixed by [1], [2] and [3]';
   }
-  return createSectionFromPoints(targetPoints);
+  return targetPoints;
 };
 
 export default validationSectionFromPoints;

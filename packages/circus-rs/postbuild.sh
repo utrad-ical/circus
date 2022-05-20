3c9427c4cf5c4a62fa10cbf4fd9dc3f4f5010731
ttakenaga <takenaga-tky@umin.ac.jp>
2022-05-20 01:58:03 +0000

    Add cpio in docker image

--- packages/circus-rs/postbuild.sh
+++ packages/circus-rs/postbuild.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-grep -lr circus-lib/src lib/ | xargs sed -i -e 's&circus-lib/src&circus-lib/lib&g'
 lessc src/browser/circus-rs.less lib/browser/circus-rs.css
 cp src/browser/circus-rs.less lib/browser/
 cd src/ && find . -name *.vert -or -name *.frag | cpio -upd ../lib/

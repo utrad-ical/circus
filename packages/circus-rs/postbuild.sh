1b1f53be3e86f8e1c0c10ca9faa3d2ae46cc4e27
ttakenaga <takenaga-tky@umin.ac.jp>
2022-05-26 07:15:48 +0000

    remove cpio in postbuild of circus-rs

--- packages/circus-rs/postbuild.sh
+++ packages/circus-rs/postbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 lessc src/browser/circus-rs.less lib/browser/circus-rs.css
 cp src/browser/circus-rs.less lib/browser/
-cd src/ && find . -name *.vert -or -name *.frag | cpio -upd ../lib/
+cd src/ && find . -name *.vert -or -name *.frag | xargs -i cp --parents {} ../lib/

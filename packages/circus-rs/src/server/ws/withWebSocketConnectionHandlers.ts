4bf0748ee26dcca7312a947b2d77f988dd063965
Masayuki Kano <kkano@genuine-ltd.com>
2022-05-31 15:15:09 +0900

    Disable compress option

--- packages/circus-rs/src/server/ws/withWebSocketConnectionHandlers.ts
+++ packages/circus-rs/src/server/ws/withWebSocketConnectionHandlers.ts
@@ -10,25 +10,25 @@ const withWebSocketConnectionHandlers = (routes: Record<string, WebSocketConnect
     const wss = new ws.Server({
       noServer: true,
       skipUTF8Validation: true,
-      perMessageDeflate: {
-        zlibDeflateOptions: {
-          // See zlib defaults.
-          chunkSize: 1024,
-          memLevel: 7,
-          level: 3
-        },
-        zlibInflateOptions: {
-          chunkSize: 10 * 1024
-        },
-        // Other options settable:
-        clientNoContextTakeover: true, // Defaults to negotiated value.
-        serverNoContextTakeover: true, // Defaults to negotiated value.
-        serverMaxWindowBits: 10, // Defaults to negotiated value.
-        // Below options specified as default values.
-        concurrencyLimit: 10, // Limits zlib concurrency for perf.
-        threshold: 1024 // Size (in bytes) below which messages
-        // should not be compressed if context takeover is disabled.
-      }
+      // perMessageDeflate: {
+      //   zlibDeflateOptions: {
+      //     // See zlib defaults.
+      //     chunkSize: 1024,
+      //     memLevel: 7,
+      //     level: 3
+      //   },
+      //   zlibInflateOptions: {
+      //     chunkSize: 10 * 1024
+      //   },
+      //   // Other options settable:
+      //   clientNoContextTakeover: true, // Defaults to negotiated value.
+      //   serverNoContextTakeover: true, // Defaults to negotiated value.
+      //   serverMaxWindowBits: 10, // Defaults to negotiated value.
+      //   // Below options specified as default values.
+      //   concurrencyLimit: 10, // Limits zlib concurrency for perf.
+      //   threshold: 1024 // Size (in bytes) below which messages
+      //   // should not be compressed if context takeover is disabled.
+      // }
     });
 
     wss.on('connection', function connection(ws: ws.WebSocket, request) {

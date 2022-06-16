0ddb5cf0e65e6a2bad5e77e7442a9c4b784a2f69
Soichiro Miki <smiki-tky@umin.ac.jp>
2022-06-16 13:35:57 +0900

    Extract LabelDisplay component

---
+++ packages/circus-web-ui/src/pages/case-detail/LabelDisplay.tsx
@@ -0,0 +1,95 @@
+import Icon from 'components/Icon';
+import React, { DragEventHandler, MouseEventHandler } from 'react';
+import styled from 'styled-components';
+import { mostReadable } from 'tinycolor2';
+import { InternalLabel, labelTypes } from './labelData';
+
+const LabelDisplay: React.FC<{
+  label: InternalLabel;
+  visibility?: 'normal' | 'hidden' | 'allHidden';
+  hintText?: string;
+  onColorClick?: MouseEventHandler;
+  colorDraggable?: boolean;
+  onColorDragStart?: DragEventHandler;
+  onColorDragOver?: DragEventHandler;
+  onColorDragEnd?: DragEventHandler;
+}> = props => {
+  const {
+    label,
+    hintText,
+    visibility = 'normal',
+    onColorClick,
+    colorDraggable,
+    onColorDragStart,
+    onColorDragOver,
+    onColorDragEnd
+  } = props;
+
+  const color = mostReadable(label.data.color, [
+    '#000000',
+    '#ffffff'
+  ]).toHexString();
+
+  return (
+    <StyledLabelDiv className="circus-label">
+      <div
+        className="color-preview"
+        onClick={onColorClick}
+        draggable={colorDraggable}
+        onDragStart={onColorDragStart}
+        onDragOver={onColorDragOver}
+        onDragEnd={onColorDragEnd}
+        style={{ backgroundColor: label.data.color, color }}
+      >
+        {visibility === 'allHidden' ? (
+          <Icon icon="glyphicon-remove-circle" />
+        ) : visibility === 'hidden' ? (
+          <Icon icon="eye-close" />
+        ) : null}
+      </div>
+      <div className="caption">
+        <Icon icon={labelTypes[label.type].icon} />
+        {label.name ?? <span className="no-name">Label</span>}
+        {hintText && (
+          <span className="hint" title={hintText}>
+            {hintText}
+          </span>
+        )}
+      </div>
+    </StyledLabelDiv>
+  );
+};
+
+export default LabelDisplay;
+
+const StyledLabelDiv = styled.div`
+  height: 28px;
+  display: inline-flex;
+  align-items: center;
+  gap: 10px;
+  > .color-preview {
+    width: 25px;
+    min-width: 25px;
+    display: block;
+    height: 20px;
+    text-align: center;
+    border: 1px solid ${(props: any) => props.theme.border};
+  }
+  > .caption {
+    white-space: nowrap;
+    flex-grow: 1;
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    > .circus-icon {
+      font-size: 130%;
+    }
+    .no-name {
+      color: gray;
+    }
+    .hint {
+      opacity: 0.7;
+      font-size: 80%;
+    }
+  }
+`;

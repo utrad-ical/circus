95ed92d60864e3516f899dcca9bf413f49b40ac6
ttakenaga <takenaga-tky@umin.ac.jp>
2022-07-07 02:29:26 +0000

    Add single connected label mode to interpolation

--- packages/circus-rs/src/common/morphology/intersliceInterpolation.ts
+++ packages/circus-rs/src/common/morphology/intersliceInterpolation.ts
@@ -10,9 +10,12 @@ const intersliceInterpolation: (
   array: Uint8Array,
   width: number,
   height: number,
-  nSlices: number
-) => Uint8Array = (array, width, height, nSlices) => {
+  nSlices: number,
+  mode: 'Single' | 'Multi'
+) => Uint8Array = (array, width, height, nSlices, mode) => {
   const labeledSlices: number[] = [];
+  const centerXs: number[] = [];
+  const centerYs: number[] = [];
   const output = array.slice(0);
   for (let k = 0; k < nSlices; k++) {
     if (
@@ -21,6 +24,26 @@ const intersliceInterpolation: (
         .some(value => value > 0)
     ) {
       labeledSlices.push(k);
+    }
+  }
+
+  if (mode === 'Single') {
+    for (const k of labeledSlices) {
+      let centerX = 0;
+      let centerY = 0;
+      let count = 0;
+      for (let j = 0; j < height; j++) {
+        for (let i = 0; i < width; i++) {
+          const pos = i + j * width + k * width * height;
+          if (0 < array[pos]) {
+            centerX += i;
+            centerY += j;
+            count++;
+          }
+        }
+      }
+      centerXs.push(centerX / count);
+      centerYs.push(centerY / count);
     }
   }
 
@@ -33,22 +56,62 @@ const intersliceInterpolation: (
       continue;
     }
     if (reset) {
-      slice1 = distanceTransform(
-        array.slice(k0 * width * height, (k0 + 1) * width * height),
-        width,
-        height
-      );
+      const inputSlice =
+        mode === 'Single'
+          ? resampled(
+              array.slice(k0 * width * height, (k0 + 1) * width * height),
+              width,
+              height,
+              centerXs[pos],
+              centerYs[pos]
+            )
+          : array.slice(k0 * width * height, (k0 + 1) * width * height);
+      slice1 = distanceTransform(inputSlice, width, height);
       reset = false;
     }
-    const slice2 = distanceTransform(
-      array.slice(k1 * width * height, (k1 + 1) * width * height),
-      width,
-      height
-    );
-    for (let k = k0 + 1; k < k1; k++) {
-      for (let i = 0; i < width * height; i++) {
-        const pos = i + k * width * height;
-        output[pos] = slice1![i] * (k1 - k) + slice2[i] * (k - k0) <= 0 ? 1 : 0;
+    const inputSlice =
+      mode === 'Single'
+        ? resampled(
+            array.slice(k1 * width * height, (k1 + 1) * width * height),
+            width,
+            height,
+            centerXs[pos + 1],
+            centerYs[pos + 1]
+          )
+        : array.slice(k1 * width * height, (k1 + 1) * width * height);
+    const slice2 = distanceTransform(inputSlice, width, height);
+    if (mode === 'Single') {
+      for (let k = k0 + 1; k < k1; k++) {
+        const interpolatedSlice = new Uint8Array(width * height);
+        for (let i = 0; i < width * height; i++) {
+          interpolatedSlice[i] =
+            slice1![i] * (k1 - k) + slice2[i] * (k - k0) <= 0 ? 1 : 0;
+        }
+
+        const centerX =
+          (centerXs[pos] * (k1 - k)) / (k1 - k0) +
+          (centerXs[pos + 1] * (k - k0)) / (k1 - k0);
+        const centerY =
+          (centerYs[pos] * (k1 - k)) / (k1 - k0) +
+          (centerYs[pos + 1] * (k - k0)) / (k1 - k0);
+        const resampleInterpolatedSlice = resampled(
+          interpolatedSlice,
+          width,
+          height,
+          centerX,
+          centerY,
+          false
+        );
+        for (let i = 0; i < width * height; i++) {
+          output[i + k * width * height] = resampleInterpolatedSlice[i];
+        }
+      }
+    } else {
+      for (let k = k0 + 1; k < k1; k++) {
+        for (let i = 0; i < width * height; i++) {
+          output[i + k * width * height] =
+            slice1![i] * (k1 - k) + slice2[i] * (k - k0) <= 0 ? 1 : 0;
+        }
       }
     }
     slice1 = slice2.slice(0);
@@ -57,3 +120,32 @@ const intersliceInterpolation: (
 };
 
 export default intersliceInterpolation;
+
+const resampled: (
+  array: Uint8Array,
+  width: number,
+  height: number,
+  centerX: number,
+  centerY: number,
+  reverse?: boolean
+) => Uint8Array = (array, width, height, centerX, centerY, reverse = true) => {
+  const output = new Uint8Array(width * height);
+  const ULx = Math.round(centerX - width / 2);
+  const ULy = Math.round(centerY - height / 2);
+
+  let pos = -1;
+  for (let j = ULy; j < ULy + height; j++) {
+    for (let i = ULx; i < ULx + width; i++) {
+      pos++;
+      if (i < 0 || width <= i || j < 0 || height <= j) continue;
+      const pos0 = i + j * width;
+      if (reverse) {
+        output[pos] = array[pos0];
+      } else {
+        output[pos0] = array[pos];
+      }
+    }
+  }
+
+  return output;
+};

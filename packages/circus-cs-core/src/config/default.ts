dd6b343fdd702b2f2fd38633c9f569a543a89471
Soichiro Miki <smiki-tky@umin.ac.jp>
2022-10-01 16:29:49 +0900

    Add doodHostWorkingPath option

--- packages/circus-cs-core/src/config/default.ts
+++ packages/circus-cs-core/src/config/default.ts
@@ -14,6 +14,7 @@ const defaults: circus.Configuration = {
   jobRunner: {
     options: {
       workingDirectory: path.join(os.tmpdir(), 'circus-cs'),
+      doodHostWorkingDirectory: undefined,
       removeTemporaryDirectory: true
     }
   },

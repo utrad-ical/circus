b8629f8a9cf519de48040e6420060f4202a145a3
Yukihiro Nomura <nomuray-tky@umin.ac.jp>
2023-01-16 18:18:31 +0900

    Add environment variable (AUTO_BOOT_JOB_MANAGER)

--- docker-build/services.sh
+++ docker-build/services.sh
@@ -6,4 +6,9 @@ mongod --config /etc/mongod.conf
 cd /var/circus/packages/circus-api
 pm2 start server.js --node-args="server.js"
 
+if [ $AUTO_BOOT_JOB_MANAGER = 1 ]; then
+    cd /var/circus/packages/circus-cs-core
+    pm2 start daemon.js --name circus-cs-job-manager
+fi
+
 tail -f /dev/null
